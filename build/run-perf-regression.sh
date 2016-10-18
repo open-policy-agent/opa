46b5d35de06ae687b2d2a0b91d77688a4cbbf09e
Torin Sandall <torinsandall@gmail.com>
2016-10-18 08:36:32 -0700

    Fix performance regression script
    Travis CI does a shallow clone of the repository. As a result, the original
    EPOCH commit is not present. Replace hard-coded EPOCH commit with commit ID
    from rev-list.

--- build/run-perf-regression.sh
+++ build/run-perf-regression.sh
@@ -7,15 +7,12 @@ DEFAULT_N=10
 # This is the number of commits to run the benchmark on.
 N=${1:-$DEFAULT_N}
 
-# This is the commit that performance benchmarks were first added.
-# This script runs the performance benchmarks on the last N commits
-# between HEAD and EPOCH.
-EPOCH=52b6684a8c03efe51cd4e166b8b60e5197cb747d
+HEAD=$(git rev-parse HEAD)
+EPOCH=$(git rev-list $HEAD | tail -n 1)
 
 echo "commit,subject,author,timestamp,benchmark,runs,latency(ns)"
 
-for rev in $(git rev-list HEAD...${EPOCH} | head -n ${N} | xargs); do
-
+for rev in $(git rev-list ${EPOCH}..${HEAD} | head -n $N | xargs); do
     # Check out next revision to run benchmarks on.
     git checkout $rev 1>&2 2>/dev/null
     metadata=$(git log --format="%h,%s,%ae,%at" -1 $rev)
