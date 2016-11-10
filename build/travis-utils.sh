6c7377ed03a8c5c20294fba252154e8a1577b87d
Torin Sandall <torinsandall@gmail.com>
2016-11-09 17:10:10 -0800

    Tweak Travis-CI configuration...

--- build/travis-utils.sh
+++ build/travis-utils.sh
@@ -1,14 +1,10 @@
 # Travis-CI sets TRAVIS_PULL_REQUEST=false when the build is triggered for
 # changes pushed into github.com/open-policy-agent/opa.
 function is_travis_push_env() {
-
-    if [ "$TRAVIS_GO_VERSION" != "$LATEST_GO_VERSION" ]; then
-      return 0
+    if [ "$TRAVIS_GO_VERSION" = "$LATEST_GO_VERSION" ]; then
+        if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+            return 0
+        fi
     fi
-
-    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-      return 0
-    fi
-
     return 1
 }
