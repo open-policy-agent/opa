f47639684017800c4c51178685c7739b75c805e3
Torin Sandall <torinsandall@gmail.com>
2017-03-07 15:16:51 -0800

    Update docker 'latest' tagging

--- build/travis-utils.sh
+++ build/travis-utils.sh
@@ -6,3 +6,13 @@ function is_travis_push_env() {
     fi
     return 1
 }
+
+# Travis-CI sets TRAVIS_TAG=<tag> when the build is triggered for a tag.
+# If the tag matches the source version then we can assume this is build is
+# for a release.
+function is_travis_release_env() {
+  if [ "$TRAVIS_TAG" = "$(make version)" ]; then
+    return 0
+  fi
+  return 1
+}
