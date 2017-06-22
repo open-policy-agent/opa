c02370b3558e7afd27a4de1eced8407ac36e799c
Torin Sandall <torinsandall@gmail.com>
2017-06-22 15:25:27 -0700

    Refactor release build script
    - Release can be built from local source instead of GitHub
    - Release can be built from local source without tag
    - Improve usage/error handling

--- build/build-release.sh
+++ build/build-release.sh
@@ -1,33 +1,67 @@
 #!/usr/bin/env bash
+# Script to build OPA releases. Assumes execution environment is golang Docker container.
 
-# This script will build an OPA release given (1) the release version and
-# (2) the output directory to write artifacts to. This script assumes it is
-# running under Docker.
+set -e
 
-set -ex
+usage() {
+    echo "build-release.sh --output-dir=<path>"
+    echo "                 --source-url=<git-url>"
+    echo "                 [--tag=<tag>]"
+}
 
-VERSION=$1
-RELEASE_DIR=$2
+for i in "$@"; do
+    case $i in
+    --source-url=*)
+        SOURCE_URL="${i#*=}"
+        shift
+        ;;
+    --output-dir=*)
+        OUTPUT_DIR="${i#*=}"
+        shift
+        ;;
+    --tag=*)
+        TAG="${i#*=}"
+        shift
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+    esac
+done
 
-git clone https://github.com/open-policy-agent/opa.git /go/src/github.com/open-policy-agent/opa
-cd /go/src/github.com/open-policy-agent/opa
-git checkout v$VERSION
+if [ -z "$OUTPUT_DIR" ]; then
+    usage
+    exit 1
+elif [ -z "$SOURCE_URL" ]; then
+    usage
+    exit 1
+fi
 
 build_binaries() {
     make deps
     GOOS=darwin GOARCH=amd64 make build
     GOOS=linux GOARCH=amd64 make build
-    mv opa_*_* $RELEASE_DIR
+    mv opa_*_* $OUTPUT_DIR
 }
 
 build_site() {
     pushd site
     jekyll build . && rm _site/assets/.sprockets-manifest-*.json
-    tar czvf $RELEASE_DIR/site.tar.gz -C _site .
+    tar czvf $OUTPUT_DIR/site.tar.gz -C _site .
     popd
 }
 
+clone_repo() {
+    git clone $SOURCE_URL /go/src/github.com/open-policy-agent/opa
+    cd /go/src/github.com/open-policy-agent/opa
+    if [ -n "$TAG" ]; then
+        git checkout $TAG
+    fi
+}
+
 main() {
+    clone_repo
     build_binaries
     build_site
     make test
