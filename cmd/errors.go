5f944ac695bf4b7f8ce58da2de3cfb82e49bf574
Torin Sandall <torinsandall@gmail.com>
2017-09-06 09:20:02 -0700

    Refactor error limit flag
    Avoid duplicating the flag description in each command that requires it.

---
+++ cmd/errors.go
@@ -0,0 +1,14 @@
+// Copyright 2017 The OPA Authors.  All rights reserved.
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/spf13/pflag"
+)
+
+func setMaxErrors(fs *pflag.FlagSet, errLimit *int) {
+	fs.IntVarP(errLimit, "max-errors", "m", ast.CompileErrorLimitDefault, "set the number of errors to allow before compilation fails early")
+}
