1a6ac3596a5c55bac5bed1f5250f88257dbf3fed
Torin Sandall <torinsandall@gmail.com>
2018-01-10 20:33:51 -0800

    Remove guards in deps installer
    If the pigeon dependency is updated, the parser is regenerated and
    committed back to the repo. Other developers need to re-install pigeon
    whenever this happens (otherwise they will regenerate a stale version of
    the parser.)

--- build/install-deps.sh
+++ build/install-deps.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-which pigeon >/dev/null || {
-	echo "Installing pigeon from vendor"
-	go install ./vendor/github.com/mna/pigeon
-}
-
-
-which goimports >/dev/null || {
-	echo "Installing goimports from vendor"
-	go install ./vendor/golang.org/x/tools/cmd/goimports
-}
-
-which golint >/dev/null || {
-	echo "Installing golint from vendor"
-	go install ./vendor/github.com/golang/lint/golint
-}
+go install ./vendor/github.com/mna/pigeon
+go install ./vendor/golang.org/x/tools/cmd/goimports
+go install ./vendor/github.com/golang/lint/golint
