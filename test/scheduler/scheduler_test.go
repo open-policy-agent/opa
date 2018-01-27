f8e1467133bc40cfdc8a04b0861d5efbc0b00f25
Torin Sandall <torinsandall@gmail.com>
2018-01-27 11:05:54 -0800

    Refactor scheduler test syntax usage

--- test/scheduler/scheduler_test.go
+++ test/scheduler/scheduler_test.go
@@ -154,8 +154,8 @@ import input.pod as req
 fit[node_name] = weight {
     scheduler_name[my_scheduler_name]
     filter[node_id]
-    prioritise[node_id] = weight
-    node_name = nodes[node_id].metadata.name
+    weight := prioritise[node_id]
+    node_name := nodes[node_id].metadata.name
 }
 
 filter[node_id] {
@@ -169,17 +169,17 @@ filter[node_id] {
     # Filtering for hollow node pods. Force them all onto
     # localhost node for testing purposes.
     hollow_node
-    nodes[node_id].metadata.name = "127.0.0.1"
+    nodes[node_id].metadata.name == "127.0.0.1"
 }
 
 port_conflicts[node_id] {
-    node = nodes[node_id]
-    pods[i].spec.nodeName = node.metadata.name
-    container = pods[i].spec.containers[j]
-    port = container.ports[k].hostPort
-    req_container = req.spec.containers[l]
-    req_port = req_container.ports[m].hostPort
-    req_port = port
+    node := nodes[node_id]
+    pods[i].spec.nodeName == node.metadata.name
+    container := pods[i].spec.containers[j]
+    port := container.ports[k].hostPort
+    req_container := req.spec.containers[l]
+    req_port := req_container.ports[m].hostPort
+    req_port == port
 }
 
 disk_conflicts[node_id] {
@@ -189,200 +189,196 @@ disk_conflicts[node_id] {
 }
 
 gce_persistent_disk_conflicts[node_id] {
-    req_disk = req.spec.volumes[i].gcePersistentDisk
+    req_disk := req.spec.volumes[i].gcePersistentDisk
     not req_disk.readOnly
-    node = nodes[node_id]
-    pod = pods[j]
-    pod.spec.nodeName = node.metadata.name
-    disk = pod.volumes[k].gcePersistentDisk
-    req_disk.pdName = disk.pdName
+    node := nodes[node_id]
+    pod := pods[j]
+    pod.spec.nodeName == node.metadata.name
+    disk := pod.volumes[k].gcePersistentDisk
+    req_disk.pdName == disk.pdName
 } {
-    req_disk = req.spec.volumes[i].gcePersistentDisk
+    req_disk := req.spec.volumes[i].gcePersistentDisk
     req_disk.readOnly
-    node = nodes[node_id]
-    pod = pods[j]
-    pod.spec.nodeName = node.metadata.name
-    disk = pod.volumes[k].gcePersistentDisk
-    req_disk.pdName = disk.pdName
+    node := nodes[node_id]
+    pod := pods[j]
+    pod.spec.nodeName == node.metadata.name
+    disk := pod.volumes[k].gcePersistentDisk
+    req_disk.pdName == disk.pdName
     not disk.readOnly
 }
 
 aws_ebs_conflicts[node_id] {
-    req_disk = req.spec.volumes[i].awsElasticBlockStore
-    node = nodes[node_id]
-    pod = pods[j]
-    pod.spec.nodeName = node.metadata.name
-    disk = pod.volumes[k].awsElasticBlockStore
-    disk.volumeID = req_disk.volumeID
+    req_disk := req.spec.volumes[i].awsElasticBlockStore
+    node := nodes[node_id]
+    pod := pods[j]
+    pod.spec.nodeName == node.metadata.name
+    disk := pod.volumes[k].awsElasticBlockStore
+    disk.volumeID == req_disk.volumeID
 }
 
 rbd_conflicts[node_id] {
-    req_disk = req.spec.volumes[i].rbd
-    node = nodes[node_id]
-    pod = pods[j]
-    pod.spec.nodeName = node.metadata.name
+    req_disk := req.spec.volumes[i].rbd
+    node := nodes[node_id]
+    pod := pods[j]
+    pod.spec.nodeName == node.metadata.name
     disk = pod.volumes[k].rbd
-    req_disk.image = disk.image
-    req_disk.pool = disk.pool
-    req_disk.monitors[l] = disk.monitors[m]
+    req_disk.image == disk.image
+    req_disk.pool == disk.pool
+    req_disk.monitors[l] == disk.monitors[m]
 }
 
 pv_zone_label_match[node_id] {
-    req_volume = req.spec.volumes[i]
-    req_claim_name = req_volume.persistentVolumeClaim.claimName
-    req_namespace = req.metadata.namespace
-    pvcs[j].metadata.namespace = req_namespace
-    pvcs[j].metadata.name = req_claim_name
-    pvs[k].metadata.name = pvcs[j].spec.volumeName
-    label = zone_labels[l]
-    pvs[k].metadata.labels[label] = value
-    nodes[node_id].metadata.labels[label] = value
+    req_volume := req.spec.volumes[i]
+    req_claim_name := req_volume.persistentVolumeClaim.claimName
+    req_namespace := req.metadata.namespace
+    pvcs[j].metadata.namespace == req_namespace
+    pvcs[j].metadata.name == req_claim_name
+    pvs[k].metadata.name == pvcs[j].spec.volumeName
+    label := zone_labels[l]
+    value := pvs[k].metadata.labels[label]
+    nodes[node_id].metadata.labels[label] == value
 }
 
 resources_available[node_id] {
-    node = nodes[node_id]
+    node := nodes[node_id]
     not pods_exceeded[node_id]
     not mem_exceeded[node_id]
     not cpu_exceeded[node_id]
 }
 
 pods_exceeded[node_id] {
-    count(pods_on_node[node_id], num_pods)
-    to_number(nodes[node_id].status.allocatable.pods, max_pods)
+    num_pods := count(pods_on_node[node_id])
+    max_pods := to_number(nodes[node_id].status.allocatable.pods)
     num_pods >= max_pods
 }
 
 mem_exceeded[node_id] {
-    allocatable_mem[node_id] = alloc
-    mem_total[node_id] = total
+    alloc := allocatable_mem[node_id]
+    total := mem_total[node_id]
     total >= alloc
 }
 
 cpu_exceeded[node_id] {
-    allocatable_cpu[node_id] = alloc
-    cpu_total[node_id] = total
+    alloc := allocatable_cpu[node_id]
+    total := cpu_total[node_id]
     total >= alloc
 }
 
 cpu_total[node_id] = cpu_t {
-    sum([cpu | cpu = req_cpu[_]], cpu_requested)
-    cpu_requested + used_cpu[node_id] = cpu_t
+    cpu_requested := sum([cpu | cpu := req_cpu[_]])
+    cpu_t := cpu_requested + used_cpu[node_id]
 }
 
 mem_total[node_id] = mem_t {
-    sum([mem | mem = req_mem[_]], mem_requested)
-    mem_requested + used_mem[node_id] = mem_t
+    mem_requested := sum([mem | mem := req_mem[_]])
+    mem_t := mem_requested + used_mem[node_id]
 }
 
 cpu_nonzero_total[node_id] = cpu_t {
-    sum([cpu | cpu = req_cpu[_]], cpu_requested)
-    cpu_requested + used_nonzero_cpu[node_id] = cpu_t
+    cpu_requested := sum([cpu | cpu := req_cpu[_]])
+    cpu_t := cpu_requested + used_nonzero_cpu[node_id]
 }
 
 mem_nonzero_total[node_id] = mem_t {
-    sum([mem | mem = req_mem[_]], mem_requested)
-    mem_requested + used_nonzero_mem[node_id] = mem_t
+    mem_requested := sum([mem | mem := req_mem[_]])
+    mem_t := mem_requested + used_nonzero_mem[node_id]
 }
 
 req_cpu[name] = cpu {
-    container = req.spec.containers[_]
-    container.name = name
-    container.resources.requests.cpu = cpu
+    container := req.spec.containers[_]
+    name := container.name
+    cpu := container.resources.requests.cpu
 } {
-    container = req.spec.containers[i]
-    container.name = name
+    container := req.spec.containers[i]
+    name := container.name
     not container.resources.requests.cpu
-    cpu = default_milli_cpu_req
+    cpu := default_milli_cpu_req
 }
 
 req_mem[name] = mem {
-    container = req.spec.containers[_]
-    container.name = name
-    container.resources.requests.memory = mem
+    container := req.spec.containers[_]
+    name := container.name
+    mem := container.resources.requests.memory
 } {
-    container = req.spec.containers[_]
-    container.name = name
+    container := req.spec.containers[_]
+    name := container.name
     not container.resources.requests.memory
-    mem = default_memory_req
+    mem := default_memory_req
 }
 
 allocatable_mem[node_id] = alloc {
-    nodes[node_id].status.allocatable.memory = alloc
+    alloc := nodes[node_id].status.allocatable.memory
 }
 
 allocatable_cpu[node_id] = alloc {
-    nodes[node_id].status.allocatable.cpu = alloc
+    alloc := nodes[node_id].status.allocatable.cpu
 }
 
 used_mem[node_id] = used {
-    pods_on_node[node_id] = node_pods
-    mem = [m | node_pods[_] = pod
-               pod.spec.containers[_] = container
-               requested = container.resources.requests
-               requested.memory = m]
-    sum(mem, used)
+    node_pods := pods_on_node[node_id]
+    mem := [m | pod := node_pods[_]
+                container := pod.spec.containers[_]
+                requested := container.resources.requests
+                m := requested.memory]
+    used := sum(mem)
 }
 
 used_cpu[node_id] = used {
-    pods_on_node[node_id] = node_pods
-    cpu = [c | node_pods[_] = pod
-               pod.spec.containers[_] = container
-               container.resources.requests = requested
-               requested.cpu = c]
-    sum(cpu, used)
+    node_pods := pods_on_node[node_id]
+    cpu := [c | pod := node_pods[_]
+                container := pod.spec.containers[_]
+                requested := container.resources.requests
+                c := requested.cpu]
+    used := sum(cpu)
 }
 
 used_nonzero_mem[node_id] = used {
-    pods_on_node[node_id] = node_pods
-    mem = [m | node_pods[_] = pod
-               pod.spec.containers[_] = container
-               requested = container.resources.requests
-               requested.memory = m]
-    def = [m | node_pods[_] = pod
-               pod.spec.containers[_] = container
-               not container.resources.requests.memory
-               m = default_memory_req]
-    sum(mem, used_nz)
-    sum(def, used_default)
-    used_nz + used_default = used
+    node_pods := pods_on_node[node_id]
+    mem := [m | pod := node_pods[_]
+                container := pod.spec.containers[_]
+                requested := container.resources.requests
+                m := requested.memory]
+    def := [m | pod := node_pods[_]
+                container := pod.spec.containers[_]
+                not container.resources.requests.memory
+                m := default_memory_req]
+    used := sum(mem) + sum(def)
 }
 
 used_nonzero_cpu[node_id] = used {
-    pods_on_node[node_id] = node_pods
-    cpu = [c | node_pods[_] = pod
-               pod.spec.containers[_] = container
-               container.resources.requests = requested
-               requested.cpu = c]
-    def = [c | node_pods[_] = pod
-               pod.spec.containers[_] = container
+    node_pods := pods_on_node[node_id]
+    cpu = [c | pod := node_pods[_]
+               container := pod.spec.containers[_]
+               requested := container.resources.requests
+               c := requested.cpu]
+    def = [c | pod := node_pods[_]
+               container := pod.spec.containers[_]
                not container.resources.requests.cpu
-               c = default_milli_cpu_req]
-    sum(cpu, used_nz)
-    sum(def, used_default)
-    used_nz + used_default = used
+               c := default_milli_cpu_req]
+    used = sum(cpu) + sum(def)
 }
 
 pods_on_node[node_id] = pds {
-    node_name = nodes[node_id].metadata.name
-    pds = [p | pods[i].spec.nodeName = node_name; p = pods[i]]
+    node_name := nodes[node_id].metadata.name
+    pds := [p | pods[i].spec.nodeName == node_name; p := pods[i]]
 }
 
 hollow_node {
-    req.metadata.labels[i] = "hollow-node"
+    req.metadata.labels[i] == "hollow-node"
 }
 
 blacklisted[node_name] {
-    node_names = [
+    node_names := [
         "127.0.0.1"
     ]
-    node_name = node_names[i]
+    node_name := node_names[i]
 }
 
 my_scheduler_name = "experimental"
 
 # This scheduler is responsible for pods annotated with the following scheduler names.
 scheduler_name[scheduler] {
-    req.metadata.annotations[k8s_scheduler_annotations] = scheduler
+    scheduler := req.metadata.annotations[k8s_scheduler_annotations]
 }
 
 # Scheduler annotation. This annotation indicates whether the scheduler is responsible
@@ -409,104 +405,79 @@ default_milli_cpu_req = 100    # 0.1 cores
 default_memory_req = 209715200 # 200MB
 
 prioritise[node_id] = weight {
-    least_requested[node_id] = lr_w
-    balanced_allocation[node_id] = ba_w
-    selector_spreading[node_id] = ss_w
-    sum([lr_w, ba_w, ss_w], sum_w)
-    sum_w / 3 = weight
+    weight := sum([
+        selector_spreading[node_id],
+        balanced_allocation[node_id],
+        least_requested[node_id]]) / 3
 }
 
 least_requested[node_id] = weight {
-    cpu_weight[node_id] = cpu_w
-    mem_weight[node_id] = mem_w
-    cpu_w + mem_w = total_w
-    total_w / 2 = weight
+    weight := (cpu_weight[node_id] + mem_weight[node_id]) / 2
 }
 
 cpu_weight[node_id] = weight {
-    allocatable_cpu[node_id] = cpu_capacity
-    cpu_capacity - cpu_nonzero_total[node_id] = cpu_delta
-    cpu_delta * 10 = cpu_scaled
-    cpu_scaled / cpu_capacity = weight
+    cpu_capacity := allocatable_cpu[node_id]
+    weight := ((cpu_capacity - cpu_nonzero_total[node_id]) * 10) / cpu_capacity
 }
 
 mem_weight[node_id] = weight {
-    allocatable_mem[node_id] = mem_capacity
-    mem_capacity - mem_nonzero_total[node_id] = mem_delta
-    mem_delta * 10 = mem_scaled
-    mem_scaled / mem_capacity = weight
+    mem_capacity := allocatable_mem[node_id]
+    weight := ((mem_capacity - mem_nonzero_total[node_id]) * 10) / mem_capacity
 }
 
 balanced_allocation[node_id] = weight {
-    mem_fraction[node_id] = mem_f
-    cpu_fraction[node_id] = cpu_f
+    mem_f := mem_fraction[node_id]
+    cpu_f := cpu_fraction[node_id]
     mem_f < 1
     cpu_f < 1
-    cpu_f - mem_f = usage
-    abs(usage, usage_pos)
-    usage_pos * 10 = usage_scaled
-    10 - usage_scaled = weight
+    weight := (10 - (abs(cpu_f - mem_f) * 10))
 } {
-    mem_fraction[node_id] = mem_f
-    cpu_fraction[node_id] = cpu_f
-    mem_f >= 1
-    cpu_f >= 1
+    mem_fraction[node_id] >= 1
+    cpu_fraction[node_id] >= 1
     weight = 0
 } {
-    mem_fraction[node_id] = mem_f
-    cpu_fraction[node_id] = cpu_f
-    mem_f < 1
-    cpu_f >= 1
+    mem_fraction[node_id] < 1
+    cpu_fraction[node_id] >= 1
     weight = 0
 } {
-    mem_fraction[node_id] = mem_f
-    cpu_fraction[node_id] = cpu_f
-    mem_f >= 1
-    cpu_f < 1
+    mem_fraction[node_id] >= 1
+    cpu_fraction[node_id] < 1
     weight = 0
 }
 
 cpu_fraction[node_id] = f {
-    cpu_nonzero_total[node_id] = cpu
-    allocatable_cpu[node_id] = cpu_capacity
-    cpu / cpu_capacity = f
+    f := cpu_nonzero_total[node_id] / allocatable_cpu[node_id]
 }
 
 mem_fraction[node_id] = f {
-    mem_nonzero_total[node_id] = mem
-    allocatable_mem[node_id] = mem_capacity
-    mem / mem_capacity = f
+    f := mem_nonzero_total[node_id] / allocatable_mem[node_id]
 }
 
 selector_spreading[node_id] = weight {
-    max_rc_match_count = max_count
-    max_count - rc_match_count[node_id] = delta
-    delta / max_count = ratio
-    ratio * 10 = weight
+    max_count := max_rc_match_count
+    weight := ((max_count - rc_match_count[node_id]) / max_count) * 10
 }
 
 max_rc_match_count = max_count {
-    max([c | rc_match_count[_] = c], max_c)
-    max([1, max_c], max_count)
+    max([1, max([c | c := rc_match_count[_]])], max_count)
 }
 
 rc_match_count[node_id] = cnt {
     nodes[node_id]
     rcs_req_matches[rc_id]
-    count([1 | rcs_on_node[node_id][_] = rc_id], cnt)
+    cnt := count([1 | rcs_on_node[node_id][_] == rc_id])
 }
 
 rcs_on_node[node_id] = rc_ids {
     pods_on_node[node_id] = node_pods
-    rc_ids = [ rc_id | node_pods[_] = pod
-                       rcs_for_pod[pod.metadata.uid][_] = rc_id]
+    rc_ids = [ rc_id | pod := node_pods[_]
+                       rc_id := rcs_for_pod[pod.metadata.uid][_]]
 }
 
 rcs_for_pod[pod_id] = rc_ids {
     pods[pod_id]
     rc_ids = [rc_id | rcs[rc_id]
-                      x = [pod_id, rc_id]
-                      selector_matches[x]]
+                      selector_matches[[pod_id, rc_id]]]
 }
 
 selector_matches[[pod_id, rc_id]] {
@@ -518,8 +489,8 @@ selector_matches[[pod_id, rc_id]] {
 
 selector_not_matches[[pod_id, rc_id]] {
     pods[pod_id] = pod
-    rcs[rc_id] = rc
-    rc.spec.selector[k] = v
+    rc := rcs[rc_id]
+    v := rc.spec.selector[k]
     not pod.metadata.labels[k] = v
 }
 
@@ -529,7 +500,7 @@ rcs_req_matches[rc_id] {
 }
 
 rcs_req_not_matches[rc_id] {
-    rcs[rc_id].spec.selector[label] = value
+    value := rcs[rc_id].spec.selector[label]
     not req.metadata.labels[label] = value
 }
 
