a400fbab78dc2e7b702ce49dbbc528d8f2beb452
Torin Sandall <torinsandall@gmail.com>
2018-08-20 16:12:06 -0700

    Add deps subcommand
    These changes add a new subcommand that analyzes policies and prints
    base and virtual document dependencies.
    
    Signed-off-by: Torin Sandall <torinsandall@gmail.com>

---
+++ cmd/deps.go
@@ -0,0 +1,113 @@
+// Copyright 2018 The OPA Authors.  All rights reserved.
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/open-policy-agent/opa/dependencies"
+	"github.com/open-policy-agent/opa/internal/presentation"
+
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/loader"
+	"github.com/open-policy-agent/opa/util"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+type depsCommandParams struct {
+	dataPaths repeatedStringFlag
+	format    *util.EnumFlag
+	ignore    []string
+}
+
+const (
+	depsFormatPretty = "pretty"
+	depsFormatJSON   = "json"
+)
+
+func init() {
+
+	var params depsCommandParams
+
+	params.format = util.NewEnumFlag(depsFormatPretty, []string{
+		depsFormatPretty, depsFormatJSON,
+	})
+
+	depsCommand := &cobra.Command{
+		Use:   "deps <query>",
+		Short: "Analyze Rego query dependencies",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return errors.New("specify exactly one query argument")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := deps(args, params); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	depsCommand.Flags().VarP(params.format, "format", "f", "set output format")
+	depsCommand.Flags().VarP(&params.dataPaths, "data", "d", "set data file(s) or directory path(s)")
+	setIgnore(depsCommand.Flags(), &params.ignore)
+
+	RootCommand.AddCommand(depsCommand)
+}
+
+func deps(args []string, params depsCommandParams) error {
+
+	query, err := ast.ParseBody(args[0])
+	if err != nil {
+		return err
+	}
+
+	f := loaderFilter{
+		Ignore: params.ignore,
+	}
+
+	result, err := loader.Filtered(params.dataPaths.v, f.Apply)
+	if err != nil {
+		return err
+	}
+
+	modules := map[string]*ast.Module{}
+	for _, m := range result.Modules {
+		modules[m.Name] = m.Parsed
+	}
+
+	compiler := ast.NewCompiler()
+	compiler.Compile(modules)
+
+	if compiler.Failed() {
+		return compiler.Errors
+	}
+
+	brs, err := dependencies.Base(compiler, query)
+	if err != nil {
+		return err
+	}
+
+	vrs, err := dependencies.Virtual(compiler, query)
+	if err != nil {
+		return err
+	}
+
+	output := presentation.DepAnalysisOutput{
+		Base:    brs,
+		Virtual: vrs,
+	}
+
+	switch params.format.String() {
+	case depsFormatJSON:
+		return presentation.JSON(os.Stdout, output)
+	default:
+		return output.Pretty(os.Stdout)
+	}
+}
