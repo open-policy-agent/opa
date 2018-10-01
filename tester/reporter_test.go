8220f743d275809ec4a129c6208ad6a0400c0d8f
Stephan Renatus <srenatus@chef.io>
2018-10-01 14:06:48 +0200

    cmd/test: output traces on failures (when verbose or JSON)
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- tester/reporter_test.go
+++ tester/reporter_test.go
@@ -6,34 +6,33 @@ import (
 	"testing"
 )
 
-func TestPrettyReporter(t *testing.T) {
-
-	ts := []*Result{
-		{nil, "data.foo.bar", "test_baz", false, nil, 0},
-		{nil, "data.foo.bar", "test_qux", false, fmt.Errorf("some err"), 0},
-		{nil, "data.foo.bar", "test_corge", true, nil, 0},
-	}
-
+func TestPrettyReporterVerbose(t *testing.T) {
 	var buf bytes.Buffer
+	ts := []*Result{
+		{nil, "data.foo.bar", "test_baz", false, nil, 0, "trace test_baz"},
+		{nil, "data.foo.bar", "test_qux", false, fmt.Errorf("some err"), 0, "trace test_qux"},
+		{nil, "data.foo.bar", "test_corge", true, nil, 0, "trace test_corge"},
+	}
 
 	r := PrettyReporter{
 		Output:  &buf,
 		Verbose: true,
 	}
 
-	ch := make(chan *Result)
-	go func() {
-		for _, tr := range ts {
-			ch <- tr
-		}
-		close(ch)
-	}()
-
+	ch := resultsChan(ts)
 	if err := r.Report(ch); err != nil {
 		t.Fatal(err)
 	}
 
-	exp := `data.foo.bar.test_baz: PASS (0s)
+	exp := `FAILURES
+--------------------------------------------------------------------------------
+data.foo.bar.test_corge: FAIL (0s)
+
+trace test_corge
+
+SUMMARY
+--------------------------------------------------------------------------------
+data.foo.bar.test_baz: PASS (0s)
 data.foo.bar.test_qux: ERROR (0s)
   some err
 data.foo.bar.test_corge: FAIL (0s)
@@ -46,4 +45,94 @@ ERROR: 1/3
 	if exp != buf.String() {
 		t.Fatalf("Expected:\n\n%v\n\nGot:\n\n%v", exp, buf.String())
 	}
+}
+
+func TestPrettyReporter(t *testing.T) {
+	var buf bytes.Buffer
+	ts := []*Result{
+		{nil, "data.foo.bar", "test_baz", false, nil, 0, "trace test_baz"},
+		{nil, "data.foo.bar", "test_qux", false, fmt.Errorf("some err"), 0, "trace test_qux"},
+		{nil, "data.foo.bar", "test_corge", true, nil, 0, "trace test_corge"},
+	}
+
+	r := PrettyReporter{
+		Output:  &buf,
+		Verbose: false,
+	}
+	ch := resultsChan(ts)
+	if err := r.Report(ch); err != nil {
+		t.Fatal(err)
+	}
+
+	exp := `data.foo.bar.test_qux: ERROR (0s)
+  some err
+data.foo.bar.test_corge: FAIL (0s)
+--------------------------------------------------------------------------------
+PASS: 1/3
+FAIL: 1/3
+ERROR: 1/3
+`
+
+	if exp != buf.String() {
+		t.Fatalf("Expected:\n\n%v\n\nGot:\n\n%v", exp, buf.String())
+	}
+}
+
+func TestJSONReporter(t *testing.T) {
+	var buf bytes.Buffer
+	ts := []*Result{
+		{nil, "data.foo.bar", "test_baz", false, nil, 0, "trace test_baz"},
+		{nil, "data.foo.bar", "test_qux", false, fmt.Errorf("some err"), 0, "trace test_qux"},
+		{nil, "data.foo.bar", "test_corge", true, nil, 0, "trace test_corge"},
+	}
+
+	r := JSONReporter{
+		Output: &buf,
+	}
+	ch := resultsChan(ts)
+	if err := r.Report(ch); err != nil {
+		t.Fatal(err)
+	}
+
+	exp := `[
+  {
+    "location": null,
+    "package": "data.foo.bar",
+    "name": "test_baz",
+    "duration": 0,
+    "trace": "trace test_baz"
+  },
+  {
+    "location": null,
+    "package": "data.foo.bar",
+    "name": "test_qux",
+    "error": {},
+    "duration": 0,
+    "trace": "trace test_qux"
+  },
+  {
+    "location": null,
+    "package": "data.foo.bar",
+    "name": "test_corge",
+    "fail": true,
+    "duration": 0,
+    "trace": "trace test_corge"
+  }
+]
+`
+
+	if exp != buf.String() {
+		t.Fatalf("Expected:\n\n%v\n\nGot:\n\n%v", exp, buf.String())
+	}
+}
+
+func resultsChan(ts []*Result) chan *Result {
+	ch := make(chan *Result)
+	go func() {
+		for _, tr := range ts {
+			ch <- tr
+		}
+		close(ch)
+	}()
+	return ch
 }
