c879a93526d9d3fdfda20f4f5d3c0bd60eaf42e6
lucperkins <lucperkins@gmail.com>
2019-04-02 13:00:12 -0700

    Fix Git issue in documentation versioning script
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- docs/scripts/load-docs.sh
+++ docs/scripts/load-docs.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
+set -x
+
 RELEASES=$(cat RELEASES)
-LATEST=$(head -n 1 RELEASES)
 
-echo ${LATEST}
-
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+ORIGINAL_COMMIT=$(git rev-parse HEAD)
 ROOT_DIR=$(git rev-parse --show-toplevel)
 RELEASES_YAML_FILE=${ROOT_DIR}/docs/data/releases.yaml
+GIT_VERSION=$(git --version)
 
-# Clean up releases.yaml file
+echo "Git version: ${GIT_VERSION}"
+
+echo "Removing data/releases.yaml file"
 rm -f ${RELEASES_YAML_FILE}
 
-# Clean up any already-copied versioned docs
-rm -rf ${ROOT_DIR}/docs/content/docs/v*
+echo "Removing any already-copied versioned docs from the generated folder"
+rm -rf ${ROOT_DIR}/docs/generated/docs/v*
 
 for release in ${RELEASES}; do
     version_docs_dir=${ROOT_DIR}/docs/generated/docs/v${release}
     mkdir -p ${version_docs_dir}
 
-    echo "Copying the documentation for release v${release}"
+    echo "Adding v${release} to releases.yaml"
     echo "- ${release}" >> ${RELEASES_YAML_FILE}
 
+    echo "Checking out tag v${release}"
     git checkout v${release}
 
+    echo "Copying ${ROOT_DIR}/docs/content/docs/* to ${version_docs_dir}/"
     cp ${ROOT_DIR}/docs/content/docs/* ${version_docs_dir}/
 done
 
-git checkout ${CURRENT_BRANCH}
+echo "Returning to commit ${ORIGINAL_COMMIT}"
+git checkout ${ORIGINAL_COMMIT}
+
+echo "Docs loading complete"
