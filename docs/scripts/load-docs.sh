230fae190d5ce8d202030755bd9add5b5015d691
Patrick East <east.patrick@gmail.com>
2019-04-24 18:39:50 -0700

    Move code snippets into docs content and version them
    Previously the code content was only kept in master and was
    Subsequently referenced by the versioned doc contents. This causes
    A few problems.. so we will version it along with the doc content.
    
    To get this to work we make a handful of changes:
    
    * Move the docs up into just opa/docs/content/*, we’re going to remove
      The difference between local/dev and production builds soon.
    * Move the “code” directory into the content dir, it is content tied
      To the markdown files already there.
    * Change the Hugo config to ignore the code directories
    * Change the “code” shortcode to load the versioned code snippet
    * Change load-docs to copy the code directory in addition to the other
      markdown content.
    
    Signed-off-by: Patrick East <east.patrick@gmail.com>

--- docs/scripts/load-docs.sh
+++ docs/scripts/load-docs.sh
@@ -51,16 +51,23 @@ rm -f ${RELEASES_YAML_FILE}
 
 for release in ${RELEASES}; do
     version_docs_dir=${ROOT_DIR}/docs/generated/docs/${release}
+
     mkdir -p ${version_docs_dir}
 
     echo "Adding ${release} to releases.yaml"
     echo "- ${release}" >> ${RELEASES_YAML_FILE}
 
-    echo "Checking out tag ${release}"
+    echo "Copying doc content from tag ${release}"
     git checkout ${release}
 
-    echo "Copying ${ROOT_DIR}/docs/content/docs/* to ${version_docs_dir}/"
-    cp ${ROOT_DIR}/docs/content/docs/* ${version_docs_dir}/
+    # TODO: Remove this check once we are no longer showing docs for v0.10.7 
+    # or older those releases have the docs content in a different location.
+    if [[ -d "${ROOT_DIR}/docs/content/code/" ]]; then
+        # new location
+        cp -r ${ROOT_DIR}/docs/content/* ${version_docs_dir}/
+    else
+        # old location
+        cp -r ${ROOT_DIR}/docs/content/docs/* ${version_docs_dir}/
+        cp -r ${ROOT_DIR}/docs/code ${version_docs_dir}/
+    fi
 done
-
-
