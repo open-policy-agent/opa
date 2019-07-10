40365f843e37c1ef3f03b414f30604215a14fe44
repenno <rapenno@gmail.com>
2019-07-10 09:34:58 -0700

    Add jws encoding built-in functions
    fixes #1174
    
    Signed-off-by: repenno <rapenno@gmail.com>

---
+++ topdown/internal/jwx/jws/verify/hmac.go
@@ -0,0 +1,31 @@
+package verify
+
+import (
+	"crypto/hmac"
+	"github.com/open-policy-agent/opa/topdown/internal/jwx/jwa"
+	"github.com/open-policy-agent/opa/topdown/internal/jwx/jws/sign"
+	"github.com/pkg/errors"
+)
+
+func newHMAC(alg jwa.SignatureAlgorithm) (*HMACVerifier, error) {
+
+	s, err := sign.New(alg)
+	if err != nil {
+		return nil, errors.Wrap(err, `failed to generate HMAC signer`)
+	}
+	return &HMACVerifier{signer: s}, nil
+}
+
+// Verify checks whether the signature for a given input and key is correct
+func (v HMACVerifier) Verify(signingInput, signature []byte, key interface{}) (err error) {
+
+	expected, err := v.signer.Sign(signingInput, key)
+	if err != nil {
+		return errors.Wrap(err, `failed to generated signature`)
+	}
+
+	if !hmac.Equal(signature, expected) {
+		return errors.New(`failed to match hmac signature`)
+	}
+	return nil
+}
