c2d2d1b7fa645959c9a37200a4fdc2f39bf5ce85
Patrick East <east.patrick@gmail.com>
2019-07-17 11:00:27 -0700

    Remove extra character in util/close.go
    Signed-off-by: Patrick East <east.patrick@gmail.com>

--- util/close.go
+++ util/close.go
@@ -1,6 +1,6 @@
 // Copyright 2018 The OPA Authors.  All rights reserved.
 // Use of this source code is governed by an Apache2
-// license that can be found in the LICENSE file.p
+// license that can be found in the LICENSE file.
 
 package util
 
