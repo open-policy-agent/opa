ea7e91f02f6ac5031d1c1579e3f8db2c2f56fc2d
Torin Sandall <torinsandall@gmail.com>
2019-08-25 13:40:41 -0400

    ast, rego: Refactor unsafe built-in handling
    Previously the rego package was modifying the compiler's unsafe
    built-in set each time a query was evaluated or prepared. This
    resulted in concurrent write panics in the server (since the server
    and other callers assume the compiler is immutable and can be shared
    across goroutines.)
    
    These changes update how unsafe built-ins are specified. The query
    compiler continues to inherit the set from the compiler but there are
    two important differences:
    
    1. Callers can provide unsafe built-ins to the query compiler. This
    allows callers to override the behaviour of the compiler if they need
    to.
    
    2. The rego package does not set unsafe built-ins if the compiler is
    provided by the caller. This ensures that the compiler is not modified
    concurrently.
    
    With these changes the rego package doesn't union unsafe built-ins
    like it used to. Since this feature was only added recently it's
    unlikely anyone is relying on it.
    
    Fixes #1666
    
    Signed-off-by: Torin Sandall <torinsandall@gmail.com>

---
+++ test/e2e/concurrency/concurrency_test.go
@@ -0,0 +1,63 @@
+package metrics
+
+import (
+	"flag"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/open-policy-agent/opa/test/e2e"
+)
+
+var testRuntime *e2e.TestRuntime
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	testServerParams := e2e.NewAPIServerTestParams()
+
+	var err error
+	testRuntime, err = e2e.NewTestRuntime(testServerParams)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	os.Exit(testRuntime.RunAPIServerTests(m))
+}
+
+func TestConcurrency(t *testing.T) {
+
+	policy := `
+	package test
+	p = true
+	`
+
+	err := testRuntime.UploadPolicy(t.Name(), strings.NewReader(policy))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			for n := 0; n < 1000; n++ {
+				dr := struct {
+					Result bool `json:"result"`
+				}{}
+				if err := testRuntime.GetDataWithInputTyped("test/p", nil, &dr); err != nil {
+					t.Fatal(err)
+				}
+				if !dr.Result {
+					t.Fatalf("Unexpected response: %+v", dr)
+				}
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+}
