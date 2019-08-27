ac1e318f3f83e875d9e3930ae4d870fa66ed1dd0
Dominik Honnef <dominik@honnef.co>
2019-08-27 10:43:17 +0200

    Add fuzzing
    This is a verbatim copy of github.com/tsandall/fuzz-opa, importing
    fuzzing to the main repository.
    
    Updates #1652
    
    Signed-off-by: Yevgeny Pats <yp@fuzzit.dev>

---
+++ ast/fuzz.go
@@ -0,0 +1,24 @@
+// +build gofuzz
+
+package ast
+
+import "bytes"
+
+func Fuzz(data []byte) int {
+	blacklist := []string{
+		"{{{{{", // nested { and [ cause the parse time to explode
+		"[[[[[",
+	}
+	for i := range blacklist {
+		if bytes.Contains(data, []byte(blacklist[i])) {
+			return -1
+		}
+	}
+	str := string(data)
+	_, _, err := ParseStatements("", str)
+	if err == nil {
+		CompileModules(map[string]string{"": str})
+		return 1
+	}
+	return 0
+}
