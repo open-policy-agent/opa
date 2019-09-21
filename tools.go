1afded8b8d1bfcc80a396ba66baf57a93b749d69
Patrick East <east.patrick@gmail.com>
2019-09-21 09:43:19 -0700

    Add tools.go
    This tricks the module system (with standard methodologies...
    https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md )
    So that we can still use our pinned versions of golang tools in the
    vendor directory.
    
    Signed-off-by: Patrick East <east.patrick@gmail.com>

---
+++ tools.go
@@ -0,0 +1,16 @@
+// +build tools
+
+// Copyright 2019 The OPA Authors.  All rights reserved.
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+// package tools imports require tooling so that
+// the dependencies are tracked with the OPA module
+// and are archived in the vendor directory.
+package tools
+
+import (
+	_ "github.com/golang/lint/golint"
+	_ "github.com/mna/pigeon"
+	_ "golang.org/x/tools/cmd/goimports"
+)
