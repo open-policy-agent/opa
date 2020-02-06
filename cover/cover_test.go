0d5f4e1c1f042581fce320e9e3bb65a544a01ab3
Patrick East <east.patrick@gmail.com>
2020-02-05 16:15:53 -0800

    cover: Exclude `some` expressions in coverage report
    They would previously show up as not-covered in any report. This
    changes to simply omit them. They will never be int the resulting
    covered or not-covered lists, or count against the % covered.
    
    Fixes: #1972
    Signed-off-by: Patrick East <east.patrick@gmail.com>

--- cover/cover_test.go
+++ cover/cover_test.go
@@ -24,6 +24,7 @@ import data.deadbeef # expect not reported
 
 foo {
 	bar
+	p
 	not baz
 }
 
@@ -37,7 +38,14 @@ baz {     # expect no exit
 	true
 	false # expect eval but fail
 	true  # expect not covered
-}`
+}
+
+p {
+	some bar # should not be included in coverage report
+	bar = 1
+	bar + 1 == 2
+}
+`
 
 	parsedModule, err := ast.ParseModule("test.rego", module)
 	if err != nil {
@@ -67,16 +75,18 @@ baz {     # expect no exit
 	}
 
 	expectedCovered := []Position{
-		{5},      // foo head
-		{6}, {7}, // foo body
-		{10},             // bar head
-		{11}, {12}, {13}, // bar body
-		{17}, {18}, // baz body hits
+		{5},           // foo head
+		{6}, {7}, {8}, // foo body
+		{11},             // bar head
+		{12}, {13}, {14}, // bar body
+		{18}, {19}, // baz body hits
+		{23},       // p head
+		{25}, {26}, // p body
 	}
 
 	expectedNotCovered := []Position{
-		{16}, // baz head
-		{19}, // baz body miss
+		{17}, // baz head
+		{20}, // baz body miss
 	}
 
 	for _, exp := range expectedCovered {
