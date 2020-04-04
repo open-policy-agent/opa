29387124470aa15431c33fd63173dfc97861cc17
Rene Klootwijk <rene.klootwijk@rencon.nl>
2020-04-04 22:20:18 +0200

    A built-in command to create a version 4 uuid
    Signed-off-by: Rene Klootwijk <rene.klootwijk@rencon.nl>
    
    A built-in command to create a version 4 uuid
    
    Signed-off-by: Rene Klootwijk <rene.klootwijk@rencon.nl>
    
    A built-in command to create a version 4 uuid
    
    Signed-off-by: Rene Klootwijk <rene.klootwijk@rencon.nl>
    
    Removed external dependency and created helper for uuid
    
    Signed-off-by: Rene Klootwijk <rene.klootwijk@rencon.nl>
    
    Added idempotency for the uuid.rfc4122() command
    
    Signed-off-by: Rene Klootwijk <rene.klootwijk@rencon.nl>

---
+++ internal/uuid/uuid_test.go
@@ -0,0 +1,21 @@
+// Copyright 2017 The OPA Authors.  All rights reserved.
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+package uuid
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestUUID4(t *testing.T) {
+	uuid, err := New(bytes.NewReader(make([]byte, 16, 16)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expect := "00000000-0000-4000-8000-000000000000"
+	if uuid != expect {
+		t.Errorf("Expected %q, got %q", expect, uuid)
+	}
+}
