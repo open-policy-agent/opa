a36e1248428314e2fb0c1f1a54b1d8b4acbafef8
Patrick East <east.patrick@gmail.com>
2020-04-09 16:09:13 -0700

    topdown/copypropagation: unionfind now uses ast.Value
    The underlying code will work in the same way, it doesn't affect the
    algorithm. We just were limiting to ast.Var before because of the
    initial use case only wanting to group variables.
    
    Signed-off-by: Patrick East <east.patrick@gmail.com>

--- topdown/copypropagation/unionfind_test.go
+++ topdown/copypropagation/unionfind_test.go
@@ -52,46 +52,35 @@ func TestUnionFindMakeSet(t *testing.T) {
 
 	tests := []struct {
 		name    string
-		v       ast.Var
+		v       ast.Value
 		result  *unionFindRoot
-		parents map[ast.Var]ast.Var
-		roots   map[ast.Var]*unionFindRoot
+		parents map[ast.Value]ast.Value
+		roots   map[ast.Value]*unionFindRoot
 	}{
 		{
 			name:   "from empty",
 			v:      ast.Var("a"),
 			result: &unionFindRoot{key: ast.Var("a")},
-			parents: map[ast.Var]ast.Var{
-				ast.Var("a"): ast.Var("a"),
-			},
-			roots: map[ast.Var]*unionFindRoot{
-				ast.Var("a"): {key: ast.Var("a")},
-			},
 		},
 		{
 			name:   "add another var",
 			v:      ast.Var("b"),
 			result: &unionFindRoot{key: ast.Var("b")},
-			parents: map[ast.Var]ast.Var{
-				ast.Var("a"): ast.Var("a"),
-				ast.Var("b"): ast.Var("b"),
-			},
-			roots: map[ast.Var]*unionFindRoot{
-				ast.Var("a"): {key: ast.Var("a")},
-				ast.Var("b"): {key: ast.Var("b")},
-			},
 		},
 		{
 			name:   "add existing",
 			v:      ast.Var("b"),
 			result: &unionFindRoot{key: ast.Var("b")},
-			parents: map[ast.Var]ast.Var{
-				ast.Var("a"): ast.Var("a"),
-				ast.Var("b"): ast.Var("b"),
-			},
-			roots: map[ast.Var]*unionFindRoot{
-				ast.Var("b"): {key: ast.Var("b")},
-			},
+		},
+		{
+			name:   "add ref",
+			v:      ast.Ref{ast.StringTerm("foo")},
+			result: &unionFindRoot{key: ast.Ref{ast.StringTerm("foo")}},
+		},
+		{
+			name:   "add ref existing",
+			v:      ast.Ref{ast.StringTerm("foo")},
+			result: &unionFindRoot{key: ast.Ref{ast.StringTerm("foo")}},
 		},
 	}
 	for _, tc := range tests {
@@ -100,172 +89,109 @@ func TestUnionFindMakeSet(t *testing.T) {
 			if !reflect.DeepEqual(actual, tc.result) {
 				t.Errorf("MakeSet(%v) = %v, expected %v", tc.v, actual, tc.result)
 			}
-			if !reflect.DeepEqual(tc.parents, uf.parents) {
-				t.Errorf("uf.parents = %v, expected %v", uf.parents, tc.parents)
-			}
-			if !reflect.DeepEqual(tc.parents, uf.parents) {
-				t.Errorf("uf.roots = %v, expected %v", uf.roots, tc.roots)
-			}
 		})
 	}
 }
 
-func TestUnionFindFind(t *testing.T) {
-	tests := []struct {
-		name          string
-		uf            *unionFind
-		v             ast.Var
-		expectedRoot  *unionFindRoot
-		expectedFound bool
-	}{
-		{
-			name:          "empty uf",
-			uf:            newUnionFind(nil),
-			v:             ast.Var("a"),
-			expectedRoot:  nil,
-			expectedFound: false,
-		},
-		{
-			name: "is parent",
-			uf: &unionFind{
-				parents: map[ast.Var]ast.Var{
-					ast.Var("a"): ast.Var("a"),
-				},
-				roots: map[ast.Var]*unionFindRoot{
-					ast.Var("a"): newUnionFindRoot("a"),
-				},
-			},
-			v:             ast.Var("a"),
-			expectedRoot:  newUnionFindRoot("a"),
-			expectedFound: true,
-		},
-		{
-			name: "find parent",
-			uf: &unionFind{
-				parents: map[ast.Var]ast.Var{
-					ast.Var("a"): ast.Var("b"),
-					ast.Var("b"): ast.Var("c"),
-					ast.Var("c"): ast.Var("d"),
-					ast.Var("d"): ast.Var("d"),
-				},
-				roots: map[ast.Var]*unionFindRoot{
-					ast.Var("d"): newUnionFindRoot("d"),
-				},
-			},
-			v:             ast.Var("a"),
-			expectedRoot:  newUnionFindRoot("d"),
-			expectedFound: true,
-		},
+func TestUnionFindFindEmptyUF(t *testing.T) {
+	uf := newUnionFind(noopUnionFindRank)
+	actual, found := uf.Find(ast.Var("a"))
+	if found || actual != nil {
+		t.Error("Expected Find() to return (nil, false)")
 	}
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
+}
 
-			actualRoot, actualFound := tc.uf.Find(tc.v)
-			if !reflect.DeepEqual(actualRoot, tc.expectedRoot) || actualFound != tc.expectedFound {
-				t.Errorf("Find(%v) got = (%v, %v), expected (%v, %v)", tc.v, actualRoot, actualFound, tc.expectedRoot, tc.expectedFound)
-			}
-		})
+func TestUnionFindFindIsParent(t *testing.T) {
+	uf := newUnionFind(noopUnionFindRank)
+
+	uf.MakeSet(ast.Var("a")) // "a" will have a parent "a"
+
+	actual, found := uf.Find(ast.Var("a"))
+
+	expected := newUnionFindRoot(ast.Var("a"))
+	if !found || actual.Value().Compare(expected.Value()) != 0 {
+		t.Errorf("Expected Find() to return (true, %+v)", expected)
+	}
+}
+
+func TestUnionFindFindParent(t *testing.T) {
+	fooBarRef := ast.Ref{ast.StringTerm("foo"), ast.StringTerm("bar"), ast.VarTerm("x")}
+	call := ast.Call{ast.RefTerm(ast.VarTerm("gt")), ast.NumberTerm("1"), ast.VarTerm("x")}
+
+	uf := newUnionFind(noopUnionFindRank)
+	uf.Merge(ast.Var("a"), ast.Var("b"))
+	uf.Merge(ast.Var("b"), ast.Var("c"))
+	uf.Merge(ast.Var("c"), fooBarRef)
+	uf.Merge(fooBarRef, ast.Var("d"))
+	uf.Merge(ast.Var("d"), call)
+	uf.Merge(call, ast.Var("e"))
+
+	actual, found := uf.Find(ast.Var("e"))
+
+	expected := newUnionFindRoot(ast.Var("a"))
+	if !found || actual.Value().Compare(expected.Value()) != 0 {
+		t.Errorf("Expected Find() to return (true, %+v)", expected)
 	}
 }
 
 func TestUnionFindMerge(t *testing.T) {
-	uf := newUnionFind(func(a *unionFindRoot, b *unionFindRoot) (*unionFindRoot, *unionFindRoot) {
-		return a, b
-	})
+	uf := newUnionFind(noopUnionFindRank)
 
 	tests := []struct {
 		name    string
-		a       ast.Var
-		b       ast.Var
+		a       ast.Value
+		b       ast.Value
 		result  *unionFindRoot
-		parents map[ast.Var]ast.Var
-		roots   map[ast.Var]*unionFindRoot
+		parents map[ast.Value]ast.Value
+		roots   map[ast.Value]*unionFindRoot
 	}{
 		{
 			name:   "empty uf",
 			a:      ast.Var("a"),
 			b:      ast.Var("b"),
-			result: newUnionFindRoot("a"),
-			parents: map[ast.Var]ast.Var{
-				ast.Var("a"): ast.Var("a"),
-				ast.Var("b"): ast.Var("a"),
-			},
-			roots: map[ast.Var]*unionFindRoot{
-				ast.Var("a"): {key: ast.Var("a")},
-			},
+			result: newUnionFindRoot(ast.Var("a")),
 		},
 		{
 			name:   "same values",
 			a:      ast.Var("a"),
 			b:      ast.Var("a"),
-			result: newUnionFindRoot("a"),
-			parents: map[ast.Var]ast.Var{
-				ast.Var("a"): ast.Var("a"),
-				ast.Var("b"): ast.Var("a"),
-			},
-			roots: map[ast.Var]*unionFindRoot{
-				ast.Var("a"): {key: ast.Var("a")},
-			},
+			result: newUnionFindRoot(ast.Var("a")),
 		},
 		{
 			name:   "same values higher rank result",
 			a:      ast.Var("b"),
 			b:      ast.Var("b"),
-			result: newUnionFindRoot("a"),
-			parents: map[ast.Var]ast.Var{
-				ast.Var("a"): ast.Var("a"),
-				ast.Var("b"): ast.Var("a"),
-			},
-			roots: map[ast.Var]*unionFindRoot{
-				ast.Var("a"): {key: ast.Var("a")},
-			},
+			result: newUnionFindRoot(ast.Var("a")),
 		},
 		{
 			name:   "transitive",
 			a:      ast.Var("b"),
 			b:      ast.Var("c"),
-			result: newUnionFindRoot("a"),
-			parents: map[ast.Var]ast.Var{
-				ast.Var("a"): ast.Var("a"),
-				ast.Var("b"): ast.Var("a"),
-				ast.Var("c"): ast.Var("a"),
-			},
-			roots: map[ast.Var]*unionFindRoot{
-				ast.Var("a"): {key: ast.Var("a")},
-			},
+			result: newUnionFindRoot(ast.Var("a")),
 		},
 		{
 			name:   "new roots",
 			a:      ast.Var("d"),
 			b:      ast.Var("e"),
-			result: newUnionFindRoot("d"),
-			parents: map[ast.Var]ast.Var{
-				ast.Var("a"): ast.Var("a"),
-				ast.Var("b"): ast.Var("a"),
-				ast.Var("c"): ast.Var("a"),
-				ast.Var("d"): ast.Var("d"),
-				ast.Var("e"): ast.Var("d"),
-			},
-			roots: map[ast.Var]*unionFindRoot{
-				ast.Var("a"): {key: ast.Var("a")},
-				ast.Var("d"): {key: ast.Var("d")},
-			},
+			result: newUnionFindRoot(ast.Var("d")),
 		},
 		{
 			name:   "combine roots",
 			a:      ast.Var("a"),
 			b:      ast.Var("e"),
-			result: newUnionFindRoot("a"),
-			parents: map[ast.Var]ast.Var{
-				ast.Var("a"): ast.Var("a"),
-				ast.Var("b"): ast.Var("a"),
-				ast.Var("c"): ast.Var("a"),
-				ast.Var("d"): ast.Var("a"),
-				ast.Var("e"): ast.Var("d"),
-			},
-			roots: map[ast.Var]*unionFindRoot{
-				ast.Var("a"): {key: ast.Var("a")},
-			},
+			result: newUnionFindRoot(ast.Var("a")),
+		},
+		{
+			name:   "new ref roots",
+			a:      ast.Ref{ast.StringTerm("foo"), ast.StringTerm("bar")},
+			b:      ast.Var("x"),
+			result: newUnionFindRoot(ast.Ref{ast.StringTerm("foo"), ast.StringTerm("bar")}),
+		},
+		{
+			name:   "combine ref roots",
+			a:      ast.Var("b"),
+			b:      ast.Ref{ast.StringTerm("foo"), ast.StringTerm("bar")},
+			result: newUnionFindRoot(ast.Var("a")),
 		},
 	}
 	for _, tc := range tests {
@@ -274,12 +200,10 @@ func TestUnionFindMerge(t *testing.T) {
 			if !reflect.DeepEqual(actualRoot, tc.result) || !canMerge {
 				t.Errorf("Merge(%v, %v) got = (%v, %v), expected (%v, true)", tc.a, tc.b, actualRoot, canMerge, tc.result)
 			}
-			if !reflect.DeepEqual(tc.parents, uf.parents) {
-				t.Errorf("uf.parents = %v, expected %v", uf.parents, tc.parents)
-			}
-			if !reflect.DeepEqual(tc.parents, uf.parents) {
-				t.Errorf("uf.roots = %v, expected %v", uf.roots, tc.roots)
-			}
 		})
 	}
+}
+
+var noopUnionFindRank = func(a *unionFindRoot, b *unionFindRoot) (*unionFindRoot, *unionFindRoot) {
+	return a, b
 }
