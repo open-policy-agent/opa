open-policy-agent/opa

commit fd79a7e0c746b30866471a004a78468c4b0c1484

cmd/fmt: Always write to stdout
There was an explicit check that would prevent writing to stdout if
the formatting didn't change anything.

This makes sense for the "write to file" or "diff" options as we can
save work by bailing out early, but if configured to write the file
contents to stdout we should do so in all cases.

Fixes: #2235
Signed-off-by: Patrick East <east.patrick@gmail.com>

cmd/fmt.go
@@ -106,10 +106,6 @@ func formatFile(filename string, info os.FileInfo, err error) error {
 		return newError("failed to parse Rego source file: %v", err)
 	}
 
-	if bytes.Equal(formatted, contents) {
-		return nil
-	}
-
 	var out io.Writer = os.Stdout
 	if fmtParams.list {
 		fmt.Fprintln(out, filename)
@@ -158,15 +154,15 @@ func formatStdin(r io.Reader, w io.Writer) error {
 		return err
 	}
 
-	if !bytes.Equal(formatted, contents) {
-		_, err := w.Write(formatted)
-		return err
-	}
-
-	return nil
+	_, err = w.Write(formatted)
+	return err
 }
 
 func doDiff(old, new []byte) (stdout, stderr bytes.Buffer, err error) {
+	if bytes.Equal(old, new) {
+		return stdout, stderr, nil
+	}
+
 	o, err := ioutil.TempFile("", ".opafmt")
 	if err != nil {
 		return stdout, stderr, err

commit 273d947ef9e31ccaffce3de6f22c256e203a0cf8

cmd/fmt: Only list files if there were changes
Fixes an issue introduced in https://github.com/open-policy-agent/opa/pull/2249
where we would now always list the filename, regardless of whether or
not the file had changes.

This corrects the behavior to only do it when there are changes in the
formatted output and the original content.

Fixes: #2295
Signed-off-by: Patrick East <east.patrick@gmail.com>

cmd/fmt.go
@@ -19,11 +19,13 @@ import (
 	fileurl "github.com/open-policy-agent/opa/internal/file/url"
 )
 
-var fmtParams = struct {
+type fmtCommandParams struct {
 	overwrite bool
 	list      bool
 	diff      bool
-}{}
+}
+
+var fmtParams = fmtCommandParams{}
 
 var formatCommand = &cobra.Command{
 	Use:   "fmt [path [...]]",
@@ -67,8 +69,10 @@ func opaFmt(args []string) int {
 			fmt.Fprintln(os.Stderr, err)
 			return 1
 		}
-
-		if err := filepath.Walk(filename, formatFile); err != nil {
+		err = filepath.Walk(filename, func(path string, info os.FileInfo, err error) error {
+			return formatFile(&fmtParams, os.Stdout, path, info, err)
+		})
+		if err != nil {
 			switch err := err.(type) {
 			case fmtError:
 				fmt.Fprintln(os.Stderr, err.msg)
@@ -83,7 +87,7 @@ func opaFmt(args []string) int {
 	return 0
 }
 
-func formatFile(filename string, info os.FileInfo, err error) error {
+func formatFile(params *fmtCommandParams, out io.Writer, filename string, info os.FileInfo, err error) error {
 	if err != nil {
 		return err
 	}
@@ -106,26 +110,29 @@ func formatFile(filename string, info os.FileInfo, err error) error {
 		return newError("failed to parse Rego source file: %v", err)
 	}
 
-	var out io.Writer = os.Stdout
-	if fmtParams.list {
-		fmt.Fprintln(out, filename)
-		out = ioutil.Discard
-	}
+	changed := !bytes.Equal(contents, formatted)
 
-	if fmtParams.diff {
-		stdout, stderr, err := doDiff(contents, formatted)
-		if err != nil && stdout.Len() == 0 {
-			fmt.Fprintln(os.Stderr, stderr.String())
-			return newError("failed to diff formatting: %v", err)
+	if params.list {
+		if changed {
+			fmt.Fprintln(out, filename)
 		}
-
-		fmt.Fprintln(out, stdout.String())
-
-		// If we called diff, we shouldn't output to stdout.
-		out = ioutil.Discard
+		return nil
 	}
 
-	if fmtParams.overwrite {
+	if params.diff {
+		if changed {
+			stdout, stderr, err := doDiff(contents, formatted)
+			if err != nil && stdout.Len() == 0 {
+				fmt.Fprintln(os.Stderr, stderr.String())
+				return newError("failed to diff formatting: %v", err)
+			}
+
+			fmt.Fprintln(out, stdout.String())
+		}
+		return nil
+	}
+
+	if params.overwrite {
 		outfile, err := os.OpenFile(filename, os.O_WRONLY|os.O_TRUNC, info.Mode().Perm())
 		if err != nil {
 			return newError("failed to open file for writing: %v", err)
@@ -159,10 +166,6 @@ func formatStdin(r io.Reader, w io.Writer) error {
 }
 
 func doDiff(old, new []byte) (stdout, stderr bytes.Buffer, err error) {
-	if bytes.Equal(old, new) {
-		return stdout, stderr, nil
-	}
-
 	o, err := ioutil.TempFile("", ".opafmt")
 	if err != nil {
 		return stdout, stderr, err

cmd/fmt_test.go
@@ -1,0 +1,155 @@
+package cmd
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/open-policy-agent/opa/util/test"
+)
+
+func TestFmtFormatFile(t *testing.T) {
+	params := fmtCommandParams{}
+	var stdout bytes.Buffer
+
+	unformatted := `
+	package test
+	
+	p { a == 1; true
+		1 +    3
+	}
+
+	
+	`
+
+	formatted := `package test
+
+p {
+	a == 1
+	true
+	1 + 3
+}
+`
+
+	files := map[string]string{
+		"policy.rego": unformatted,
+	}
+
+	test.WithTempFS(files, func(path string) {
+		policyFile := filepath.Join(path, "policy.rego")
+		info, err := os.Stat(policyFile)
+		err = formatFile(&params, &stdout, policyFile, info, err)
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		actual := stdout.String()
+		if actual != formatted {
+			t.Fatalf("Expected:%s\n\nGot:\n%s\n\n", formatted, actual)
+		}
+	})
+}
+
+func TestFmtFormatFileNoChanges(t *testing.T) {
+	params := fmtCommandParams{}
+	var stdout bytes.Buffer
+
+	policyContent := `package test
+
+p {
+	a == 1
+	true
+	1 + 3
+}
+`
+
+	files := map[string]string{
+		"policy.rego": policyContent,
+	}
+
+	test.WithTempFS(files, func(path string) {
+		policyFile := filepath.Join(path, "policy.rego")
+		info, err := os.Stat(policyFile)
+		err = formatFile(&params, &stdout, policyFile, info, err)
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		actual := stdout.String()
+		if actual != policyContent {
+			t.Fatalf("Expected:%s\n\nGot:\n%s\n\n", policyContent, actual)
+		}
+	})
+}
+
+func TestFmtFormatFileDiff(t *testing.T) {
+	params := fmtCommandParams{
+		diff: true,
+	}
+	var stdout bytes.Buffer
+
+	policyContent := `package test
+
+p {
+	a == 1
+	true
+	1 + 3
+}
+`
+
+	files := map[string]string{
+		"policy.rego": policyContent,
+	}
+
+	test.WithTempFS(files, func(path string) {
+		policyFile := filepath.Join(path, "policy.rego")
+		info, err := os.Stat(policyFile)
+		err = formatFile(&params, &stdout, policyFile, info, err)
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		actual := stdout.String()
+
+		if len(actual) > 0 {
+			t.Fatalf("Expected no output, got:\n%s\n\n", actual)
+		}
+	})
+}
+
+func TestFmtFormatFileList(t *testing.T) {
+	params := fmtCommandParams{
+		list: true,
+	}
+	var stdout bytes.Buffer
+
+	policyContent := `package test
+
+p {
+	a == 1
+	true
+	1 + 3
+}
+`
+
+	files := map[string]string{
+		"policy.rego": policyContent,
+	}
+
+	test.WithTempFS(files, func(path string) {
+		policyFile := filepath.Join(path, "policy.rego")
+		info, err := os.Stat(policyFile)
+		err = formatFile(&params, &stdout, policyFile, info, err)
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		actual := strings.TrimSpace(stdout.String())
+
+		if len(actual) > 0 {
+			t.Fatalf("Expected no output, got:\n%s\n\n", actual)
+		}
+	})
+}