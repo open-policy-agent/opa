376aaabb6feb9c7f5b7e95d74be0673005353c6b
Alan Silva <alansq16@gmail.com>
2020-08-05 21:39:11 -0400

    Updating deps command to use addDataFlag and addBundleFlag
    Signed-off-by: Alan Silva <alansq16@gmail.com>

--- cmd/deps.go
+++ cmd/deps.go
@@ -57,9 +57,9 @@ func init() {
 	}
 
 	depsCommand.Flags().VarP(params.format, "format", "f", "set output format")
-	depsCommand.Flags().VarP(&params.dataPaths, "data", "d", "set policy or data file(s). This flag can be repeated.")
-	depsCommand.Flags().VarP(&params.bundlePaths, "bundle", "b", "set bundle file(s) or directory path(s). This flag can be repeated.")
 	addIgnoreFlag(depsCommand.Flags(), &params.ignore)
+	addDataFlag(depsCommand.Flags(), &params.dataPaths)
+	addBundleFlag(depsCommand.Flags(), &params.bundlePaths)
 
 	RootCommand.AddCommand(depsCommand)
 }
