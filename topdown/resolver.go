d1c2173ca28ab00e0516ec6b217c1b55d5cec59e
Torin Sandall <torinsandall@gmail.com>
2020-08-18 12:49:57 -0400

    Add external resolvers API
    This allows for resolving refs outside of the topdown eval or store.
    
    Signed-off-by: Patrick East <east.patrick@gmail.com>

---
+++ topdown/resolver.go
@@ -0,0 +1,76 @@
+// Copyright 2020 The OPA Authors.  All rights reserved.
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+package topdown
+
+import (
+	"context"
+
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/resolver"
+)
+
+type resolverTrie struct {
+	r        resolver.Resolver
+	children map[ast.Value]*resolverTrie
+}
+
+func newResolverTrie() *resolverTrie {
+	return &resolverTrie{children: map[ast.Value]*resolverTrie{}}
+}
+
+func (t *resolverTrie) Put(ref ast.Ref, r resolver.Resolver) {
+	node := t
+	for _, t := range ref {
+		child, ok := node.children[t.Value]
+		if !ok {
+			child = &resolverTrie{children: map[ast.Value]*resolverTrie{}}
+			node.children[t.Value] = child
+		}
+		node = child
+	}
+	node.r = r
+}
+
+func (t *resolverTrie) Resolve(ctx context.Context, ref ast.Ref, input *ast.Term) (ast.Value, error) {
+	in := resolver.Input{
+		Ref:   ref,
+		Input: input,
+	}
+	node := t
+	for i, t := range ref {
+		child, ok := node.children[t.Value]
+		if !ok {
+			return nil, nil
+		}
+		node = child
+		if node.r != nil {
+			result, err := node.r.Eval(ctx, in)
+			if err != nil {
+				return nil, err
+			}
+			return result.Value.Find(ref[i+1:])
+		}
+	}
+	return node.mktree(ctx, in)
+}
+
+func (t *resolverTrie) mktree(ctx context.Context, in resolver.Input) (ast.Value, error) {
+	if t.r != nil {
+		result, err := t.r.Eval(ctx, in)
+		if err != nil {
+			return nil, err
+		}
+		return result.Value, nil
+	}
+	obj := ast.NewObject()
+	for k, child := range t.children {
+		v, err := child.mktree(ctx, in)
+		if err != nil {
+			return nil, err
+		}
+		obj.Insert(ast.NewTerm(k), ast.NewTerm(v))
+	}
+	return obj, nil
+}
