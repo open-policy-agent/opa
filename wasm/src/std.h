57657b43118fe9bcb29a9f5e9417c52bf675ce24
Teemu Koponen <koponen@styra.com>
2020-10-19 15:50:34 -0700

    wasm: Simplify the container_of macro.
    Signed-off-by: Teemu Koponen <koponen@styra.com>

--- wasm/src/std.h
+++ wasm/src/std.h
@@ -7,9 +7,8 @@
 extern "C" {
 #endif
 
-#define container_of(ptr, type, member) ({ \
-    const typeof( ((type *)0)->member ) *__mptr = (ptr); \
-    (type *)( (char *)__mptr - offsetof(type,member) ); })
+#define container_of(ptr, type, member)                             \
+    ((type *)(void *)( ((char *)(ptr) - offsetof(type, member) )))
 
 void opa_abort(const char *msg);
 void opa_println(const char *msg);
