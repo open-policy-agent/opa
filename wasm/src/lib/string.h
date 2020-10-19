0a3ed038cba3f876b1fb991f9ff51f3451cb99e5
Teemu Koponen <koponen@styra.com>
2020-10-19 10:59:06 -0700

    wasm: Minimal C++ standard library support.
    This provides:
    
    1) no-op implementations for std::atomic and std::mutex, useful in
       compiling code that depends on them without explicit no-threading
       mode.
    
    2) explicit template instantiations for portions of std::string,
       std::vector, and std::sort that are not part of the C++ standard
       library headers. This is essential to avoid the linking to the C++
       library.
    
    3) std::__call_once and std::__next_prime functions from the C++
       library to avoid the linking.
    
    Finally, this adds declarations for various libc functions the C++
    standard library depends on. If one were to use these particular
    portions of the library, function implementations need to be added.
    
    Signed-off-by: Teemu Koponen <koponen@styra.com>

--- wasm/src/lib/string.h
+++ wasm/src/lib/string.h
@@ -12,9 +12,26 @@ int memcmp(const void *s1, const void *s2, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
-char *strcpy(char *dest, const char *src);
 char *strchr(const char *s, int c);
 size_t strlen(const char *s);
+
+// not implemented:
+
+char *strcat(char *dest, const char *src);
+int strcmp(const char *s1, const char *s2);
+int strcoll(const char *s1, const char *s2);
+char *strcpy(char *dest, const char *src);
+size_t strcspn(const char *s, const char *reject);
+char *strerror(int errnum);
+char *strncat(char *dest, const char *src, size_t n);
+int strncmp(const char *s1, const char *s2, size_t n);
+char *strncpy(char *dest, const char *src, size_t n);
+char *strpbrk(const char *s, const char *accept);
+char *strrchr(const char *s, int c);
+size_t strspn(const char *s, const char *accept);
+char *strstr(const char *haystack, const char *needle);
+char *strtok(char *str, const char *delim);
+size_t strxfrm(char *dest, const char *src, size_t n);
 
 #ifdef __cplusplus
 }
--- wasm/src/unicode.h
+++ wasm/src/unicode.h
@@ -1,6 +1,10 @@
 #ifndef OPA_UNICODE_H
 #define OPA_UNICODE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int opa_unicode_decode_surrogate(int codepoint1, int codepoint2);
 int opa_unicode_decode_unit(const char *in, int i, int len);
 int opa_unicode_decode_utf8(const char *in, int i, int len, int *olen);
@@ -10,5 +14,9 @@ int opa_unicode_last_utf8(const char *in, int start, int end);
 int opa_unicode_surrogate(int codepoint);
 int opa_unicode_to_lower(int codepoint);
 int opa_unicode_to_upper(int codepoint);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
