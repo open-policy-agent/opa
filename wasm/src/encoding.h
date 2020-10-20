914aec4686c125fd92966e1d71aa838f2cb2bd35
Teemu Koponen <koponen@styra.com>
2020-10-20 11:34:55 -0700

    wasm: base64 builtins.
    Signed-off-by: Teemu Koponen <koponen@styra.com>

---
+++ wasm/src/encoding.h
@@ -0,0 +1,12 @@
+#ifndef OPA_ENCODING_H
+#define OPA_ENCODING_H
+
+#include "value.h"
+
+opa_value *opa_base64_is_valid(opa_value *a);
+opa_value *opa_base64_decode(opa_value *a);
+opa_value *opa_base64_encode(opa_value *a);
+opa_value *opa_base64_url_decode(opa_value *a);
+opa_value *opa_base64_url_encode(opa_value *a);
+
+#endif
