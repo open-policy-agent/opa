250ee9631ed1e31502911c17698570bb69eec966
Patrick East <east.patrick@gmail.com>
2020-10-28 14:30:46 -0700

    ci: Auto generate and commit wasm binaries
    Rather than force PR's to include the generated wasm binaries we can
    accept changes which require regenerating them. The post merge
    workflow will now generate them and commit+push the new binaries.
    
    The PR check for generated changes includes new functionality to
    exclude files, the first ones being the wasm generated ones.
    
    Signed-off-by: Patrick East <east.patrick@gmail.com>

---
+++ build/commit-wasm-bins.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+OPA_DIR=$(dirname "${BASH_SOURCE}")/..
+
+cd ${OPA_DIR}
+
+WASMFILES=(
+  "internal/compiler/wasm/opa/opa.go"
+  "internal/compiler/wasm/opa/opa.wasm"
+)
+
+for file in "${WASMFILES[@]}"; do
+  git add ${file}
+done
+
+if [[ -z "$(git diff --name-only --cached)" ]]; then
+  echo "No Wasm changes to commit!"
+  exit 1
+fi
+
+git commit -m "wasm: Update generated binaries"
+
+echo ""
+echo "Committed changes for files:"
+git diff-tree --no-commit-id --name-only -r HEAD
+echo ""
