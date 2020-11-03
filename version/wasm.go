7668650892e9648755883e4cda53ef44b808f9e1
Torin Sandall <torinsandall@gmail.com>
2020-11-03 11:39:38 -0500

    resolver: Refactor wasmer dependency to be conditional
    This commit updates the resolver package to make the wasmer dependency
    conditional at build-time. If CGO is enabled then the wasmer
    dependency will be included. If CGO is not enabled, the wasmer
    dependency will be excluded and the OPA binary will not be able to run
    wasm-compiled policies. This allows us to continue building statically
    linked OPA executables that can be distributed and used.
    
    Signed-off-by: Torin Sandall <torinsandall@gmail.com>

---
+++ version/wasm.go
@@ -0,0 +1,10 @@
+// Copyright 2020 The OPA Authors.  All rights reserved.
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+// +build cgo
+
+package version
+
+// WasmRuntimeAvailable indicates if a wasm runtime is available in this OPA.
+const WasmRuntimeAvailable = true
