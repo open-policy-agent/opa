a258fdb2f56caa3f0643b0e07584fcb05f9a5d48
Torin Sandall <torinsandall@gmail.com>
2020-12-03 10:12:00 -0500

    Makefile: Future proof the wasm runtime flag
    Signed-off-by: Torin Sandall <torinsandall@gmail.com>

--- build/run-goimports.sh
+++ build/run-goimports.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-GOFLAGS=-mod=vendor GO111MODULE=on GOOS="" GOARCH="" go run -tags=wasmer ./vendor/golang.org/x/tools/cmd/goimports -local github.com/open-policy-agent/opa $@
+GOFLAGS=-mod=vendor GO111MODULE=on GOOS="" GOARCH="" go run -tags=opa_wasm ./vendor/golang.org/x/tools/cmd/goimports -local github.com/open-policy-agent/opa $@
