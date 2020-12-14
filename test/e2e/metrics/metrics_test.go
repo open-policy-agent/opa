58cf41f4c669b7b1b471745f7c6c69db1288576b
Stephan Renatus <stephan.renatus@gmail.com>
2020-12-14 11:08:14 +0100

    server: pass instrumentation via rego.EvalInstrument
    Fixes #3000.
    
    The assertions on the response metrics object should be enough to
    cover the bug -- depending on what is happening during eval, the
    keys of that object may differ. (E.g. if there's a ref to be resolved,
    that operation is timed; if there are none, there's no timer data.)
    
    Small change to test/e2e: close some request bodies
    
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- test/e2e/metrics/metrics_test.go
+++ test/e2e/metrics/metrics_test.go
@@ -75,3 +75,117 @@ func TestMetricsEndpoint(t *testing.T) {
 		}
 	}
 }
+
+type response struct {
+	Result  bool                   `json:"result"`
+	Metrics map[string]interface{} `json:"metrics"`
+}
+
+func TestRequestWithInstrumentationV1DataAPI(t *testing.T) {
+
+	policy := `
+	package test
+	p = true
+	q = true
+	`
+
+	err := testRuntime.UploadPolicy(t.Name(), strings.NewReader(policy))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var resp response
+	if err := testRuntime.GetDataWithInputTyped("test/p?instrument", nil, &resp); err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp.Result {
+		t.Fatalf("Unexpected response: %+v", resp)
+	}
+
+	assertInstrumentationMetricsInMap(t, true, resp.Metrics)
+
+	// run another request, this should re-use the compiled query
+	var resp2 response
+	if err := testRuntime.GetDataWithInputTyped("test/p?instrument", nil, &resp2); err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp2.Result {
+		t.Fatalf("Unexpected response: %+v", resp2)
+	}
+
+	assertInstrumentationMetricsInMap(t, false, resp2.Metrics)
+
+	// GET data endpoint
+	var resp3 response
+	if err := testRuntime.GetDataWithInputTyped("test/q?instrument", nil, &resp3); err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp.Result {
+		t.Fatalf("Unexpected response: %+v", resp3)
+	}
+
+	assertInstrumentationMetricsInMap(t, true, resp3.Metrics)
+
+	// 2nd GET data endpoint
+	var resp4 response
+	if err := testRuntime.GetDataWithInputTyped("test/q?instrument", nil, &resp4); err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp.Result {
+		t.Fatalf("Unexpected response: %+v", resp4)
+	}
+
+	assertInstrumentationMetricsInMap(t, false, resp4.Metrics)
+}
+
+func assertInstrumentationMetricsInMap(t *testing.T, includeCompile bool, metrics map[string]interface{}) {
+	expectedKeys := []string{
+		"counter_server_query_cache_hit",
+		"counter_eval_op_virtual_cache_miss",
+		"histogram_eval_op_plug",
+		"timer_eval_op_plug_ns",
+		"timer_rego_input_parse_ns",
+		"timer_rego_query_eval_ns",
+		"timer_server_handler_ns",
+	}
+	compileStageKeys := []string{
+		"timer_rego_query_parse_ns",
+		"timer_rego_query_compile_ns",
+		"timer_query_compile_stage_build_comprehension_index_ns",
+		"timer_query_compile_stage_check_safety_ns",
+		"timer_query_compile_stage_check_types_ns",
+		"timer_query_compile_stage_check_undefined_funcs_ns",
+		"timer_query_compile_stage_check_unsafe_builtins_ns",
+		"timer_query_compile_stage_resolve_refs_ns",
+		"timer_query_compile_stage_rewrite_comprehension_terms_ns",
+		"timer_query_compile_stage_rewrite_dynamic_terms_ns",
+		"timer_query_compile_stage_rewrite_expr_terms_ns",
+		"timer_query_compile_stage_rewrite_local_vars_ns",
+		"timer_query_compile_stage_rewrite_to_capture_value_ns",
+		"timer_query_compile_stage_rewrite_with_values_ns",
+	}
+
+	if includeCompile {
+		expectedKeys = append(expectedKeys, compileStageKeys...)
+	}
+
+	for _, key := range expectedKeys {
+		if metrics[key] == nil {
+			t.Errorf("Expected to find key %q in metrics response", key)
+		}
+	}
+	if !includeCompile {
+		for _, key := range compileStageKeys {
+			if metrics[key] != nil {
+				t.Errorf("Expected NOT to find key %q in metrics response", key)
+			}
+		}
+	}
+	if t.Failed() {
+		t.Logf("metrics response: %v\n", metrics)
+	}
+}
