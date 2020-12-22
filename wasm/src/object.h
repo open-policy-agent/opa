adcfd32aaa1ce5856e893b399364bdfa77f162fc
Ashutosh Narkar <anarkar4387@gmail.com>
2020-12-22 11:53:57 -0800

    wasm: Add native support for object.remove and object.union builtin function
    Signed-off-by: Ashutosh Narkar <anarkar4387@gmail.com>

--- wasm/src/object.h
+++ wasm/src/object.h
@@ -5,5 +5,7 @@
 
 opa_value *builtin_object_filter(opa_value *obj, opa_value *keys);
 opa_value *builtin_object_get(opa_value *obj, opa_value *key, opa_value *value);
+opa_value *builtin_object_remove(opa_value *obj, opa_value *keys);
+opa_value *builtin_object_union(opa_value *a, opa_value *b);
 
 #endif
