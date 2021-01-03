557fe97f5d4ccb314818d41db158188b98d2e6cd
Anthony Barbieri <anthonyabarbieri@gmail.com>
2021-01-03 11:10:31 -0500

    fixup spacing on link (squash before merge)
    Signed-off-by: Anthony Barbieri <anthonyabarbieri@gmail.com>

--- cmd/flags.go
+++ cmd/flags.go
@@ -91,7 +91,7 @@ func addSigningAlgFlag(fs *pflag.FlagSet, alg *string, value string) {
 }
 
 func addClaimsFileFlag(fs *pflag.FlagSet, file *string) {
-	fs.StringVarP(file, "claims-file", "", "", "set path of JSON file containing optional claims (see:  https://openpolicyagent.org/docs/latest/management/#signature-format)")
+	fs.StringVarP(file, "claims-file", "", "", "set path of JSON file containing optional claims (see: https://openpolicyagent.org/docs/latest/management/#signature-format)")
 }
 
 func addSigningKeyFlag(fs *pflag.FlagSet, key *string) {
