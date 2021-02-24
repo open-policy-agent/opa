b02db2e98a013f9ba15f570b887d8f888492701d
Anders Eknert <anders@eknert.com>
2021-02-24 11:33:59 +0100

    Skip WASM tests on WASM_ENABLED=0
    Signed-off-by: Anders Eknert <anders@eknert.com>

--- internal/wasm/sdk/opa/opa_bench_test.go
+++ internal/wasm/sdk/opa/opa_bench_test.go
@@ -1,3 +1,9 @@
+// Copyright 2021 The OPA Authors.  All rights reserved.
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+// +build opa_wasm
+
 package opa_test
 
 import (
