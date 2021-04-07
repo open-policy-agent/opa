e3cbe8c9ef88b1cbd4c3bae958d821f55e363971
Ashutosh Narkar <anarkar4387@gmail.com>
2021-04-07 15:37:08 -0700

    cmd: Add tooling to support package renaming
    This commit adds a new command to OPA to implement refactoring
    operations over Rego modules. To start a new sub-command has been
    added to support renaming package paths and other references.
    
    Fixes: #3290
    
    Signed-off-by: Ashutosh Narkar <anarkar4387@gmail.com>

---
+++ refactor/refactor.go
@@ -0,0 +1,130 @@
+// Copyright 2021 The OPA Authors.  All rights reserved.
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+// Package refactor implements different refactoring operations over Rego modules.
+package refactor
+
+import (
+	"fmt"
+
+	"github.com/open-policy-agent/opa/ast"
+)
+
+// Error defines the structure of errors returned by refactor.
+type Error struct {
+	Message  string        `json:"message"`
+	Location *ast.Location `json:"location,omitempty"`
+}
+
+func (e Error) Error() string {
+
+	if e.Location != nil {
+		return e.Location.Format(e.Message)
+	}
+
+	return e.Message
+}
+
+// Refactor implements different refactoring operations over Rego modules eg. renaming packages.
+type Refactor struct {
+}
+
+// New returns a new Refactor object.
+func New() *Refactor {
+	return &Refactor{}
+}
+
+// MoveQuery holds the set of Rego modules whose package paths and other references are to be rewritten
+// as per the mapping defined in SrcDstMapping.
+// If validate is true, the moved modules will be compiled to ensure they are valid.
+type MoveQuery struct {
+	Modules       map[string]*ast.Module
+	SrcDstMapping map[string]string
+	validate      bool
+}
+
+// WithValidation controls whether to compile moved modules to ensure they are valid.
+func (mq MoveQuery) WithValidation(v bool) MoveQuery {
+	mq.validate = v
+	return mq
+}
+
+// MoveQueryResult defines the output of a move query and holds the rewritten modules with updated packages paths
+// and references.
+type MoveQueryResult struct {
+	Result map[string]*ast.Module `json:"result"`
+}
+
+// validate validates moved modules by compiling them.
+func (mqr *MoveQueryResult) validate() error {
+	compiler := ast.NewCompiler()
+	compiler.Compile(mqr.Result)
+
+	if compiler.Failed() {
+		return compiler.Errors
+	}
+	return nil
+}
+
+// Move rewrites Rego code by updating package paths and other references in q's modules as per
+// the mapping specified in q.
+func (r *Refactor) Move(q MoveQuery) (*MoveQueryResult, error) {
+
+	for _, module := range q.Modules {
+		t := ast.NewGenericTransformer(func(x interface{}) (interface{}, error) {
+			if s, ok := x.(ast.Ref); ok {
+				// exact match
+				if val, found := q.SrcDstMapping[s.String()]; found {
+					return ast.ParseRef(val)
+				}
+
+				// prefix match
+				for k, v := range q.SrcDstMapping {
+					other, err := ast.ParseRef(k)
+					if err != nil {
+						return nil, err
+					}
+
+					if s.HasPrefix(other) {
+						newRef, err := ast.ParseRef(v)
+						if err != nil {
+							return nil, err
+						}
+						return newRef.Concat(s[len(other):]), nil
+					}
+
+					// check if a reference in the policy is a prefix of a source reference
+					// example: policy_reference = data.foo
+					//          mapping: {"data.foo.bar": "data.baz"}
+					// In this scenario, we can relocate data.foo.bar but everything under data.foo
+					// (e.g., data.foo.baz, data.foo.qux, etc.) can't be relocated
+					if other.HasPrefix(s.ConstantPrefix()) {
+						msg := fmt.Sprintf("cannot rewrite `%v`: constant prefix `%v` of `%v` is too short", s, s.ConstantPrefix(), s)
+						x := Error{Message: msg, Location: s[len(s)-1].Loc()}
+						return nil, x
+					}
+				}
+			}
+			return x, nil
+		})
+		_, err := ast.Transform(t, module)
+		if err != nil {
+			switch err.(type) {
+			case Error:
+				return nil, err
+			default:
+				return nil, Error{Message: err.Error()}
+			}
+		}
+	}
+
+	result := &MoveQueryResult{Result: q.Modules}
+
+	if q.validate {
+		if err := result.validate(); err != nil {
+			return nil, err
+		}
+	}
+	return result, nil
+}
---
+++ refactor/refactor_test.go
@@ -0,0 +1,337 @@
+package refactor
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/open-policy-agent/opa/ast"
+)
+
+func TestMoveRenamePackage(t *testing.T) {
+	module := ast.MustParseModule(`package lib.foo
+
+default allow = false
+
+allow {
+        input.message == "hello"
+}`)
+
+	modules := map[string]*ast.Module{
+		"policy.rego": module,
+	}
+
+	mappings := map[string]string{
+		"data.lib.foo": "data.baz.bar",
+	}
+
+	result, err := New().Move(MoveQuery{
+		Modules:       modules,
+		SrcDstMapping: mappings,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := result.Result["policy.rego"]
+
+	expected := ast.MustParseModule(`package baz.bar
+
+default allow = false
+
+allow {
+        input.message == "hello"
+}`)
+
+	if !expected.Equal(actual) {
+		t.Fatalf("Expected module:\n%v\n\nGot:\n%v\n", expected, actual)
+	}
+}
+
+func TestMoveRenamePackagePrefix(t *testing.T) {
+	module1 := ast.MustParseModule(`package lib.foo
+
+default allow = false
+
+allow {
+        input.message == "hello"
+}`)
+
+	module2 := ast.MustParseModule(`package lib.bar
+
+allow {
+        input.message == "world"
+}`)
+
+	modules := map[string]*ast.Module{
+		"policy1.rego": module1,
+		"policy2.rego": module2,
+	}
+
+	mappings := map[string]string{
+		"data.lib": "data.hidden",
+	}
+
+	result, err := New().Move(MoveQuery{
+		Modules:       modules,
+		SrcDstMapping: mappings,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual1 := result.Result["policy1.rego"]
+	actual2 := result.Result["policy2.rego"]
+
+	expected1 := ast.MustParseModule(`package hidden.foo
+
+default allow = false
+
+allow {
+        input.message == "hello"
+}`)
+
+	expected2 := ast.MustParseModule(`package hidden.bar
+
+allow {
+        input.message == "world"
+}`)
+
+	if !expected1.Equal(actual1) {
+		t.Fatalf("Expected module:\n%v\n\nGot:\n%v\n", expected1, actual1)
+	}
+
+	if !expected2.Equal(actual2) {
+		t.Fatalf("Expected module:\n%v\n\nGot:\n%v\n", expected2, actual2)
+	}
+}
+
+func TestMovePrefixInjection(t *testing.T) {
+	module1 := ast.MustParseModule(`package a.b
+
+p { data.x.q }`)
+
+	module2 := ast.MustParseModule(`package x
+
+q = true`)
+
+	modules := map[string]*ast.Module{
+		"policy1.rego": module1,
+		"policy2.rego": module2,
+	}
+
+	mappings := map[string]string{
+		"data": "data.deadbeef",
+	}
+
+	result, err := New().Move(MoveQuery{
+		Modules:       modules,
+		SrcDstMapping: mappings,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual1 := result.Result["policy1.rego"]
+	actual2 := result.Result["policy2.rego"]
+
+	expected1 := ast.MustParseModule(`package deadbeef.a.b
+
+p {
+	data.deadbeef.x.q
+}`)
+
+	expected2 := ast.MustParseModule(`package deadbeef.x
+
+q = true`)
+
+	if !expected1.Equal(actual1) {
+		t.Fatalf("Expected module:\n%v\n\nGot:\n%v\n", expected1, actual1)
+	}
+
+	if !expected2.Equal(actual2) {
+		t.Fatalf("Expected module:\n%v\n\nGot:\n%v\n", expected2, actual2)
+	}
+}
+
+func TestMoveWithKeyword(t *testing.T) {
+	module1 := ast.MustParseModule(`package a.b
+
+import data.x.q as r
+
+p { r with data.foo as 7 }`)
+
+	module2 := ast.MustParseModule(`package x
+
+q { data.foo == 7 }`)
+
+	modules := map[string]*ast.Module{
+		"policy1.rego": module1,
+		"policy2.rego": module2,
+	}
+
+	mappings := map[string]string{
+		"data": "data.deadbeef",
+	}
+
+	result, err := New().Move(MoveQuery{
+		Modules:       modules,
+		SrcDstMapping: mappings,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual1 := result.Result["policy1.rego"]
+	actual2 := result.Result["policy2.rego"]
+
+	expected1 := ast.MustParseModule(`package deadbeef.a.b
+
+import data.deadbeef.x.q as r
+
+p {
+	r with data.deadbeef.foo as 7
+}`)
+
+	expected2 := ast.MustParseModule(`package deadbeef.x
+
+q {
+	data.deadbeef.foo == 7
+}`)
+
+	if !expected1.Equal(actual1) {
+		t.Fatalf("Expected module:\n%v\n\nGot:\n%v\n", expected1, actual1)
+	}
+
+	if !expected2.Equal(actual2) {
+		t.Fatalf("Expected module:\n%v\n\nGot:\n%v\n", expected2, actual2)
+	}
+}
+
+func TestMovePrefixTooShort(t *testing.T) {
+	module1 := ast.MustParseModule(`package foo.bar
+
+p = 7`)
+
+	module2 := ast.MustParseModule(`package a
+
+p = data.foo`)
+
+	modules := map[string]*ast.Module{
+		"policy1.rego": module1,
+		"policy2.rego": module2,
+	}
+
+	mappings := map[string]string{
+		"data.foo.bar": "data.baz",
+	}
+
+	_, err := New().Move(MoveQuery{
+		Modules:       modules,
+		SrcDstMapping: mappings,
+	})
+	if err == nil {
+		t.Fatal("Expected error but got nil")
+	}
+
+	errMsg := "cannot rewrite `data.foo`: constant prefix `data.foo` of `data.foo` is too short"
+	if !strings.Contains(err.Error(), errMsg) {
+		t.Fatalf("Expected error message %v but got %v", errMsg, err.Error())
+	}
+}
+
+func TestMoveConflictingRulesNoValidation(t *testing.T) {
+	module1 := ast.MustParseModule(`package a.b
+
+p[1]`)
+
+	module2 := ast.MustParseModule(`package b
+
+p = 7`)
+
+	modules := map[string]*ast.Module{
+		"policy1.rego": module1,
+		"policy2.rego": module2,
+	}
+
+	mappings := map[string]string{
+		"data.a": "data",
+	}
+
+	_, err := New().Move(MoveQuery{
+		Modules:       modules,
+		SrcDstMapping: mappings,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestMoveConflictingRulesWithValidation(t *testing.T) {
+	module1 := ast.MustParseModule(`package a.b
+
+p[1]`)
+
+	module2 := ast.MustParseModule(`package b
+
+p = 7`)
+
+	modules := map[string]*ast.Module{
+		"policy1.rego": module1,
+		"policy2.rego": module2,
+	}
+
+	mappings := map[string]string{
+		"data.a": "data",
+	}
+
+	_, err := New().Move(MoveQuery{
+		Modules:       modules,
+		SrcDstMapping: mappings,
+	}.WithValidation(true))
+	if err == nil {
+		t.Fatal("Expected error but got nil")
+	}
+
+	errMsg := "rego_type_error: conflicting rules named p found"
+	if !strings.Contains(err.Error(), errMsg) {
+		t.Fatalf("Expected error message %v but got %v", errMsg, err.Error())
+	}
+}
+
+func TestMoveBadSourceMapping(t *testing.T) {
+	module := ast.MustParseModule(`package lib.foo
+
+default allow = false
+
+allow {
+        input.message == "hello"
+}`)
+
+	modules := map[string]*ast.Module{
+		"policy.rego": module,
+	}
+
+	mappings := map[string]string{
+		"data.lib.": "data.hidden",
+	}
+
+	_, err := New().Move(MoveQuery{
+		Modules:       modules,
+		SrcDstMapping: mappings,
+	})
+	if err == nil {
+		t.Fatal("Expected error but got nil")
+	}
+
+	mappings = map[string]string{
+		"data.lib": "data.hidden.",
+	}
+
+	_, err = New().Move(MoveQuery{
+		Modules:       modules,
+		SrcDstMapping: mappings,
+	})
+	if err == nil {
+		t.Fatal("Expected error but got nil")
+	}
+}
