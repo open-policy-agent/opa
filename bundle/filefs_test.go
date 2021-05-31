32ee3b77b6ca57569d6e16e6d88e61416e79bc01
Simon Gottschlag <simon@gottschlag.se>
2021-05-31 10:18:45 +0200

    bundle: Implement a DirectoryLoader for fs.FS (#3493)
    To make it possible to create a DirectoryLoader based on the fs.FS interface.
    This interface requires go version 1.16 or above.
    
    Fixes: #3489
    
    Signed-off-by: Simon Gottschlag <simon.gottschlag@xenit.se>

---
+++ bundle/filefs_test.go
@@ -0,0 +1,26 @@
+// +build go1.16
+
+package bundle
+
+import (
+	"strings"
+	"testing"
+	"testing/fstest"
+)
+
+func TestFSLoader(t *testing.T) {
+	archiveFS := make(fstest.MapFS)
+	for k, v := range archiveFiles {
+		file := strings.TrimPrefix(k, "/")
+		archiveFS[file] = &fstest.MapFile{
+			Data: []byte(v),
+		}
+	}
+
+	loader, err := NewFSLoader(archiveFS)
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+
+	testLoader(t, loader, "", archiveFiles)
+}
