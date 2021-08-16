7845ff2d2706741ae5066bdc1834da1203af01b4
Stephan Renatus <stephan.renatus@gmail.com>
2021-08-16 10:22:18 +0200

    no opa_wasm built tag on features/wasm
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- features/wasm/wasm.go
+++ features/wasm/wasm.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by an Apache2
 // license that can be found in the LICENSE file.
 
-// +build opa_wasm
-
 // Import this package to enable evaluation of rego code using the
 // built-in wasm engine.
 package wasm
