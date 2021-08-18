d7f21019ec5aa6c188bf49c5b8660afb828aa101
Stephan Renatus <stephan.renatus@gmail.com>
2021-08-18 13:57:50 +0200

    tester: remove pre-1.16 fixtures
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- tester/fixture_test.go
+++ tester/fixture_test.go
@@ -1,10 +1,3 @@
-// +build !go1.16
-
-// The outputs here change with Go 1.16, see
-// https://github.com/golang/go/commit/afe7c8d0b25f26f0abd749ca52c7e1e7dfdee8cb
-// But since we've been using 1.14 by the time these split-off fixtures
-// were introduced, the filename mentions go1.14.
-
 package tester
 
 const fixtureReporterVerboseBenchmark = `FAILURES
@@ -14,7 +7,7 @@ data.foo.bar.test_corge: FAIL (0s)
 
 SUMMARY
 --------------------------------------------------------------------------------
-data.foo.bar.test_baz	    1000	       123 ns/op
+data.foo.bar.test_baz	    1000	       123.0 ns/op
 data.foo.bar.test_qux: ERROR (0s)
   some err
 data.foo.bar.test_corge: FAIL (0s)
@@ -31,7 +24,7 @@ data.foo.bar.test_corge: FAIL (0s)
 
 SUMMARY
 --------------------------------------------------------------------------------
-data.foo.bar.test_baz	    1000	       123 ns/op	       123 timer_rego_query_eval_ns/op	      91 B/op	       0 allocs/op
+data.foo.bar.test_baz	    1000	       123.0 ns/op	       123.0 timer_rego_query_eval_ns/op	      91 B/op	       0 allocs/op
 data.foo.bar.test_qux: ERROR (0s)
   some err
 data.foo.bar.test_corge: FAIL (0s)
@@ -48,7 +41,7 @@ data.foo.bar.test_corge: FAIL (0s)
 
 SUMMARY
 --------------------------------------------------------------------------------
-BenchmarkDataFooBarTestBaz	    1000	       123 ns/op	       123 timer_rego_query_eval_ns/op	      91 B/op	       0 allocs/op
+BenchmarkDataFooBarTestBaz	    1000	       123.0 ns/op	       123.0 timer_rego_query_eval_ns/op	      91 B/op	       0 allocs/op
 data.foo.bar.test_qux: ERROR (0s)
   some err
 data.foo.bar.test_corge: FAIL (0s)
