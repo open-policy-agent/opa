bff539985626b2a4b8a76eebcb884a5968f0d484
Stephan Renatus <stephan.renatus@gmail.com>
2021-09-27 18:36:18 +0200

    ast/term: don't sort an object's keys slice in-place (#3823)
    * ast/term: sort-on-insert for object.keys slice
    
    Instead of sorting on each call including Compare() or String(), we'll sort the key
    slice on insertion of a new value.
    
    * testcases: add case
    
    As observed in the original issue, the failure does not happen on every
    run. To observe the added test case _failing_ on branch main, use a test
    call like
    
        go test -v ./topdown -run TestRego/partialsetdoc:_object_sort_while_iter -count=10 -v
    
    The added `sort_bindings: true` is not required for the topdown eval,
    but when executing this on the wasm engine, the (unspecified, non-
    guaranteed) order is reversed.
    
    Fixes #3819
    
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- ast/term_test.go
+++ ast/term_test.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"sort"
 	"strings"
 	"testing"
 
@@ -259,6 +260,37 @@ func TestObjectFilter(t *testing.T) {
 				t.Errorf("Expected:\n\n\t%s\n\nGot:\n\n\t%s\n\n", expected, actual)
 			}
 		})
+	}
+}
+
+func TestObjectInsertKeepsSorting(t *testing.T) {
+	keysSorted := func(o *object) func(int, int) bool {
+		return func(i, j int) bool {
+			return Compare(o.keys[i].key, o.keys[j].key) < 0
+		}
+	}
+
+	obj := NewObject(
+		[2]*Term{StringTerm("d"), IntNumberTerm(4)},
+		[2]*Term{StringTerm("b"), IntNumberTerm(2)},
+		[2]*Term{StringTerm("a"), IntNumberTerm(1)},
+	)
+	o := obj.(*object)
+	act := sort.SliceIsSorted(o.keys, keysSorted(o))
+	if exp := true; act != exp {
+		t.Errorf("SliceIsSorted: expected %v, got %v", exp, act)
+		for i := range o.keys {
+			t.Logf("elem[%d]: %v", i, o.keys[i].key)
+		}
+	}
+
+	obj.Insert(StringTerm("c"), IntNumberTerm(3))
+	act = sort.SliceIsSorted(o.keys, keysSorted(o))
+	if exp := true; act != exp {
+		t.Errorf("SliceIsSorted: expected %v, got %v", exp, act)
+		for i := range o.keys {
+			t.Logf("elem[%d]: %v", i, o.keys[i].key)
+		}
 	}
 }
 
