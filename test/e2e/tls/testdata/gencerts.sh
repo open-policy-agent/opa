f27c3209e2b4207b38b0f3167110d787572eb8fb
Stephan Renatus <stephan.renatus@gmail.com>
2021-10-07 13:51:08 +0200

    test/e2e/tls: regenerate certs, change validity to 10yrs
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- test/e2e/tls/testdata/gencerts.sh
+++ test/e2e/tls/testdata/gencerts.sh
@@ -20,16 +20,16 @@ IP.1 = 127.0.0.1
 EOF
 
 openssl genrsa -out ca-key.pem 2048
-openssl req -x509 -new -nodes -key ca-key.pem -days 1000 -out ca.pem -subj "/CN=my-ca"
+openssl req -x509 -new -nodes -key ca-key.pem -days 3650 -out ca.pem -subj "/CN=my-ca"
 
 openssl genrsa -out client-key.pem 2048
 openssl req -new -key client-key.pem -out csr.pem -subj "/CN=my-client"
-openssl x509 -req -in csr.pem -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -days 1000
+openssl x509 -req -in csr.pem -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -days 3650
 
 openssl genrsa -out client-key-2.pem 2048
 openssl req -new -key client-key-2.pem -out csr.pem -subj "/CN=my-client-2"
-openssl x509 -req -in csr.pem -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out client-cert-2.pem -days 1000
+openssl x509 -req -in csr.pem -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out client-cert-2.pem -days 3650
 
 openssl genrsa -out server-key.pem 2048
 openssl req -new -key server-key.pem -out csr.pem -subj "/CN=my-server" -config req.cnf
-openssl x509 -req -in csr.pem -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -days 1000 -extensions v3_req -extfile req.cnf
+openssl x509 -req -in csr.pem -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -days 3650 -extensions v3_req -extfile req.cnf
