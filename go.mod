16f04e06e8bc5b5691af61ac288331f5f11aeb85
Stephan Renatus <stephan.renatus@gmail.com>
2021-11-11 08:16:56 +0100

    deps: add mockdns, revendor
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/dgraph-io/badger/v3 v3.2103.2
 	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
 	github.com/fortytw2/leaktest v1.3.0
+	github.com/foxcpp/go-mockdns v0.0.0-20210729171921-fb145fc6f897
 	github.com/fsnotify/fsnotify v1.5.1
 	github.com/ghodss/yaml v1.0.0
 	github.com/gobwas/glob v0.2.3
@@ -17,6 +18,7 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/klauspost/compress v1.13.5 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/miekg/dns v1.1.43 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/peterh/liner v0.0.0-20170211195444-bf27d3ba8e1d
@@ -32,8 +34,8 @@ require (
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415
 	github.com/yashtewari/glob-intersection v0.0.0-20180916065949-5c77d914dd0b
 	go.uber.org/automaxprocs v1.4.0
-	golang.org/x/net v0.0.0-20210825183410-e898025ed96a
-	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/net v0.0.0-20211111083644-e5c967477495
+	golang.org/x/sys v0.0.0-20211110154304-99a53858aa08 // indirect
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
---
+++ vendor/github.com/foxcpp/go-mockdns/resolver.go
@@ -0,0 +1,288 @@
+package mockdns
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"strings"
+
+	"github.com/miekg/dns"
+)
+
+type Zone struct {
+	// Return the specified error on any lookup using this zone.
+	// For Server, non-nil value results in SERVFAIL response.
+	Err error
+
+	// When used with Server, set the Authenticated Data (AD) flag
+	// in the responses.
+	AD bool
+
+	A     []string
+	AAAA  []string
+	TXT   []string
+	PTR   []string
+	CNAME string
+	MX    []net.MX
+	NS    []net.NS
+	SRV   []net.SRV
+
+	// Misc includes other associated zone records, they can be returned only
+	// when used with Server.
+	Misc map[dns.Type][]dns.RR
+}
+
+// Resolver is the struct that implements interface same as net.Resolver
+// and so can be used as a drop-in replacement for it if tested code
+// supports it.
+type Resolver struct {
+	Zones map[string]Zone
+
+	// Don't follow CNAME in Zones for Lookup*.
+	SkipCNAME bool
+}
+
+func (r *Resolver) LookupAddr(ctx context.Context, addr string) (names []string, err error) {
+	arpa, err := dns.ReverseAddr(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	rzone, ok := r.Zones[strings.ToLower(arpa)]
+	if !ok {
+		return nil, notFound(arpa)
+	}
+	if rzone.Err != nil {
+		return nil, rzone.Err
+	}
+
+	names = make([]string, len(rzone.PTR))
+	copy(names, rzone.PTR)
+	return
+}
+
+func (r *Resolver) LookupCNAME(ctx context.Context, host string) (cname string, err error) {
+	rzone, ok := r.Zones[strings.ToLower(host)]
+	if !ok {
+		return "", notFound(host)
+	}
+
+	return rzone.CNAME, nil
+}
+
+func (r *Resolver) LookupHost(ctx context.Context, host string) (addrs []string, err error) {
+	_, addrs4, err := r.lookupA(ctx, host)
+	if err != nil {
+		return nil, err
+	}
+	_, addrs6, err := r.lookupAAAA(ctx, host)
+	if err != nil {
+		return nil, err
+	}
+
+	addrs = append(addrs, addrs4...)
+	addrs = append(addrs, addrs6...)
+
+	if len(addrs) == 0 {
+		return nil, notFound(host)
+	}
+
+	return addrs, err
+}
+
+func (r *Resolver) targetZone(name string) (ad bool, rname string, zone Zone, err error) {
+	rname = strings.ToLower(dns.Fqdn(name))
+	rzone, ok := r.Zones[rname]
+	if !ok {
+		return false, "", Zone{}, notFound(name)
+	}
+
+	if rzone.Err != nil {
+		return false, "", rzone, rzone.Err
+	}
+
+	ad = rzone.AD
+
+	if !r.SkipCNAME {
+		for rzone.CNAME != "" {
+			rname = rzone.CNAME
+			rzone, ok = r.Zones[rname]
+			if !ok {
+				return false, rname, Zone{}, notFound(rname)
+			}
+			if rzone.Err != nil {
+				return false, "", rzone, rzone.Err
+			}
+			ad = ad && rzone.AD
+		}
+	}
+
+	return ad, rname, rzone, nil
+}
+
+func (r *Resolver) lookupA(ctx context.Context, host string) (cname string, addrs []string, err error) {
+	_, cname, rzone, err := r.targetZone(host)
+	if err != nil {
+		return cname, nil, err
+	}
+
+	return cname, rzone.A, nil
+}
+
+func (r *Resolver) lookupAAAA(ctx context.Context, host string) (cname string, addrs []string, err error) {
+	_, cname, rzone, err := r.targetZone(host)
+	if err != nil {
+		return cname, nil, err
+	}
+
+	return cname, rzone.AAAA, nil
+}
+
+func (r *Resolver) LookupIPAddr(ctx context.Context, host string) ([]net.IPAddr, error) {
+	addrs, err := r.LookupHost(ctx, host)
+	if err != nil {
+		return nil, err
+	}
+
+	parsed := make([]net.IPAddr, 0, len(addrs))
+	for _, addr := range addrs {
+		ip := net.ParseIP(addr)
+		if ip == nil {
+			return nil, fmt.Errorf("malformed IP in records: %v", addr)
+		}
+
+		parsed = append(parsed, net.IPAddr{IP: ip})
+	}
+
+	return parsed, nil
+}
+
+func (r *Resolver) LookupMX(ctx context.Context, name string) ([]*net.MX, error) {
+	_, mx, err := r.lookupMX(ctx, name)
+	res := make([]*net.MX, len(mx))
+	copy(res, mx)
+	return res, err
+}
+
+func (r *Resolver) lookupMX(ctx context.Context, name string) (string, []*net.MX, error) {
+	_, cname, rzone, err := r.targetZone(name)
+	if err != nil {
+		return "", nil, err
+	}
+
+	out := make([]*net.MX, 0, len(rzone.MX))
+	for _, mx := range rzone.MX {
+		mxCpy := mx
+		out = append(out, &mxCpy)
+	}
+
+	return cname, out, nil
+}
+
+func (r *Resolver) LookupNS(ctx context.Context, name string) ([]*net.NS, error) {
+	_, ns, err := r.lookupNS(ctx, name)
+	res := make([]*net.NS, len(ns))
+	copy(res, ns)
+	return res, err
+}
+
+func (r *Resolver) lookupNS(ctx context.Context, name string) (string, []*net.NS, error) {
+	_, cname, rzone, err := r.targetZone(name)
+	if err != nil {
+		return "", nil, err
+	}
+
+	out := make([]*net.NS, 0, len(rzone.MX))
+	for _, ns := range rzone.NS {
+		nsCpy := ns
+		out = append(out, &nsCpy)
+	}
+
+	return cname, out, nil
+}
+
+func (r *Resolver) LookupPort(ctx context.Context, network, service string) (port int, err error) {
+	// TODO: Check whether it can cause problems with net.DefaultResolver hjacking.
+	return net.LookupPort(network, service)
+}
+
+func (r *Resolver) LookupSRV(ctx context.Context, service, proto, name string) (cname string, addrs []*net.SRV, err error) {
+	query := fmt.Sprintf("_%s._%s.%s", service, proto, name)
+	return r.lookupSRV(ctx, query)
+}
+
+func (r *Resolver) lookupSRV(ctx context.Context, query string) (cname string, addrs []*net.SRV, err error) {
+	_, cname, rzone, err := r.targetZone(query)
+	if err != nil {
+		return "", nil, err
+	}
+
+	out := make([]*net.SRV, 0, len(rzone.SRV))
+	for _, srv := range rzone.SRV {
+		srvCpy := srv
+		out = append(out, &srvCpy)
+	}
+
+	return cname, out, nil
+}
+
+func (r *Resolver) LookupTXT(ctx context.Context, name string) ([]string, error) {
+	_, txt, err := r.lookupTXT(ctx, name)
+	res := make([]string, len(txt))
+	copy(res, txt)
+	return res, err
+}
+
+func (r *Resolver) lookupTXT(ctx context.Context, name string) (string, []string, error) {
+	_, cname, rzone, err := r.targetZone(name)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return cname, rzone.TXT, nil
+}
+
+// Dial implements the function similar to net.Dial that uses Resolver zones
+// to find the the IP address to use. It is very simple and does not fully
+// replicate the net.Dial behavior. Notably it does not implement Fast Fallback
+// and always prefers IPv6 over IPv4.
+func (r *Resolver) Dial(network, addr string) (net.Conn, error) {
+	return r.DialContext(context.Background(), network, addr)
+}
+
+func (r *Resolver) DialContext(ctx context.Context, network, addr string) (net.Conn, error) {
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	ip := net.ParseIP(host)
+	if ip != nil {
+		return net.Dial(network, addr)
+	}
+
+	_, addrs6, err := r.lookupAAAA(ctx, host)
+	if err != nil {
+		return nil, err
+	}
+	_, addrs4, err := r.lookupA(ctx, host)
+	if err != nil {
+		return nil, err
+	}
+	addrs := append(addrs6, addrs4...)
+
+	if len(addrs) == 0 {
+		return nil, notFound(host)
+	}
+
+	var lastErr error
+	for _, addrTry := range addrs {
+		conn, err := net.Dial(network, net.JoinHostPort(addrTry, port))
+		if err != nil {
+			lastErr = err
+			continue
+		}
+		return conn, nil
+	}
+	return nil, lastErr
+}
