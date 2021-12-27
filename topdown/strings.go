6f81c4a620fbb8035c8c06072d6752582b6e9dd6
Kristian Svalland <54534849+kristiansvalland@users.noreply.github.com>
2021-12-27 12:47:39 +0100

    Add `array.reverse(array)` and `strings.reverse(string)` built-in functions. (#4161)
    The function `array.reverse` takes an array as an argument, and returns an array with a reversed order of elements.
    The function `strings.reverse` takes a string as an argument, and returns a string with a reversed order of unicode code points.
    WASM support is included for both built-ins.
    
    Fixes #3736
    
    Signed-off-by: Kristian Svalland <kristian.svalland@gmail.com>

--- topdown/strings.go
+++ topdown/strings.go
@@ -412,6 +412,25 @@ func builtinSprintf(a, b ast.Value) (ast.Value, error) {
 	return ast.String(fmt.Sprintf(string(s), args...)), nil
 }
 
+func builtinReverse(bctx BuiltinContext, operands []*ast.Term, iter func(*ast.Term) error) error {
+	s, err := builtins.StringOperand(operands[0].Value, 1)
+	if err != nil {
+		return err
+	}
+
+	sRunes := []rune(string(s))
+	length := len(sRunes)
+	reversedRunes := make([]rune, length)
+
+	for index, r := range sRunes {
+		reversedRunes[length-index-1] = r
+	}
+
+	reversedString := string(reversedRunes)
+
+	return iter(ast.StringTerm(reversedString))
+}
+
 func init() {
 	RegisterFunctionalBuiltin2(ast.FormatInt.Name, builtinFormatInt)
 	RegisterFunctionalBuiltin2(ast.Concat.Name, builtinConcat)
@@ -432,4 +451,5 @@ func init() {
 	RegisterFunctionalBuiltin2(ast.TrimSuffix.Name, builtinTrimSuffix)
 	RegisterFunctionalBuiltin1(ast.TrimSpace.Name, builtinTrimSpace)
 	RegisterFunctionalBuiltin2(ast.Sprintf.Name, builtinSprintf)
+	RegisterBuiltinFunc(ast.StringReverse.Name, builtinReverse)
 }
--- wasm/src/array.h
+++ wasm/src/array.h
@@ -3,5 +3,6 @@
 
 opa_value *opa_array_concat(opa_value *a, opa_value *b);
 opa_value *opa_array_slice(opa_value *a, opa_value *i, opa_value *j);
+opa_value *opa_array_reverse(opa_value *a);
 
 #endif
