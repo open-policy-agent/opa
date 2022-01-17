258501134dc52dc594b1255e58f04da5541f89d8
Justin Lindh <justin.lindh@workiva.com>
2022-01-17 01:31:31 -0700

    built-ins: add graph.reachable_paths (#4205)
    This new built-in functionality allows callers to find all reachable
    paths in a graph based on an array or set of root nodes. See the
    updates to policy-reference.md for more details and usage information.
    
    Signed-off-by: Justin Lindh <justin.lindh@webfilings.com>

--- topdown/reachable.go
+++ topdown/reachable.go
@@ -6,6 +6,7 @@ package topdown
 
 import (
 	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/topdown/builtins"
 )
 
 // Helper: sets of vertices can be represented as Arrays or Sets.
@@ -16,6 +17,18 @@ func foreachVertex(collection *ast.Term, f func(*ast.Term)) {
 	case *ast.Array:
 		v.Foreach(f)
 	}
+}
+
+// numberOfEdges returns the number of elements of an array or a set (of edges)
+func numberOfEdges(collection *ast.Term) int {
+	switch v := collection.Value.(type) {
+	case ast.Set:
+		return v.Len()
+	case *ast.Array:
+		return v.Len()
+	}
+
+	return 0
 }
 
 func builtinReachable(bctx BuiltinContext, args []*ast.Term, iter func(*ast.Term) error) error {
@@ -56,6 +69,66 @@ func builtinReachable(bctx BuiltinContext, args []*ast.Term, iter func(*ast.Term
 	return iter(ast.NewTerm(reached))
 }
 
+// pathBuilder is called recursively to build an array of paths that are reachable from the root
+func pathBuilder(graph ast.Object, root *ast.Term, path []*ast.Term, paths []*ast.Term, reached ast.Set) []*ast.Term {
+	if edges := graph.Get(root); edges != nil {
+		path = append(path, root)
+
+		if numberOfEdges(edges) >= 1 {
+			foreachVertex(edges, func(neighbor *ast.Term) {
+				if reached.Contains(neighbor) {
+					// If we've already reached this node, return current path (avoid infinite recursion)
+					paths = append(paths, path...)
+				} else {
+					reached.Add(root)
+					paths = pathBuilder(graph, neighbor, path, paths, reached)
+				}
+			})
+		} else {
+			paths = append(paths, path...)
+		}
+	} else {
+		// Node is nonexistent (not in graph). Commit the current path (without adding this root)
+		paths = append(paths, path...)
+	}
+
+	return paths
+}
+
+func builtinReachablePaths(bctx BuiltinContext, args []*ast.Term, iter func(*ast.Term) error) error {
+	// Return an error if the first argument is not an object.
+	graph, err := builtins.ObjectOperand(args[0].Value, 1)
+	if err != nil {
+		return err
+	}
+
+	// This is a queue that holds all nodes we still need to visit.  It is
+	// initialised to the initial set of nodes we start out with.
+	var queue []*ast.Term
+	foreachVertex(args[1], func(t *ast.Term) {
+		queue = append(queue, t)
+	})
+
+	results := ast.NewSet()
+
+	for _, node := range queue {
+		// Find reachable paths from edges in root node in queue and append arrays to the results set
+		if edges := graph.Get(node); edges != nil {
+			if numberOfEdges(edges) >= 1 {
+				foreachVertex(edges, func(neighbor *ast.Term) {
+					paths := pathBuilder(graph, neighbor, []*ast.Term{node}, []*ast.Term{}, ast.NewSet(node))
+					results.Add(ast.ArrayTerm(paths...))
+				})
+			} else {
+				results.Add(ast.ArrayTerm(node))
+			}
+		}
+	}
+
+	return iter(ast.NewTerm(results))
+}
+
 func init() {
 	RegisterBuiltinFunc(ast.ReachableBuiltin.Name, builtinReachable)
+	RegisterBuiltinFunc(ast.ReachablePathsBuiltin.Name, builtinReachablePaths)
 }
