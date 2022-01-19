9b3d047735c5cc940c10188b99101399de015993
Damien Burks <damien@hackintosh.attlocal.net>
2022-01-18 21:35:48 -0600

    removing -l flag from the test subcommand
    Signed-off-by: Damien Burks <damien@hackintosh.attlocal.net>

--- cmd/test.go
+++ cmd/test.go
@@ -352,8 +352,6 @@ func filterTrace(params *testCommandParams, trace []*topdown.Event) []*topdown.E
 func init() {
 	testCommand.Flags().BoolVarP(&testParams.skipExitZero, "exit-zero-on-skipped", "z", false, "skipped tests return status 0")
 	testCommand.Flags().BoolVarP(&testParams.verbose, "verbose", "v", false, "set verbose reporting mode")
-	testCommand.Flags().BoolVarP(&testParams.failureLine, "show-failure-line", "l", false, "show test failure line")
-	_ = testCommand.Flags().MarkDeprecated("show-failure-line", "use -v instead")
 	testCommand.Flags().DurationVar(&testParams.timeout, "timeout", 0, "set test timeout (default 5s, 30s when benchmarking)")
 	testCommand.Flags().VarP(testParams.outputFormat, "format", "f", "set output format")
 	testCommand.Flags().BoolVarP(&testParams.coverage, "coverage", "c", false, "report coverage (overrides debug tracing)")
