2bbdec65ed361d54f2f67eee3407fa2416b0944c
Stephan Renatus <stephan.renatus@gmail.com>
2022-02-02 14:44:38 +0100

    internal/prometheus: try to fix json marshalling
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/go-logr/logr v1.2.2
 	github.com/gobwas/glob v0.2.3
 	github.com/golang/glog v1.0.0 // indirect
+	github.com/golang/protobuf v1.5.2
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/klauspost/compress v1.13.5 // indirect
--- internal/prometheus/prometheus_test.go
+++ internal/prometheus/prometheus_test.go
@@ -2,26 +2,25 @@
 // Use of this source code is governed by an Apache2
 // license that can be found in the LICENSE file.
 
-package prometheus_test
+package prometheus
 
 import (
 	"encoding/json"
 	"testing"
 
-	"github.com/open-policy-agent/opa/internal/prometheus"
 	"github.com/open-policy-agent/opa/logging"
 	"github.com/open-policy-agent/opa/metrics"
 )
 
 func TestJSONSerialization(t *testing.T) {
 	inner := metrics.New()
-	logger := func(logger logging.Logger) func(attrs map[string]interface{}, f string, a ...interface{}) {
+	logger := func(logger logging.Logger) loggerFunc {
 		return func(attrs map[string]interface{}, f string, a ...interface{}) {
 			logger.WithFields(map[string]interface{}(attrs)).Error(f, a...)
 		}
 	}(logging.NewNoOpLogger())
 
-	prom := prometheus.New(inner, logger)
+	prom := New(inner, logger)
 
 	m := prom.All()
 	bs, err := json.Marshal(m)
