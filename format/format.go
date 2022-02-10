5a2ecba250c8aaedc8138edd49a1bd89ec36f7f2
Stephan Renatus <stephan.renatus@gmail.com>
2022-02-10 10:55:03 +0100

    format: generated vars may have a proper location
    Re-introduce check that got removed in the last fix to groupIterable.
    
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- format/format.go
+++ format/format.go
@@ -904,10 +904,21 @@ func groupIterable(elements []interface{}, last *ast.Location) [][]interface{} {
 	// Those variables, and wildcard variables have the "default location",
 	// set in `Ast()`). That is no proper file location, and the grouping
 	// based on source location will yield a bad result.
+	// Another case is generated variables: they do have proper file locations,
+	// but their row/col information may no longer match their AST location.
+	// So, for generated variables, we also don't trust the location, but
+	// keep them ungrouped.
 	def := false // default location found?
 	for _, elem := range elements {
 		ast.WalkTerms(elem, func(t *ast.Term) bool {
 			if t.Location.File == defaultLocationFile {
+				def = true
+				return true
+			}
+			return false
+		})
+		ast.WalkVars(elem, func(v ast.Var) bool {
+			if v.IsGenerated() {
 				def = true
 				return true
 			}
