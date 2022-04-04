4956dfbd46ff285ee9ce564c5674c7c26e3adedd
Stephan Renatus <stephan.renatus@gmail.com>
2022-04-04 14:30:12 +0200

    topdown/eval: add first yaml tests, fix Var->Ref issue
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- ast/compile.go
+++ ast/compile.go
@@ -4813,8 +4813,8 @@ func validateWith(c *Compiler, target, value *Term) (bool, *Error) {
 			return false, NewError(CompileErr, target.Loc(), "with keyword replacing built-in function: value must be a reference to a function")
 		}
 
-		if _, ok := target.Value.(Var); ok { // TODO(sr): ensure we're dealing with Refs only in topdown, write test case
-			target = RefTerm(target)
+		if v, ok := target.Value.(Var); ok { // TODO(sr): ensure we're dealing with Refs only in topdown, write test case
+			target.Value = Ref([]*Term{NewTerm(v)})
 		}
 
 		node := c.RuleTree
