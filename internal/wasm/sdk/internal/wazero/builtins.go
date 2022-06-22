ad8dcdce3ea409cba8f4aec5863fca40cb621cc0
Kaijlo <kaijserlove@gmail.com>
2022-06-22 15:36:46 +0200

    this should work

--- internal/wasm/sdk/internal/wazero/builtins.go
+++ internal/wasm/sdk/internal/wazero/builtins.go
@@ -1,36 +1,24 @@
 package wazero
 
 import (
-	"context"
-	"crypto/rand"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"github.com/open-policy-agent/opa/ast"
-	"github.com/open-policy-agent/opa/metrics"
-	"github.com/open-policy-agent/opa/topdown"
-	"github.com/open-policy-agent/opa/topdown/builtins"
-	"github.com/open-policy-agent/opa/topdown/cache"
-	"github.com/open-policy-agent/opa/topdown/print"
-	"io"
-	"log"
 	"strconv"
-	"time"
+
+	"github.com/open-policy-agent/opa/topdown"
 )
 
 func newBuiltinTable(mod Module) map[int32]topdown.BuiltinFunc {
 	builtinStrAddr := mod.builtins(mod.ctx)
-	builtinsJSON, err := mod.jsonDump(mod.ctx, uint64(builtinStrAddr))
+	builtinsJSON, err := mod.json_dump(mod.ctx, (builtinStrAddr))
 	if err != nil {
 		panic(err)
 	}
-	builtinStr := mod.readStr(uint32(builtinsJSON[0]))
+	builtinStr := mod.readStr(uint32(builtinsJSON))
 	builtinNameMap := parseJsonString(builtinStr)
 	builtinIdMap, err := getFuncs(builtinNameMap)
 	if err != nil {
 		panic(err)
 	}
-	log.Println(builtinIdMap)
 	return builtinIdMap
 }
 func parseJsonString(str string) map[string]int32 {
@@ -70,149 +58,9 @@ func getFuncs(ids map[string]int32) (map[int32]topdown.BuiltinFunc, error) {
 	out := map[int32]topdown.BuiltinFunc{}
 	for name, id := range ids {
 		out[id] = topdown.GetBuiltin(name)
-		log.Println(name)
 		if out[id] == nil && name != "" {
 			return out, fmt.Errorf("no function named %s", name)
 		}
 	}
 	return out, nil
-}
-
-type exports struct {
-	val_dump  func(context.Context, ...uint64) ([]uint64, error)
-	val_parse func(context.Context, ...uint64) ([]uint64, error)
-	json_dump func(context.Context, ...uint64) ([]uint64, error)
-	malloc    func(context.Context, ...uint64) ([]uint64, error)
-}
-type builtinContainer struct {
-	builtinIdMap map[int32]topdown.BuiltinFunc
-	module       Module
-	e            exports
-	ctx          *topdown.BuiltinContext
-}
-
-func (b *builtinContainer) Call(args ...int32) int32 {
-	log.Println("calling")
-	var output *ast.Term
-	pArgs := []*ast.Term{}
-	for _, ter := range args[2:] {
-		log.Println("value_dump")
-		serialized, err := b.e.val_dump(b.module.ctx, uint64(ter))
-		log.Println("post_value_dump")
-		if err != nil {
-			log.Println("93", err)
-			panic(builtinError{err: err})
-		}
-		log.Println("readstr")
-		data := b.module.readStr(uint32(serialized[0]))
-		log.Println("ParseTerm")
-		pTer, err := ast.ParseTerm(string(data))
-		if err != nil {
-			log.Println("99", err)
-			panic(builtinError{err: err})
-		}
-		pArgs = append(pArgs, pTer)
-	}
-	log.Println("testing")
-	err := b.builtinIdMap[args[0]](*b.ctx, pArgs, func(t *ast.Term) error {
-		output = t
-		log.Println(t)
-		log.Println("hi")
-		return nil
-	})
-	if err != nil {
-		if errors.As(err, &topdown.Halt{}) {
-			var e *topdown.Error
-			if errors.As(err, &e) && e.Code == topdown.CancelErr {
-				log.Println("112", e.Message)
-				panic(cancelledError{message: e.Message})
-			}
-			log.Println("115", err)
-			panic(builtinError{err: err})
-		}
-		// non-halt errors are treated as undefined ("non-strict eval" is the only
-		// mode in wasm), the `output == nil` case below will return NULL
-	}
-	if output == nil {
-		log.Println("Hi")
-		return 0
-	}
-	outB := []byte(output.String())
-	loc := b.module.writeMem(outB)
-	addr, err := b.e.val_dump(b.module.ctx, uint64(loc), uint64(len(outB)))
-	if err != nil {
-		log.Println("128", err)
-		panic(err)
-	}
-	log.Println("hi", addr)
-	log.Println("hi", b.module.readUntil(int32(addr[0]), 0b0))
-	return int32(addr[0])
-}
-
-// Exported to wasm and executes Call with the given builtin_id and arguments
-func (b *builtinContainer) C0(id, ctx int32) int32 {
-	return b.Call(id, ctx)
-}
-func (b *builtinContainer) C1(id, ctx, a1 int32) int32 {
-	return b.Call(id, ctx, a1)
-}
-func (b *builtinContainer) C2(id, ctx, a1, a2 int32) int32 {
-	return b.Call(id, ctx, a1, a2)
-}
-func (b *builtinContainer) C3(id, ctx, a1, a2, a3 int32) int32 {
-	return b.Call(id, ctx, a1, a2, a3)
-}
-func (b *builtinContainer) C4(id, ctx, a1, a2, a3, a4 int32) int32 {
-	return b.Call(id, ctx, a1, a2, a3, a4)
-}
-func (b *builtinContainer) Reset(ctx context.Context,
-	seed io.Reader,
-	ns time.Time,
-	iqbCache cache.InterQueryCache,
-	ph print.Hook,
-	capabilities *ast.Capabilities) {
-	if ns.IsZero() {
-		ns = time.Now()
-	}
-	if seed == nil {
-		seed = rand.Reader
-	}
-	b.ctx = &topdown.BuiltinContext{
-		Context:                ctx,
-		Metrics:                metrics.New(),
-		Seed:                   seed,
-		Time:                   ast.NumberTerm(json.Number(strconv.FormatInt(ns.UnixNano(), 10))),
-		Cancel:                 topdown.NewCancel(),
-		Runtime:                nil,
-		Cache:                  make(builtins.Cache),
-		Location:               nil,
-		Tracers:                nil,
-		QueryTracers:           nil,
-		QueryID:                0,
-		ParentID:               0,
-		InterQueryBuiltinCache: iqbCache,
-		PrintHook:              ph,
-		Capabilities:           capabilities,
-	}
-
-}
-func newBuiltinContainer(m Module) *builtinContainer {
-	bc := builtinContainer{}
-	bc.builtinIdMap = newBuiltinTable(m)
-	bc.e = exports{val_dump: m.module.ExportedFunction("opa_value_dump").Call, val_parse: m.module.ExportedFunction("opa_value_parse").Call, json_dump: m.module.ExportedFunction("opa_json_dump").Call, malloc: m.module.ExportedFunction("opa_malloc").Call}
-	bc.ctx = &topdown.BuiltinContext{
-		Context:      m.ctx,
-		Metrics:      metrics.New(),
-		Seed:         rand.Reader,
-		Time:         ast.NumberTerm(json.Number(strconv.FormatInt(time.Now().UnixNano(), 10))),
-		Cancel:       topdown.NewCancel(),
-		Runtime:      nil,
-		Cache:        make(builtins.Cache),
-		Location:     nil,
-		Tracers:      nil,
-		QueryTracers: nil,
-		QueryID:      0,
-		ParentID:     0,
-	}
-	return &bc
 }
--- internal/wasm/sdk/internal/wazero/module.go
+++ internal/wasm/sdk/internal/wazero/module.go
@@ -1,16 +1,24 @@
 package wazero
 
 import (
+	bs "bytes"
 	"context"
-	"io"
-	"log"
-	"time"
-
+	"crypto/rand"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/metrics"
+	"github.com/open-policy-agent/opa/topdown"
+	"github.com/open-policy-agent/opa/topdown/builtins"
 	"github.com/open-policy-agent/opa/topdown/cache"
 	"github.com/open-policy-agent/opa/topdown/print"
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/api"
+	"io"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type moduleOpts struct {
@@ -23,38 +31,129 @@ type moduleOpts struct {
 
 //wrapper for wazero policy module and environment module
 type Module struct {
-	module               api.Module
-	env                  environment
-	ctx                  context.Context
-	vm                   *VM
-	entrypointT          map[string]int32
-	evalOneOff           func(context.Context, ...uint64) ([]uint64, error)
-	eval                 func(context.Context, ...uint64) ([]uint64, error)
-	evalCtxGetResult     func(context.Context, ...uint64) ([]uint64, error)
-	evalCtxNew           func(context.Context, ...uint64) ([]uint64, error)
-	evalCtxSetData       func(context.Context, ...uint64) ([]uint64, error)
-	evalCtxSetInput      func(context.Context, ...uint64) ([]uint64, error)
-	evalCtxSetEntrypoint func(context.Context, ...uint64) ([]uint64, error)
-	heapPtrGet           func(context.Context, ...uint64) ([]uint64, error)
-	heapPtrSet           func(context.Context, ...uint64) ([]uint64, error)
-	jsonDump             func(context.Context, ...uint64) ([]uint64, error)
-	jsonParse            func(context.Context, ...uint64) ([]uint64, error)
-	valueDump            func(context.Context, ...uint64) ([]uint64, error)
-	valueParse           func(context.Context, ...uint64) ([]uint64, error)
-	malloc               func(context.Context, ...uint64) ([]uint64, error)
-	free                 func(context.Context, ...uint64) ([]uint64, error)
-	valueAddPath         func(context.Context, ...uint64) ([]uint64, error)
-	valueRemovePath      func(context.Context, ...uint64) ([]uint64, error)
+	module, env            api.Module
+	ctx, ctxt              context.Context
+	tCTX                   *topdown.BuiltinContext
+	vm                     *VM
+	maxMemSize, minMemSize int
+	builtinT               map[int32]topdown.BuiltinFunc
+	entrypointT            map[string]int32
+	seed                   io.Reader
+	ns                     time.Time
+	iqbCache               cache.InterQueryCache
+	ph                     print.Hook
+	capabilities           *ast.Capabilities
 }
 
 // Env is a wasm module that holds the shared memory buffer and the builtin bindings
+func (m *Module) newEnv(opts moduleOpts, r wazero.Runtime) (api.Module, error) {
+	if opts.maxMemSize == (moduleOpts{}).maxMemSize {
 
+		return r.NewModuleBuilder("env").
+			ExportFunction("opa_abort", m.opaAbort).
+			ExportFunction("opa_builtin0", m.C0).
+			ExportFunction("opa_builtin1", m.C1).
+			ExportFunction("opa_builtin2", m.C2).
+			ExportFunction("opa_builtin3", m.C3).
+			ExportFunction("opa_builtin4", m.C4).
+			ExportFunction("opa_println", m.opaPrintln).
+			ExportMemory("memory", uint32(opts.minMemSize)).
+			Instantiate(opts.ctx, r)
+	}
+	return r.NewModuleBuilder("env").
+		ExportFunction("opa_abort", m.opaAbort).
+		ExportFunction("opa_builtin0", m.C0).
+		ExportFunction("opa_builtin1", m.C1).
+		ExportFunction("opa_builtin2", m.C2).
+		ExportFunction("opa_builtin3", m.C3).
+		ExportFunction("opa_builtin4", m.C4).
+		ExportFunction("opa_println", m.opaPrintln).
+		ExportMemoryWithMax("memory", uint32(opts.minMemSize), uint32(opts.maxMemSize)).
+		Instantiate(opts.ctx, r)
+
+}
 func (m *Module) GetEntrypoints() map[string]int32 {
 	eLoc := m.entrypoints(m.ctx)
 	return parseJsonString(m.fromRegoJSON(eLoc))
 }
+func (m *Module) opaAbort(ptr int32) {
+	data := m.readFrom(ptr)
+	n := bs.IndexByte(data, 0)
+	if n < 0 {
+		panic("invalid abort argument")
+	}
+	panic(abortError{message: string(data[:n])})
+}
 
 // calls the built-in functions
+func (m *Module) Call(id, ctx int32, args ...int32) int32 {
+	done := make(chan struct{})
+	defer close(done)
+	go func() {
+		select {
+		case <-done:
+		case <-m.tCTX.Context.Done():
+			m.tCTX.Cancel.Cancel()
+		}
+	}()
+	var output *ast.Term
+	pArgs := []*ast.Term{}
+	for _, ter := range args {
+		serialized, err := m.value_dump(m.ctx, (ter))
+		if err != nil {
+			panic(builtinError{err: err})
+		}
+		data := m.readStr(uint32(serialized))
+		pTer, err := ast.ParseTerm(string(data))
+		if err != nil {
+			panic(builtinError{err: err})
+		}
+		pArgs = append(pArgs, pTer)
+	}
+	err := m.builtinT[id](*m.tCTX, pArgs, func(t *ast.Term) error {
+		output = t
+		return nil
+	})
+	if err != nil {
+		if errors.As(err, &topdown.Halt{}) {
+			var e *topdown.Error
+			if errors.As(err, &e) && e.Code == topdown.CancelErr {
+
+				panic(cancelledError{message: e.Message})
+			}
+			panic(builtinError{err: err})
+		}
+		// non-halt errors are treated as undefined ("non-strict eval" is the only
+		// mode in wasm), the `output == nil` case below will return NULL
+	}
+	if output == nil {
+		return 0
+	}
+	outB := []byte(output.String())
+	loc := m.writeMem(outB)
+	addr, err := m.value_parse(m.ctx, int32(loc), int32(len(outB)))
+	if err != nil {
+		panic(err)
+	}
+	return int32(addr)
+}
+
+// Exported to wasm and executes Call with the given builtin_id and arguments
+func (m *Module) C0(id, ctx int32) int32 {
+	return m.Call(id, ctx)
+}
+func (m *Module) C1(id, ctx, a1 int32) int32 {
+	return m.Call(id, ctx, a1)
+}
+func (m *Module) C2(id, ctx, a1, a2 int32) int32 {
+	return m.Call(id, ctx, a1, a2)
+}
+func (m *Module) C3(id, ctx, a1, a2, a3 int32) int32 {
+	return m.Call(id, ctx, a1, a2, a3)
+}
+func (m *Module) C4(id, ctx, a1, a2, a3, a4 int32) int32 {
+	return m.Call(id, ctx, a1, a2, a3, a4)
+}
 
 // resets the Builtin Context
 func (m *Module) Reset(ctx context.Context,
@@ -63,64 +162,116 @@ func (m *Module) Reset(ctx context.Context,
 	iqbCache cache.InterQueryCache,
 	ph print.Hook,
 	capabilities *ast.Capabilities) {
-	m.env.builtins.Reset(ctx, seed, ns, iqbCache, ph, capabilities)
+	if ns.IsZero() {
+		ns = time.Now()
+	}
+	if seed == nil {
+		seed = rand.Reader
+	}
+	m.ctxt = ctx
+	m.seed = seed
+	m.capabilities = capabilities
+	m.ns = ns
+	m.ph = ph
+	m.iqbCache = iqbCache
+	m.tCTX = &topdown.BuiltinContext{
+		Context:                ctx,
+		Metrics:                metrics.New(),
+		Seed:                   seed,
+		Time:                   ast.NumberTerm(json.Number(strconv.FormatInt(ns.UnixNano(), 10))),
+		Cancel:                 topdown.NewCancel(),
+		Runtime:                nil,
+		Cache:                  make(builtins.Cache),
+		Location:               nil,
+		Tracers:                nil,
+		QueryTracers:           nil,
+		QueryID:                0,
+		ParentID:               0,
+		InterQueryBuiltinCache: iqbCache,
+		PrintHook:              ph,
+		Capabilities:           capabilities,
+	}
+}
+func (m *Module) opaPrintln(ptr int32) {
+
+	bytes := []byte{}
+	var index uint32 = 0
+	for ok := true; ok; {
+		b := m.readMemByte(uint32(ptr) + index)
+		if b == 0b0 {
+			ok = false
+		} else {
+			bytes = append(bytes, b)
+		}
+		index++
+	}
+	out := ""
+	for _, b := range bytes {
+		out += string(b)
+	}
+	fmt.Println(out)
 
 }
-
 func newModule(opts moduleOpts, r wazero.Runtime) Module {
 	m := Module{}
 	m.vm = opts.vm
 	m.ctx = opts.ctx
 	var err error
 
-	m.env, err = newEnv(&m, opts, r)
+	m.env, err = m.newEnv(opts, r)
+	m.minMemSize, m.maxMemSize = opts.minMemSize, opts.maxMemSize
 	if err != nil {
-		log.Panic(err)
+		panic(err)
 	}
 	m.module, err = r.InstantiateModuleFromBinary(opts.ctx, opts.policy)
 	if err != nil {
-		log.Panic(err)
+		panic(err)
 	}
+	m.tCTX = &topdown.BuiltinContext{Context: context.Background(), Metrics: metrics.New(),
+		Time:  ast.NumberTerm(json.Number(strconv.FormatInt(time.Now().UnixNano(), 10))),
+		Cache: make(builtins.Cache)}
+	m.builtinT = newBuiltinTable(m)
 	m.entrypointT = m.GetEntrypoints()
-	m.env.setBuiltins()
-	m.evalOneOff = m.module.ExportedFunction("opa_eval").Call
-	m.eval = m.module.ExportedFunction("eval").Call
-	m.evalCtxGetResult = m.module.ExportedFunction("opa_eval_ctx_get_result").Call
-	m.evalCtxNew = m.module.ExportedFunction("opa_eval_ctx_new").Call
-	m.evalCtxSetData = m.module.ExportedFunction("opa_eval_ctx_set_data").Call
-	m.evalCtxSetInput = m.module.ExportedFunction("opa_eval_ctx_set_input").Call
-	m.evalCtxSetEntrypoint = m.module.ExportedFunction("opa_").Call
-	m.heapPtrGet = m.module.ExportedFunction("opa_heap_ptr_get").Call
-	m.heapPtrSet = m.module.ExportedFunction("opa_heap_ptr_set").Call
-	m.jsonDump = m.module.ExportedFunction("opa_json_dump").Call
-	m.jsonParse = m.module.ExportedFunction("opa_json_parse").Call
-	m.valueDump = m.module.ExportedFunction("opa_value_dump").Call
-	m.valueParse = m.module.ExportedFunction("opa_value_parse").Call
-	m.malloc = m.module.ExportedFunction("opa_malloc").Call
-	m.free = m.module.ExportedFunction("opa_free").Call
-	m.valueAddPath = m.module.ExportedFunction("opa_value_add_path").Call
-	m.valueRemovePath = m.module.ExportedFunction("opa_value_remove_path").Call
 	return m
 }
 
 // reads the shared memory buffer
 func (m *Module) readMem(offset, length uint32) []byte {
-	return m.env.readMem(offset, length)
+	data, _ := m.env.Memory().Read(m.ctx, offset, length)
+	return data
 }
 
 //reads a single byte from the shared memory buffer
 func (m *Module) readMemByte(offset uint32) byte {
-	return m.env.readMemByte(offset)
+	data, _ := m.env.Memory().ReadByte(m.ctx, offset)
+	return data
 }
 
 //writes data to a given point in memory, grows if necessary
 func (m *Module) writeMemPlus(wAddr uint32, wData []byte, caller string) error {
-	return m.env.writeMemPlus(wAddr, wData, caller)
+	dataLeft := (m.env.Memory().Size(m.ctx)) - wAddr
+	finPtrLoc := wAddr + uint32(len(wData))
+	if (m.env.Memory().Size(m.ctx)) < finPtrLoc { // need to grow memory
+
+		delta := uint32(len(wData)) - dataLeft
+		_, success := m.env.Memory().Grow(m.ctx, Pages(uint32(delta)))
+		if !success {
+			return fmt.Errorf("%s: failed to grow memory by `%d` (max pages %d)", caller, Pages(delta), m.maxMemSize)
+		}
+	}
+	m.env.Memory().Write(m.ctx, wAddr, wData)
+	return nil
 }
 
 //allocates and writes data to the shared memory buffer
 func (m *Module) writeMem(data []byte) uint32 {
-	return m.env.writeMem(data)
+	addr, err := m.malloc(m.ctx, int32(len(data)))
+	if err != nil {
+		panic("internal_error: opa_malloc: failed")
+	}
+	m.env.Memory().Write(m.ctx, uint32(addr), data)
+
+	return uint32(addr)
 }
 
 //reads a null terminated string starting at the given address in the shared memory buffer
@@ -143,153 +294,159 @@ func (m *Module) readStr(loc uint32) string {
 	return out
 }
 func (m *Module) fromRegoJSON(addr int32) string {
-	dump_addr, err := m.jsonDump(m.ctx, uint64(addr))
+	dump_addr, err := m.json_dump(m.ctx, addr)
 	if err != nil {
-		log.Panic(err)
+		panic(err)
 	}
-	str := m.readStr(uint32(dump_addr[0]))
+	str := m.readStr(uint32(dump_addr))
 	return str
 }
 
 //Reads and returns the shared memory buffer from the given address and stops when it reaches the terminator byte or reaches the end of the buffer
 func (m *Module) readUntil(addr int32, terminator byte) []byte {
-	return m.env.readUntil(addr, terminator)
+	out := []byte{}
+	for i, j := addr, true; j; i++ {
+		_, j = m.env.Memory().Read(m.ctx, uint32(i), 1)
+		if m.readMemByte(uint32(i)) == terminator {
+			return out
+		}
+		out = append(out, m.readMemByte(uint32(i)))
+	}
+	return out
 }
 
 //reads the shared memory buffer from the given address to the end
 func (m *Module) readFrom(addr int32) []byte {
-	return m.env.readFrom(addr)
+	out := []byte{}
+	for i, j := addr, true; j; i++ {
+		_, j = m.env.Memory().Read(m.ctx, 0, uint32(i))
+		if j {
+			out = append(out, m.readMemByte(uint32(i)))
+		}
+	}
+	return out
 }
 
-func (m Module) wasm_abi_version() int32 {
-	return int32(m.module.ExportedGlobal("opa_wasm_abi_version").Get(m.ctx))
-}
-func (m Module) wasm_abi_minor_version() int32 {
-	return int32(m.module.ExportedGlobal("opa_wasm_abi_minor_version").Get(m.ctx))
-}
-func (m Module) builtins(ctx context.Context) int32 {
-	addr, _ := m.module.ExportedFunction("builtins").Call(ctx)
-	return int32(addr[0])
-}
-func (m Module) entrypoints(ctx context.Context) int32 {
-	addr, _ := m.module.ExportedFunction("entrypoints").Call(ctx)
-	return int32(addr[0])
-}
-
-/*
 //
 // Expose the exported wasm functions for ease of use
 //
-func (m Module) eval(ctx context.Context, ctx_addr int32) error {
+func (m *Module) wasm_abi_version() int32 {
+	return int32(m.module.ExportedGlobal("opa_wasm_abi_version").Get(m.ctx))
+}
+func (m *Module) wasm_abi_minor_version() int32 {
+	return int32(m.module.ExportedGlobal("opa_wasm_abi_minor_version").Get(m.ctx))
+}
+func (m *Module) eval(ctx context.Context, ctx_addr int32) error {
 	_, err := m.module.ExportedFunction("eval").Call(ctx, uint64(ctx_addr))
 	return err
 }
-func (m Module) eval_ctx_new(ctx context.Context) (int32, error) {
+func (m *Module) builtins(ctx context.Context) int32 {
+	addr, _ := m.module.ExportedFunction("builtins").Call(ctx)
+	return int32(addr[0])
+}
+func (m *Module) entrypoints(ctx context.Context) int32 {
+	addr, _ := m.module.ExportedFunction("entrypoints").Call(ctx)
+	return int32(addr[0])
+}
+func (m *Module) eval_ctx_new(ctx context.Context) (int32, error) {
 	addr, err := m.module.ExportedFunction("opa_eval_ctx_new").Call(ctx)
 	if err != nil {
 		return 0, err
 	}
 	return int32(addr[0]), err
 }
-func (m Module) eval_ctx_set_input(ctx context.Context, ctx_addr, value_addr int32) error {
+func (m *Module) eval_ctx_set_input(ctx context.Context, ctx_addr, value_addr int32) error {
 	_, err := m.module.ExportedFunction("opa_eval_ctx_set_input").Call(ctx, uint64(ctx_addr), uint64(value_addr))
 	return err
 }
-func (m Module) eval_ctx_set_data(ctx context.Context, ctx_addr, value_addr int32) error {
+func (m *Module) eval_ctx_set_data(ctx context.Context, ctx_addr, value_addr int32) error {
 	_, err := m.module.ExportedFunction("opa_eval_ctx_set_data").Call(ctx, uint64(ctx_addr), uint64(value_addr))
 	return err
 }
-func (m Module) eval_ctx_set_entrypoint(ctx context.Context, ctx_addr, entrypoint_id int32) error {
+func (m *Module) eval_ctx_set_entrypoint(ctx context.Context, ctx_addr, entrypoint_id int32) error {
 	_, err := m.module.ExportedFunction("opa_eval_ctx_set_data").Call(ctx, uint64(ctx_addr), uint64(entrypoint_id))
 	return err
 }
-func (m Module) eval_ctx_get_result(ctx context.Context, ctx_addr int32) (int32, error) {
+func (m *Module) eval_ctx_get_result(ctx context.Context, ctx_addr int32) (int32, error) {
 	addr, err := m.module.ExportedFunction("opa_eval_ctx_get_result").Call(ctx, uint64(ctx_addr))
 	if err != nil {
 		return 0, err
 	}
 	return int32(addr[0]), err
 }
-func (m Module) malloc(ctx context.Context, size int32) (int32, error) {
+func (m *Module) malloc(ctx context.Context, size int32) (int32, error) {
 	addr, err := m.module.ExportedFunction("opa_malloc").Call(ctx, uint64(size))
 	if err != nil {
 		return 0, errors.New("internal_error: opa_malloc: failed")
 	}
 	return int32(addr[0]), err
 }
-func (m Module) free(ctx context.Context, addr int32) error {
+func (m *Module) free(ctx context.Context, addr int32) error {
 	_, err := m.module.ExportedFunction("opa_free").Call(ctx, uint64(addr))
 	return err
 }
-func (m Module) json_parse(ctx context.Context, str_addr, size int32) (int32, error) {
+func (m *Module) json_parse(ctx context.Context, str_addr, size int32) (int32, error) {
 	addr, err := m.module.ExportedFunction("opa_json_parse").Call(ctx, uint64(str_addr), uint64(size))
 	if err != nil {
 		return 0, err
 	}
 	return int32(addr[0]), err
 }
-func (m Module) value_parse(ctx context.Context, str_addr, size int32) (int32, error) {
+func (m *Module) value_parse(ctx context.Context, str_addr, size int32) (int32, error) {
 	addr, err := m.module.ExportedFunction("opa_value_parse").Call(ctx, uint64(str_addr), uint64(size))
 	if err != nil {
 		return 0, err
 	}
 	return int32(addr[0]), err
 }
-func (m Module) json_dump(ctx context.Context, value_addr int32) (int32, error) {
+func (m *Module) json_dump(ctx context.Context, value_addr int32) (int32, error) {
 	addr, err := m.module.ExportedFunction("opa_json_dump").Call(ctx, uint64(value_addr))
 	if err != nil {
 		return 0, err
 	}
 	return int32(addr[0]), err
 }
-func (m Module) value_dump(ctx context.Context, value_addr int32) (int32, error) {
-	log.Println(m.module.ExportedFunction("opa_value_dump"))
-	log.Println("value_dump:")
+func (m *Module) value_dump(ctx context.Context, value_addr int32) (int32, error) {
 	addr, err := m.module.ExportedFunction("opa_value_dump").Call(ctx, uint64(value_addr))
 	if err != nil {
 		return 0, err
 	}
 	return int32(addr[0]), err
 }
-func (m Module) heap_ptr_set(ctx context.Context, addr int32) error {
+func (m *Module) heap_ptr_set(ctx context.Context, addr int32) error {
 	_, err := m.module.ExportedFunction("opa_heap_ptr_set").Call(ctx, uint64(addr))
 	return err
 }
-func (m Module) heap_ptr_get(ctx context.Context) (int32, error) {
+func (m *Module) heap_ptr_get(ctx context.Context) (int32, error) {
 	addr, err := m.module.ExportedFunction("opa_heap_ptr_get").Call(ctx)
 	if err != nil {
 		return 0, err
 	}
 	return int32(addr[0]), err
 }
-func (m Module) value_add_path(ctx context.Context, base_value_addr, path_value_addr, value_addr int32) (int32, error) {
+func (m *Module) value_add_path(ctx context.Context, base_value_addr, path_value_addr, value_addr int32) (int32, error) {
 	ret, err := m.module.ExportedFunction("opa_value_add_path").Call(ctx, uint64(base_value_addr), uint64(path_value_addr), uint64(value_addr))
 	if err != nil {
 		return 0, err
 	}
 	return int32(ret[0]), err
 }
-func (m Module) value_remove_path(ctx context.Context, base_value_addr, path_value_addr int32) (int32, error) {
+func (m *Module) value_remove_path(ctx context.Context, base_value_addr, path_value_addr int32) (int32, error) {
 	ret, err := m.module.ExportedFunction("opa_value_remove_path").Call(ctx, uint64(base_value_addr), uint64(path_value_addr))
 	if err != nil {
 		return 0, err
 	}
 	return int32(ret[0]), err
 }
-func (m Module) opa_eval(ctx context.Context, entrypoint_id, data, input, input_len, heap_ptr int32) (int32, error) {
+func (m *Module) opa_eval(ctx context.Context, entrypoint_id, data, input, input_len, heap_ptr int32) (int32, error) {
 	addr, err := m.module.ExportedFunction("opa_eval").Call(ctx, 0, uint64(entrypoint_id), uint64(data), uint64(input), uint64(input_len), uint64(heap_ptr), 0)
 	if err != nil {
-		str := err.Error()
-		if str[0] == '{' {
-			str = str[1:]
-		}
-		var end int
-		end = strings.Index(str, " (recovered")
-		if str[end-1] == '}' {
-			end--
+		str := err.Error()[1:]
+		end := strings.Index(str, "} (recovered")
+		if end < 0 {
+			return 0, err
 		}
 		return 0, fmt.Errorf("internal_error: %s", str[:end])
 	}
 	return int32(addr[0]), err
 }
-*/
