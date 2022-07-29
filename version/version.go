2e693697359d73095f6f8303e5a07d420048ba19
Philip Conrad <philipaconrad@gmail.com>
2022-07-29 16:45:16 -0400

    Prepare v0.44.0 development
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>

--- version/version.go
+++ version/version.go
@@ -10,7 +10,7 @@ import (
 )
 
 // Version is the canonical version of OPA.
-var Version = "0.43.0"
+var Version = "0.44.0-dev"
 
 // GoVersion is the version of Go this was built with
 var GoVersion = runtime.Version()
