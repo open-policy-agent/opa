94aef7f9a7f6eb9b17cfba72a74eac4eba91b1c2
Philip Conrad <philipaconrad@gmail.com>
2022-09-16 14:03:38 -0400

    docs+build: Restore diff-against-main behavior for PRs.
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>

--- build/docs-build-needed.sh
+++ build/docs-build-needed.sh
@@ -2,7 +2,13 @@
 
 set -exo pipefail
 
+if [ "$PULL_REQUEST" == "true" ]; then
+  BASE=main
+else
+  BASE=$CACHED_COMMIT_REF
+fi
+
 # NOTE(sr): we include version because that's what drives releases
 # Makefile and netlify.toml capture when the build infrastructure changes
 # ast/builtins.go and capabilities.json are driving the builtins_metadata.
-git diff --exit-code $CACHED_COMMIT_REF $COMMIT_REF docs/ Makefile build/ netlify.toml ast/builtins.go capabilities.json version/
+git diff --exit-code $BASE $COMMIT_REF docs/ Makefile build/ netlify.toml ast/builtins.go capabilities.json version/
