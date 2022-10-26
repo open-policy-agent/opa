801b3387256bf37ba4bc62319429a50ebf3fc362
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2022-10-26 13:52:04 +0000

    chore: moving unit test to test/cases/testdata
    moving net.cidr_is_valid test cases to the test/cases/testdata
    directory.
    
    Signed-off-by: Ricardo Maraschini <ricardo.maraschini@gmail.com>

--- topdown/cidr_test.go
+++ topdown/cidr_test.go
@@ -2,7 +2,6 @@ package topdown
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
@@ -44,76 +43,4 @@ func TestNetCIDRExpandCancellation(t *testing.T) {
 		t.Fatalf("Expected cancel error but got: %v (err: %v)", qrs, err)
 	}
 
-}
-
-func TestNetCIDRIsValid(t *testing.T) {
-	ctx := context.Background()
-	store := inmem.New()
-	txn := storage.NewTransactionOrDie(ctx, store)
-
-	for _, tt := range []struct {
-		name     string
-		cidr     string
-		expected bool
-	}{
-		{
-			name:     "valid ipv4 cidr",
-			cidr:     `192.168.1.0/24`,
-			expected: true,
-		},
-		{
-			name:     "empty cidr",
-			cidr:     "",
-			expected: false,
-		},
-		{
-			name:     "string",
-			cidr:     "there goes a string",
-			expected: false,
-		},
-		{
-			name:     "valid ipv4 address",
-			cidr:     `192.168.1.2`,
-			expected: false,
-		},
-		{
-			name:     "valid ipv6 cidr",
-			cidr:     `2002::1234:abcd:ffff:c0a8:101/64`,
-			expected: true,
-		},
-	} {
-		t.Run(tt.name, func(t *testing.T) {
-			module := fmt.Sprintf(
-				`
-				package test
-				default valid = false
-				valid { net.cidr_is_valid(%q) }
-				`,
-				tt.cidr,
-			)
-
-			compiler := compileModules([]string{module})
-
-			query := NewQuery(ast.MustParseBody("data.test.valid = x")).
-				WithCompiler(compiler).
-				WithStore(store).
-				WithTransaction(txn)
-
-			qrs, err := query.Run(ctx)
-			if err != nil {
-				t.Fatalf("unexpected error: %s", err)
-				return
-			}
-
-			if len(qrs) != 1 {
-				t.Fatalf("unexpected query result set size: %+v", qrs)
-				return
-			}
-
-			val := qrs[0][ast.Var("x")].String()
-			if fmt.Sprintf("%v", tt.expected) != val {
-				t.Errorf("expected %v, received %v", tt.expected, val)
-			}
-		})
-	}
 }
