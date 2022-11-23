d686e361659ec32be746ce69829a8cfdab5c6805
Philip Conrad <philipaconrad@gmail.com>
2022-11-23 14:41:53 -0500

    docs: Document reference timestamp behavior for `time.parse_ns`
    This commit adds documentation and examples around one of the bigger
    "gotchas" in handling time values in OPA: parsing date formats.
    
    The issue is that users of `time.parse_ns` have to use the same
    "reference timestamp" values as Go's `time/format` module does, which
    can be deeply surprising behavior to newcomers to the Go language,
    and to OPA users as well.
    
    By documenting this, the hope is that we'll help users make fewer
    mistakes around parsing date formats in OPA.
    
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>

--- ast/builtins.go
+++ ast/builtins.go
@@ -2136,7 +2136,7 @@ var ParseDurationNanos = &Builtin{
 	Description: "Returns the duration in nanoseconds represented by a string.",
 	Decl: types.NewFunction(
 		types.Args(
-			types.Named("duration", types.S).Description("a duration like \"3m\"; seethe [Go `time` package documentation](https://golang.org/pkg/time/#ParseDuration) for more details"),
+			types.Named("duration", types.S).Description("a duration like \"3m\"; see the [Go `time` package documentation](https://golang.org/pkg/time/#ParseDuration) for more details"),
 		),
 		types.Named("ns", types.N).Description("the `duration` in nanoseconds"),
 	),
