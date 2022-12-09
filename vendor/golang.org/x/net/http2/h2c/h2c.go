007ab859af2358c30c2dac71253c889ff8f73d0a
Stephan Renatus <stephan.renatus@gmail.com>
2022-12-09 10:18:57 +0100

    build(deps): bump golang.org/x/net to v0.4.0 (#5464)
    This fixes the latest finding of govulncheck -- we had thought GO-2022-1144
    was fully addressed by updating golang to 1.19.4, but it seems like that was
    NOT the case:
    
     Vulnerability #1: GO-2022-1144
      An attacker can cause excessive memory growth in a Go server
      accepting HTTP/2 requests. HTTP/2 server connections contain a
      cache of HTTP header keys sent by the client. While the total
      number of entries in this cache is capped, an attacker sending
      very large keys can cause the server to allocate approximately
      64 MiB per open connection.
    
      Call stacks in your code:
    Error:       server/server.go:477:18: github.com/open-policy-agent/opa/server.baseHTTPListener.ListenAndServe calls net/http.Server.Serve, which eventually calls golang.org/x/net/http2.Server.ServeConn
    
      Found in: golang.org/x/net/http2@v0.0.0-20220909164309-bea034e7d591
      Fixed in: golang.org/x/net/http2@v0.4.0
      More info: https://pkg.go.dev/vuln/GO-2022-1144
    
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- vendor/golang.org/x/net/http2/h2c/h2c.go
+++ vendor/golang.org/x/net/http2/h2c/h2c.go
@@ -70,6 +70,15 @@ func NewHandler(h http.Handler, s *http2.Server) http.Handler {
 	}
 }
 
+// extractServer extracts existing http.Server instance from http.Request or create an empty http.Server
+func extractServer(r *http.Request) *http.Server {
+	server, ok := r.Context().Value(http.ServerContextKey).(*http.Server)
+	if ok {
+		return server
+	}
+	return new(http.Server)
+}
+
 // ServeHTTP implement the h2c support that is enabled by h2c.GetH2CHandler.
 func (s h2cHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Handle h2c with prior knowledge (RFC 7540 Section 3.4)
@@ -87,6 +96,7 @@ func (s h2cHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		defer conn.Close()
 		s.s.ServeConn(conn, &http2.ServeConnOpts{
 			Context:          r.Context(),
+			BaseConfig:       extractServer(r),
 			Handler:          s.Handler,
 			SawClientPreface: true,
 		})
@@ -99,11 +109,13 @@ func (s h2cHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			if http2VerboseLogs {
 				log.Printf("h2c: error h2c upgrade: %v", err)
 			}
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		defer conn.Close()
 		s.s.ServeConn(conn, &http2.ServeConnOpts{
 			Context:        r.Context(),
+			BaseConfig:     extractServer(r),
 			Handler:        s.Handler,
 			UpgradeRequest: r,
 			Settings:       settings,
@@ -156,7 +168,10 @@ func h2cUpgrade(w http.ResponseWriter, r *http.Request) (_ net.Conn, settings []
 		return nil, nil, errors.New("h2c: connection does not support Hijack")
 	}
 
-	body, _ := io.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, nil, err
+	}
 	r.Body = io.NopCloser(bytes.NewBuffer(body))
 
 	conn, rw, err := hijacker.Hijack()
