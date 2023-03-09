fbfa7050631a69b08ff63a4c78227e4476272058
Charlie Egan <charlie@styra.com>
2023-03-09 09:49:30 +0000

    [ast/opa parse] Support marshalling of all ast location data (#5576)
    Signed-off-by: Charlie Egan <charlie@styra.com>

--- cmd/parse.go
+++ cmd/parse.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -23,10 +24,14 @@ const (
 	parseFormatJSON   = "json"
 )
 
-var parseParams = struct {
-	format *util.EnumFlag
-}{
-	format: util.NewEnumFlag(parseFormatPretty, []string{parseFormatPretty, parseFormatJSON}),
+type parseParams struct {
+	format      *util.EnumFlag
+	jsonInclude string
+}
+
+var configuredParseParams = parseParams{
+	format:      util.NewEnumFlag(parseFormatPretty, []string{parseFormatPretty, parseFormatJSON}),
+	jsonInclude: "",
 }
 
 var parseCommand = &cobra.Command{
@@ -40,30 +45,72 @@ var parseCommand = &cobra.Command{
 		return nil
 	},
 	Run: func(_ *cobra.Command, args []string) {
-		os.Exit(parse(args, os.Stdout, os.Stderr))
+		os.Exit(parse(args, &configuredParseParams, os.Stdout, os.Stderr))
 	},
 }
 
-func parse(args []string, stdout io.Writer, stderr io.Writer) int {
+func parse(args []string, params *parseParams, stdout io.Writer, stderr io.Writer) int {
 	if len(args) == 0 {
 		return 0
 	}
 
-	result, err := loader.RegoWithOpts(args[0], ast.ParserOptions{ProcessAnnotation: true})
+	exposeLocation := false
+	exposeComments := true
+	for _, opt := range strings.Split(params.jsonInclude, ",") {
+		value := true
+		if strings.HasPrefix(opt, "-") {
+			value = false
+		}
 
-	switch parseParams.format.String() {
+		if strings.HasSuffix(opt, "locations") {
+			exposeLocation = value
+		}
+		if strings.HasSuffix(opt, "comments") {
+			exposeComments = value
+		}
+	}
+
+	parserOpts := ast.ParserOptions{ProcessAnnotation: true}
+	if exposeLocation {
+		parserOpts.JSONOptions = &ast.JSONOptions{
+			MarshalOptions: ast.JSONMarshalOptions{
+				IncludeLocation: ast.NodeToggle{
+					Term:           true,
+					Package:        true,
+					Comment:        true,
+					Import:         true,
+					Rule:           true,
+					Head:           true,
+					Expr:           true,
+					SomeDecl:       true,
+					Every:          true,
+					With:           true,
+					Annotations:    true,
+					AnnotationsRef: true,
+				},
+			},
+		}
+	}
+
+	result, err := loader.RegoWithOpts(args[0], parserOpts)
+	if err != nil {
+		_ = pr.JSON(stderr, pr.Output{Errors: pr.NewOutputErrors(err)})
+		return 1
+	}
+
+	if !exposeComments {
+		result.Parsed.Comments = nil
+	}
+
+	switch params.format.String() {
 	case parseFormatJSON:
+		bs, err := json.MarshalIndent(result.Parsed, "", "  ")
 		if err != nil {
 			_ = pr.JSON(stderr, pr.Output{Errors: pr.NewOutputErrors(err)})
 			return 1
 		}
 
-		bs, err := json.MarshalIndent(result.Parsed, "", "  ")
-		if err != nil {
-			fmt.Fprintln(stderr, err)
-			return 1
-		}
-		fmt.Println(string(bs))
+		fmt.Fprint(stdout, string(bs)+"\n")
 	default:
 		if err != nil {
 			fmt.Fprintln(stderr, err)
@@ -76,6 +123,8 @@ func parse(args []string, stdout io.Writer, stderr io.Writer) int {
 }
 
 func init() {
-	parseCommand.Flags().VarP(parseParams.format, "format", "f", "set output format")
+	parseCommand.Flags().VarP(configuredParseParams.format, "format", "f", "set output format")
+	parseCommand.Flags().StringVarP(&configuredParseParams.jsonInclude, "json-include", "", "", "include or exclude optional elements. By default comments are included. Current options: locations, comments. E.g. --json-include locations,-comments will include locations and exclude comments.")
+
 	RootCommand.AddCommand(parseCommand)
 }
