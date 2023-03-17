76e5fda8b79a190d609f8f7aee1c1f349f13f1f2
Gianluca Oldani <oldanigianluca@gmail.com>
2023-03-17 11:08:53 +0100

    fmt: report wrong arity for built-in functions This commit fixes the index out of range error discussed in #5646 and adds error handling to avoid that `fmt` panics when such errors are encountered by the formatter
    The error handling procedure introduced is similar to the one used by the Scanner struct
    responsible for the parsing of a `rego` file, since it uses a slice which is filled with
    all the eventual errors that can be found during the format procedure
    
    Fixes: #5646
    Signed-off-by: Gianluca Oldani <oldanigianluca@gmail.com>

--- cmd/fmt_test.go
+++ cmd/fmt_test.go
@@ -2,12 +2,15 @@ package cmd
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/format"
 	"github.com/open-policy-agent/opa/util/test"
 )
 
@@ -28,6 +31,27 @@ const unformatted = `
         }
 
 
+`
+
+const singleWrongArity = `package test
+
+p {
+	a := 1
+	b := 2
+	plus(a, b, c) == 3
+}
+`
+
+const MultipleWrongArity = `package test
+
+p {
+	x:=5
+	y:=7
+	z:=6
+	plus([x, y]) == 3
+	and(true, false, false) == false
+	plus(a, x, y, z)
+}
 `
 
 func TestFmtFormatFile(t *testing.T) {
@@ -266,6 +290,92 @@ func TestFmtFailFileChangesDiff(t *testing.T) {
 		actual := strings.TrimSpace(stdout.String())
 		if len(actual) == 0 {
 			t.Fatalf("Expected output, got:\n%v\n\n", actual)
+		}
+	})
+}
+
+func TestFmtSingleWrongArityError(t *testing.T) {
+	params := fmtCommandParams{}
+	var stdout bytes.Buffer
+
+	files := map[string]string{
+		"policy.rego": singleWrongArity,
+	}
+
+	test.WithTempFS(files, func(path string) {
+		policyFile := filepath.Join(path, "policy.rego")
+		info, err := os.Stat(policyFile)
+		err = formatFile(&params, &stdout, policyFile, info, err)
+		if err == nil {
+			t.Fatalf("Expected error but did not receive one")
+		}
+
+		loc := ast.Location{File: policyFile, Row: 6}
+		errExp := ast.NewError(ast.TypeErr, &loc, "%s: %s", "plus", "arity mismatch")
+		errExp.Details = &format.ArityFormatErrDetail{
+			Have: []string{"var", "var", "var"},
+			Want: []string{"number", "number"},
+		}
+		expectedErrs := ast.Errors(make([]*ast.Error, 1))
+		expectedErrs[0] = errExp
+		expectedSingleWrongArityErr := newError("failed to parse Rego source file: %v", fmt.Errorf("%s: %v", policyFile, expectedErrs))
+
+		if err != expectedSingleWrongArityErr {
+			t.Fatalf("Expected:%s\n\nGot:%s\n\n", expectedSingleWrongArityErr, err)
+		}
+	})
+}
+
+func TestFmtMultipleWrongArityError(t *testing.T) {
+	params := fmtCommandParams{}
+	var stdout bytes.Buffer
+
+	files := map[string]string{
+		"policy.rego": MultipleWrongArity,
+	}
+
+	test.WithTempFS(files, func(path string) {
+		policyFile := filepath.Join(path, "policy.rego")
+		info, err := os.Stat(policyFile)
+		err = formatFile(&params, &stdout, policyFile, info, err)
+		if err == nil {
+			t.Fatalf("Expected error but did not receive one")
+		}
+
+		locations := []ast.Location{
+			{File: policyFile, Row: 7},
+			{File: policyFile, Row: 8},
+			{File: policyFile, Row: 9},
+		}
+		haveStrings := [][]string{
+			{"array"},
+			{"boolean", "boolean", "boolean"},
+			{"var", "var", "var", "var"},
+		}
+		wantStrings := [][]string{
+			{"number", "number"},
+			{"set[any]", "set[any]"},
+			{"number", "number"},
+		}
+		operators := []string{
+			"plus",
+			"and",
+			"plus",
+		}
+		expectedErrs := ast.Errors(make([]*ast.Error, 3))
+		for i := 0; i < 3; i++ {
+			loc := locations[i]
+			errExp := ast.NewError(ast.TypeErr, &loc, "%s: %s", operators[i], "arity mismatch")
+			errExp.Details = &format.ArityFormatErrDetail{
+				Have: haveStrings[i],
+				Want: wantStrings[i],
+			}
+			expectedErrs[i] = errExp
+		}
+		expectedMultipleWrongArityErr := newError("failed to parse Rego source file: %v", fmt.Errorf("%s: %v", policyFile, expectedErrs))
+
+		if err != expectedMultipleWrongArityErr {
+			t.Fatalf("Expected:%s\n\nGot:%s\n\n", expectedMultipleWrongArityErr, err)
 		}
 	})
 }
