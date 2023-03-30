7abe17861fce9d76aaa0d7fd7d5e3b511126bbe6
Charlie Egan <charlie@styra.com>
2023-03-30 11:55:42 +0100

    [docs] Fix dashboard height issue (#5799)
    * [docs] Fix dashboard hight issue
    
    https://github.com/open-policy-agent/opa/pull/5795, merged yesterday,
    contained a flawed assumption: that a warning banner was always
    displayed. This is not the case and led to the latest docs page wrapper
    height being incorrect.
    
    This changes the management of the messages and dashboard wrapper to be
    in the same place and makes a change so that the height can be correctly
    calculated regardless of the version.
    
    Signed-off-by: Charlie Egan <charlie@styra.com>

--- docs/website/assets/js/app.js
+++ docs/website/assets/js/app.js
@@ -1,6 +1,6 @@
 function initBannerVersionWarningCloseButton() {
   $(".banner-version-warning").find(".delete").click(function() {
-    $(".dashboard-wrapper").addClass("banner-version-warning-dismissed");
+    $(".dashboard-wrapper").removeClass("showing-banner-version-warning");
   });
 }
 
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     contentSelector: '.content',
     headingSelector: 'h1, h2, h3, h4, h5',
     scrollSmooth: false,
-    scrollContainer: ".dashboard-main",
+    scrollContainer: ".article",
     scrollEndCallback: function(e) {
       // Make sure the current ToC item we are on is visible in the nav bar
       $('.docs-nav-item.is-active')[0].scrollIntoView();
