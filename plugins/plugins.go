b808f9e59d629b950c0c94df0c5b01b68f4514dd
Stephan Renatus <stephan@styra.com>
2023-06-28 11:22:02 +0200

    build: add errors.Join workaround for go <1.20
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -7,7 +7,6 @@ package plugins
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"sync"
 	"time"
@@ -22,6 +21,7 @@ import (
 	"github.com/open-policy-agent/opa/hooks"
 	bundleUtils "github.com/open-policy-agent/opa/internal/bundle"
 	cfg "github.com/open-policy-agent/opa/internal/config"
+	"github.com/open-policy-agent/opa/internal/errors"
 	initload "github.com/open-policy-agent/opa/internal/runtime/init"
 	"github.com/open-policy-agent/opa/keys"
 	"github.com/open-policy-agent/opa/loader"
