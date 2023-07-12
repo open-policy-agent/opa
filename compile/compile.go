6b680fafa115f1ae50f6168ac32615e08d16aba1
Jakub Kulich <jakubkulich@gmail.com>
2023-07-12 14:38:35 +0200

    compile: bundle roots can be provided by caller
    Previously, bundle roots couldn't be provided and neither .manifest file couldn't be provided, because it was replaced by the .manifest generated by the compiler.
    
    Fixes: #6085
    
    Signed-off-by: Jakub Kulich <jakubkulich@gmail.com>

--- compile/compile.go
+++ compile/compile.go
@@ -67,6 +67,7 @@ type Compiler struct {
 	filter                       loader.Filter              // filter to apply to file loader
 	paths                        []string                   // file paths to load. TODO(tsandall): add support for supplying readers for embedded users.
 	entrypoints                  orderedStringSet           // policy entrypoints required for optimization and certain targets
+	roots                        []string                   // optionally, bundle roots can be provided
 	useRegoAnnotationEntrypoints bool                       // allow compiler to late-bind entrypoints from annotated rules in policies.
 	optimizationLevel            int                        // how aggressive should optimization be
 	target                       string                     // target type (wasm, rego, etc.)
@@ -220,6 +221,12 @@ func (c *Compiler) WithCapabilities(capabilities *ast.Capabilities) *Compiler {
 // WithMetadata sets the additional data to be included in .manifest
 func (c *Compiler) WithMetadata(metadata *map[string]interface{}) *Compiler {
 	c.metadata = metadata
+	return c
+}
+
+// WithRoots sets the roots to include in the output bundle manifest.
+func (c *Compiler) WithRoots(r ...string) *Compiler {
+	c.roots = append(c.roots, r...)
 	return c
 }
 
@@ -453,11 +460,14 @@ func (c *Compiler) initBundle() error {
 		return nil
 	}
 
-	// TODO(tsandall): add support for controlling roots. Either the caller could
-	// supply them or the compiler could infer them based on the packages and data
-	// contents. The latter would require changes to the loader to preserve the
+	// TODO(tsandall): roots could be automatically inferred based on the packages and data
+	// contents. That would require changes to the loader to preserve the
 	// locations where base documents were mounted under data.
 	result := &bundle.Bundle{}
+	if len(c.roots) > 0 {
+		result.Manifest.Roots = &c.roots
+	}
+
 	result.Manifest.Init()
 	result.Data = load.Files.Documents
 
