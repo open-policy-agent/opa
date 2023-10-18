1c69c5014020b170b1c574dc152391f508931a6d
Ashutosh Narkar <anarkar4387@gmail.com>
2023-10-18 14:14:47 -0700

    build: Migrate to GitHub CLI tool
    The `hub` tool is deprecated in favor of the GitHub CLI and
    is removed from GitHub's action runner images.
    
    Fixes: #6326
    
    Signed-off-by: Ashutosh Narkar <anarkar4387@gmail.com>

--- build/github-release.sh
+++ build/github-release.sh
@@ -31,7 +31,7 @@ done
 # Collect a list of opa binaries (expect binaries in the form: opa_<platform>_<arch>[extension])
 ASSETS=()
 for asset in "${ASSET_DIR}"/opa_*_*; do
-    ASSETS+=("-a" "$asset")
+    ASSETS+=("$asset")
 done
 
 # Gather the release notes from the CHANGELOG for the latest version
@@ -44,11 +44,10 @@ echo -e "${TAG_NAME}\n" > "${RELEASE_NOTES}"
 ./build/latest-release-notes.sh --output="${RELEASE_NOTES}"
 
 # Update or create a release on github
-if hub release show "${TAG_NAME}" > /dev/null; then
+if gh release view "${TAG_NAME}" --repo open-policy-agent/opa > /dev/null; then
     # Occurs when the tag is created via GitHub UI w/ a release
-    # Use -m "" to preserve the existing text.
-    hub release edit "${ASSETS[@]}" -m "" "${TAG_NAME}"
+    gh release upload "${TAG_NAME}" "${ASSETS[@]}" --repo open-policy-agent/opa
 else
     # Create a draft release
-    hub release create "${ASSETS[@]}" -F ${RELEASE_NOTES} --draft "${TAG_NAME}"
+    gh release create "${TAG_NAME}" "${ASSETS[@]}" -F ${RELEASE_NOTES} --draft --title "${TAG_NAME}" --repo open-policy-agent/opa
 fi
