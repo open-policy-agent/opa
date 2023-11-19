0c82a1c2bde33ca765626938140f112438dfe451
Colin Lacy <colinjlacy@gmail.com>
2023-11-19 09:01:07 -0600

    plugins/rest: masks X-AMZ-SECURITY-TOKEN header in decision logs
    Decision logs had previously been configured to hide the value of the
    Authorization header, as that is considered sensitive information.
    However, there are cases when additional headers are provided that
    contain sensitive information, such as the X-AMZ-SECURITY-TOKEN header.
    This PR creates an internal map of headers that should be masked, which
    can be expanded if additional headers are required. It then loops over
    the headers in a request, and performs a lookup on the internal map
    to see if any of them match those that should be masked. If so, it
    replaces their values with "REDACTED". An existing test was added to
    check both the header keys that should be masked, as well as a key
    that should not.
    
    Additional work, out of scope for this PR, would be to open a config
    setting that would allow users to pass in a list of headers that should
    be masked.
    
    Fixes: #5848
    
    Signed-off-by: Colin Lacy <colinjlacy@gmail.com>

--- plugins/rest/rest_test.go
+++ plugins/rest/rest_test.go
@@ -1881,6 +1881,7 @@ func TestAWSCredentialServiceChain(t *testing.T) {
 
 func TestDebugLoggingRequestMaskAuthorizationHeader(t *testing.T) {
 	token := "secret"
+	plaintext := "plaintext"
 	ts := testServer{t: t, expBearerToken: token}
 	ts.start()
 	defer ts.stop()
@@ -1892,8 +1893,12 @@ func TestDebugLoggingRequestMaskAuthorizationHeader(t *testing.T) {
 			"bearer": {
 				"token": %q
 			}
+		},
+		"headers": {
+			"X-AMZ-SECURITY-TOKEN": "secret",
+			"remains-unmasked": %q
 		}
-	}`, ts.server.URL, token)
+	}`, ts.server.URL, token, plaintext)
 	client, err := New([]byte(config), map[string]*keys.Config{})
 	if err != nil {
 		t.Fatalf("Unexpected error: %v", err)
@@ -1912,11 +1917,15 @@ func TestDebugLoggingRequestMaskAuthorizationHeader(t *testing.T) {
 	for _, entry := range logger.Entries() {
 		if entry.Fields["headers"] != nil {
 			headers := entry.Fields["headers"].(http.Header)
-			authzHeader := headers.Get("Authorization")
-			if authzHeader != "" {
-				reqLogFound = true
-				if authzHeader != "REDACTED" {
-					t.Errorf("Excpected redacted Authorization header value, got %v", authzHeader)
+			for k := range headers {
+				v := headers.Get(k)
+				if _, ok := maskedHeaderKeys[k]; ok {
+					reqLogFound = true
+					if v != "REDACTED" {
+						t.Errorf("Expected redacted %q header value, got %v", k, v)
+					}
+				} else if k == "Remains-Unmasked" && v != plaintext {
+					t.Errorf("Expected %q header to have value %q, got %v", k, plaintext, v)
 				}
 			}
 		}
