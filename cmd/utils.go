b369ea789c6d88764552401efd334215799554ea
Stephan Renatus <stephan@styra.com>
2023-12-18 14:17:34 +0100

    cmd: use regoError to carry compiler errors into CLI machinery
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The OPA Authors.  All rights reserved.
+// Copyright 2025 The OPA Authors.  All rights reserved.
 // Use of this source code is governed by an Apache2
 // license that can be found in the LICENSE file.
 
@@ -9,13 +9,22 @@ import (
 )
 
 type ExitError struct {
-	Exit int
+	Exit    int
+	wrapped error
 }
 
 func newExitError(exit int) error {
 	return &ExitError{Exit: exit}
 }
 
+func newExitErrorWrap(exit int, err error) error {
+	return &ExitError{Exit: exit, wrapped: err}
+}
+
 func (c *ExitError) Error() string {
 	return fmt.Sprintf("exit %d", c.Exit)
+}
+
+func (c *ExitError) Unwrap() error {
+	return c.wrapped
 }
