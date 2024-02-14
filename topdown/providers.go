37c2da897a730bf68ed49056610458feedd18d64
Prasanth Jayachandran <p_jayachandran@apple.com>
2024-02-14 15:38:14 -0800

    aws: support for Unsigned Payload or provided content sha256 in AWS signing (#6581)
    To support uses cases where OPA is used for signing s3 requests whose payload is
    not known upfront or payload is big enough (big file upload) to be sent over wire,
    this PR adds support for unsigned payloads.
    
    AWS signer has configurable option to use unsigned payload where the
    x-amz-content-sha256 is set to "UNSIGNED-PAYLOAD" and is included as part
    of signing process. This PR provides an option for unsigned payload if
    aws_config.disable_payload_signing is set to true. If payload signing is
    disabled, SignV4 method will not compute the content sha from the request body
    but instead use "UNSIGNED-PAYLOAD" string literal for x-amz-content-sha256
    header during signature computation.
    
    References:
    https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-header-based-auth.html
    https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-auth-using-authorization-header.html
    Signed-off-by: Prasanth Jayachandran <p_jayachandran@apple.com>

--- topdown/providers.go
+++ topdown/providers.go
@@ -173,7 +173,19 @@ func builtinAWSSigV4SignReq(ctx BuiltinContext, operands []*ast.Term, iter func(
 
 	// Sign the request object's headers, and reconstruct the headers map.
 	headersMap := objectToMap(headers)
-	authHeader, awsHeadersMap := aws.SignV4(headersMap, method, theURL, body, service, awsCreds, signingTimestamp)
+
+	// if payload signing config is set, pass it down to the signing method
+	disablePayloadSigning := false
+	t := awsConfigObj.Get(ast.StringTerm("disable_payload_signing"))
+	if t != nil {
+		if v, ok := t.Value.(ast.Boolean); ok {
+			disablePayloadSigning = bool(v)
+		} else {
+			return builtins.NewOperandErr(2, "invalid value for 'disable_payload_signing' in AWS config")
+		}
+	}
+
+	authHeader, awsHeadersMap := aws.SignV4(headersMap, method, theURL, body, service, awsCreds, signingTimestamp, disablePayloadSigning)
 	signedHeadersObj := ast.NewObject()
 	// Restore original headers
 	for k, v := range headersMap {
