f448f68ff4fa87ec04832a2473f4f2d033b5dcb8
Thomas Sidebottom <thomas.sidebottom@va.gov>
2024-02-21 17:23:26 -0500

    Update decision_label_add.go
    Signed-off-by: Thomas Sidebottom <thomas.sidebottom@va.gov>

--- topdown/decision_label_add.go
+++ topdown/decision_label_add.go
@@ -1,1 +1,8 @@
 package topdown
+
+// add init() in here
+// In init() register the function
+// http.go Line 201
+
+// Operands (http.go Line 128) reference the Decl field from the Builtin Struct and are insanely useful in here
+// Need to ensure proper state is accessed, related to ast, no further information right now.
