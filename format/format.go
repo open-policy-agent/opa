64ef639b6ba52506065b4f18a03dd33240917333
Ashutosh Narkar <anarkar4387@gmail.com>
2024-02-22 15:33:43 -0800

    Add a conditional for blank line insertion
    Signed-off-by: Ashutosh Narkar <anarkar4387@gmail.com>

--- format/format.go
+++ format/format.go
@@ -394,7 +394,9 @@ func (w *writer) writeRules(rules []*ast.Rule, o fmtOpts, comments []*ast.Commen
 
 			// add a blank line to separate annotations from trailing comments to avoid errors during
 			// annotation parsing which expects a blank line between them
-			w.blankLine()
+			if len(comments) > 0 {
+				w.blankLine()
+			}
 		}
 
 		comments = w.insertComments(comments, rule.Location)
