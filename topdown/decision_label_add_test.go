34d2b30092e0054d874621048f3b52ec567a50e8
Thomas Sidebottom <thomas.sidebottom@va.gov>
2024-03-19 19:37:29 -0400

    Started new builtin test file
    Signed-off-by: Thomas Sidebottom <thomas.sidebottom@va.gov>

--- topdown/decision_label_add_test.go
+++ topdown/decision_label_add_test.go
@@ -1,1 +1,58 @@
 package topdown
+
+import (
+	"github.com/open-policy-agent/opa/ast"
+	"testing"
+)
+
+func TestValidateKeyStringOperand(t *testing.T) {
+
+	input := ast.StringTerm("foo")
+
+	if _, err := validateKeyStringOperand(input, 1); err != nil {
+		t.Fatalf("String Key %s was not properly validated.", input.String())
+	}
+
+}
+
+func TestValidateKeyStringOperandInvalidKeyType(t *testing.T) {
+
+	input := ast.IntNumberTerm(1337)
+
+	if _, err := validateKeyStringOperand(input, 1); err == nil {
+		t.Fatalf("Non-String Key %s passed validation.", input.Value)
+	}
+
+}
+
+func TestValidateValueStringOperand(t *testing.T) {
+
+	input := ast.StringTerm("bar")
+
+	if _, err := validateValueStringOperand(input, 2); err != nil {
+		t.Fatalf("String Value %s was not properly validated.", input.String())
+	}
+
+}
+
+func TestValidateValueStringOperandInvalidValueType(t *testing.T) {
+
+	input := ast.IntNumberTerm(1338)
+
+	if _, err := validateValueStringOperand(input, 2); err == nil {
+		t.Fatalf("Non-String Value %s passed validation.", input.Value)
+	}
+
+}
+
+func TestAssignOperandsToDecisionLabel(t *testing.T) {
+
+	//ctx := context.Background()
+	//key := ast.StringTerm("foo")
+	//value := ast.StringTerm("bar")
+
+}
+
+func TestAssignOperandsToDecisionLabelSameKeySecondEntry(t *testing.T) {
+
+}
