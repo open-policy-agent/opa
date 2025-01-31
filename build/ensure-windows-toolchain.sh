e09de8d9ab173ddb9970d1f3e3a9b1d063c41623
Stephan Renatus <stephan@styra.com>
2025-01-31 20:35:32 +0100

    try experimental package
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- build/ensure-windows-toolchain.sh
+++ build/ensure-windows-toolchain.sh
@@ -5,6 +5,7 @@ CC=x86_64-w64-mingw32-gcc
 PKG=gcc-mingw-w64-x86-64
 
 type -f ${CC} 2>/dev/null && exit 0
+echo "deb https://deb.debian.org/debian experimental main" >> /etc/apt/sources.list
 
 apt-get update && \
   apt-get install --no-install-recommends -y ${PKG} && \
