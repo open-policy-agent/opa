624a70b17441eac70bfcefd7ff4672894889b62d
sspaink <sspaink@styra.com>
2025-04-24 14:16:57 -0500

    revert TestStatusV1 changes
    Signed-off-by: sspaink <sspaink@styra.com>

--- v1/server/server_test.go
+++ v1/server/server_test.go
@@ -3964,10 +3964,6 @@ func TestStatusV1(t *testing.T) {
 		}
 	}
 
-	if !ok {
-		t.Fatal("plugin status never changed to 'OK' status")
-	}
-
 	// Expect HTTP 200 and updated status after bundle update occurs
 	bs.BulkUpdateBundleStatus(map[string]*pluginBundle.Status{
 		"test": {
@@ -3976,39 +3972,33 @@ func TestStatusV1(t *testing.T) {
 		},
 	})
 
-	t0 = time.Now()
-	ok = false
-	for !ok && time.Since(t0) < time.Second {
-		req = newReqV1(http.MethodGet, "/status", "")
-		f.reset()
-		f.server.Handler.ServeHTTP(f.recorder, req)
-		if f.recorder.Result().StatusCode != http.StatusOK {
-			t.Fatal("expected ok")
-		}
+	req = newReqV1(http.MethodGet, "/status", "")
+	f.reset()
+	f.server.Handler.ServeHTTP(f.recorder, req)
 
-		var resp2 struct {
-			Result struct {
-				Bundles struct {
-					Test struct {
-						Name     string
-						HTTPCode json.Number `json:"http_code"`
-					}
+	if f.recorder.Result().StatusCode != http.StatusOK {
+		t.Fatal("expected ok")
+	}
+
+	var resp2 struct {
+		Result struct {
+			Bundles struct {
+				Test struct {
+					Name     string
+					HTTPCode json.Number `json:"http_code"`
 				}
 			}
 		}
-
-		if err := util.NewJSONDecoder(f.recorder.Body).Decode(&resp2); err != nil {
-			t.Fatal(err)
-		}
-		if resp2.Result.Bundles.Test.HTTPCode == "403" && resp2.Result.Bundles.Test.Name == "test" {
-			ok = true
-		} else {
-			t.Log("expected status to be 403 and bundle name to be \"test\" but got:", resp2)
-		}
 	}
 
-	if !ok {
-		t.Fatal("plugin status never changed to 'OK' status")
+	if err := util.NewJSONDecoder(f.recorder.Body).Decode(&resp2); err != nil {
+		t.Fatal(err)
+	}
+	if resp2.Result.Bundles.Test.Name != "test" {
+		t.Fatal("expected bundle to exist in status response but got:", resp2)
+	}
+	if resp2.Result.Bundles.Test.HTTPCode != "403" {
+		t.Fatal("expected HTTPCode to equal 403 but got:", resp2)
 	}
 }
 
