7dd0dceb6d70783fe7d732f11f00a832e3347ada
robmyersrobmyers <1243316+robmyersrobmyers@users.noreply.github.com>
2025-05-05 12:11:27 -0400

    refactor: Remove internal/gqlparser and use upstream instead. (#7538)
    This updates gqlparser from version ~v2.5.1 and the June 2018 GraphQL spec to
    v2.5.26 and the October 2021 GraphQL spec.
    
    Signed-off-by: Rob Myers <1243316+robmyersrobmyers@users.noreply.github.com>

---
+++ vendor/github.com/vektah/gqlparser/v2/validator/rules/no_unused_fragments.go
@@ -0,0 +1,36 @@
+package rules
+
+import (
+	"github.com/vektah/gqlparser/v2/ast"
+
+	//nolint:staticcheck // Validator rules each use dot imports for convenience.
+	. "github.com/vektah/gqlparser/v2/validator"
+)
+
+var NoUnusedFragmentsRule = Rule{
+	Name: "NoUnusedFragments",
+	RuleFunc: func(observers *Events, addError AddErrFunc) {
+		inFragmentDefinition := false
+		fragmentNameUsed := make(map[string]bool)
+
+		observers.OnFragmentSpread(func(walker *Walker, fragmentSpread *ast.FragmentSpread) {
+			if !inFragmentDefinition {
+				fragmentNameUsed[fragmentSpread.Name] = true
+			}
+		})
+
+		observers.OnFragment(func(walker *Walker, fragment *ast.FragmentDefinition) {
+			inFragmentDefinition = true
+			if !fragmentNameUsed[fragment.Name] {
+				addError(
+					Message(`Fragment "%s" is never used.`, fragment.Name),
+					At(fragment.Position),
+				)
+			}
+		})
+	},
+}
+
+func init() {
+	AddRule(NoUnusedFragmentsRule.Name, NoUnusedFragmentsRule.RuleFunc)
+}
