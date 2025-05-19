5a91750537a891c660a7ee9037a07d30cd395509
Charlie Egan <charlie@styra.com>
2025-05-19 11:05:21 +0100

    docs: Use browser only for notfound content
    Using the window variables is not possible in builds.
    
    Signed-off-by: Charlie Egan <charlie@styra.com>

--- docs/src/theme/NotFound/index.js
+++ docs/src/theme/NotFound/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import {translate} from '@docusaurus/Translate';
-import {PageMetadata} from '@docusaurus/theme-common';
-import Layout from '@theme/Layout';
-import NotFoundContent from '@theme/NotFound/Content';
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import { PageMetadata } from "@docusaurus/theme-common";
+import { translate } from "@docusaurus/Translate";
+import Layout from "@theme/Layout";
+import NotFoundContent from "@theme/NotFound/Content";
+import React from "react";
 export default function Index() {
   const title = translate({
-    id: 'theme.NotFound.title',
-    message: 'Page Not Found',
+    id: "theme.NotFound.title",
+    message: "Page Not Found",
   });
   return (
     <>
       <PageMetadata title={title} />
       <Layout>
-        <NotFoundContent />
+        <BrowserOnly>
+          <NotFoundContent />
+        </BrowserOnly>
       </Layout>
     </>
   );
