de8ec20a5a2d13bc7cee5fed9c1191889157c631
David Schmitt <davidschmittgit@gmail.com>
2025-07-15 15:57:30 -0400

    v1/ast: Fix "undeclared" error when printing nested comprehension
    Treat generated variables as safe during rewritePrintCalls's safety
    analysis stage. This prevents an "undeclared" error originating from
    from head keys within inner comprehensions. These head keys would
    typically be marked safe in later compiler stages. A "proper" safety
    analysis would require further compiler context and comprehension
    rewriting. Instead this fix avoids duplicating the logic of later
    compiler stages.
    
    Fixes: #7647
    Signed-off-by: David Schmitt <davidschmittgit@gmail.com>

--- v1/ast/compile.go
+++ v1/ast/compile.go
@@ -2163,6 +2163,15 @@ func rewritePrintCalls(gen *localVarGenerator, getArity func(Ref) int, globals V
 		var errs Errors
 		safe := outputVarsForBody(body[:i], getArity, globals)
 		safe.Update(globals)
+
+		// Fixes Issue #7647 by adding generated variables to the safe set
+		WalkVars(body[:i], func(v Var) bool {
+			if v.IsGenerated() {
+				safe.Add(v)
+			}
+			return false
+		})
+
 		args := body[i].Operands()
 
 		var vis *VarVisitor
