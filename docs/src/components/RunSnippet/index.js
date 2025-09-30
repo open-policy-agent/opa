3d87732b160e9128d5881cf8f3bd0317d4718b9d
Charlie Egan <charlie_egan@apple.com>
2025-09-30 15:10:31 +0100

    website: FOUC squashing on the homepage
    https://en.wikipedia.org/wiki/Flash_of_unstyled_content
    
    Signed-off-by: Charlie Egan <charlie_egan@apple.com>

--- docs/src/components/RunSnippet/index.js
+++ docs/src/components/RunSnippet/index.js
@@ -1,4 +1,4 @@
-import { React, useMemo } from "react";
+import { React, useEffect, useMemo, useState } from "react";
 
 import BrowserOnly from "@docusaurus/BrowserOnly";
 
@@ -30,6 +30,17 @@ const emojis = [
 ];
 
 export default function RunSnippet({ id, files, depends, command, playgroundLink }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const loadDelay = 500;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, loadDelay);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   if (!command && !files) {
     // json file
     return (
@@ -42,28 +53,33 @@ export default function RunSnippet({ id, files, depends, command, playgroundLink
   const icon = useMemo(() => emojis[Math.floor(Math.random() * emojis.length)], []);
 
   return (
-    <BrowserOnly>
-      {() => (
-        <codapi-snippet
-          sandbox="javascript"
-          engine="playground"
-          editor="basic"
-          id={id}
-          command={command}
-          files={files}
-          depends-on={depends}
-          init-delay="500" // we need this for codapi-toolbar to work
-        >
-          <codapi-toolbar>
-            <button>Evaluate</button>
-            <a href="#edit">Edit</a>
-            {playgroundLink && <a target="_blank" href={playgroundLink}>Open in Playground</a>}
-            <div className={styles.codeApiHider}>
-              <codapi-status done={`${icon} Done in $DURATION`}></codapi-status>
-            </div>
-          </codapi-toolbar>
-        </codapi-snippet>
-      )}
-    </BrowserOnly>
+    <>
+      <BrowserOnly>
+        {() => (
+          <codapi-snippet
+            sandbox="javascript"
+            engine="playground"
+            editor="basic"
+            id={id}
+            command={command}
+            files={files}
+            depends-on={depends}
+            init-delay={500} // we need this for codapi-toolbar to work
+            className={isLoading ? styles.dn : styles.codeApiSnippet}
+          >
+            <codapi-toolbar>
+              <button>Evaluate</button>
+              <a href="#edit">Edit</a>
+              {playgroundLink && <a target="_blank" href={playgroundLink}>Open in Playground</a>}
+              <div className={styles.codeApiHider}>
+                <codapi-status done={`${icon} Done in $DURATION`}></codapi-status>
+              </div>
+            </codapi-toolbar>
+          </codapi-snippet>
+        )}
+      </BrowserOnly>
+      {/* must be at the end or it'll become the codapi policy */}
+      <div className={isLoading ? styles.codeApiSnippetLoadingPlaceholder : styles.dn}>Loading...</div>
+    </>
   );
 }
