c52a154f211065095dc6dfac13dc2179e1b0cfac
Sebastian Spaink <3441183+sspaink@users.noreply.github.com>
2026-01-14 13:37:26 -0600

    allow multiple calls to `config.ValidateAndInjectDefaults` (#8216)
    Add a separate field to keep track of the parsed min and max interval keeping the original fields to represent the user provided values. This prevents multiple calls to config.ValidateAndInjectDefaults from overflowing the values but still makes sure that the value has been converted at least once.
    
    Signed-off-by: Sebastian Spaink <sebastianspaink@gmail.com>
    (cherry picked from commit 95280c0fcf2b683b224988c58a19b57b5a2dfd58)

--- v1/download/config_test.go
+++ v1/download/config_test.go
@@ -6,6 +6,7 @@ package download
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 )
@@ -13,11 +14,13 @@ import (
 func TestConfigValidation(t *testing.T) {
 
 	tests := []struct {
-		note    string
-		input   string
-		wantErr bool
-		expMin  time.Duration
-		expMax  time.Duration
+		note         string
+		input        string
+		wantErr      bool
+		expMin       *int64
+		expMax       *int64
+		expParsedMin time.Duration
+		expParsedMax time.Duration
 	}{
 		{
 			note: "min > max",
@@ -30,10 +33,12 @@ func TestConfigValidation(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			note:   "empty",
-			input:  `{}`,
-			expMin: time.Second * time.Duration(defaultMinDelaySeconds),
-			expMax: time.Second * time.Duration(defaultMaxDelaySeconds),
+			note:         "empty",
+			input:        `{}`,
+			expMin:       nil,
+			expMax:       nil,
+			expParsedMin: time.Second * time.Duration(defaultMinDelaySeconds),
+			expParsedMax: time.Second * time.Duration(defaultMaxDelaySeconds),
 		},
 		{
 			note: "min missing",
@@ -61,8 +66,16 @@ func TestConfigValidation(t *testing.T) {
 					"max_delay_seconds": 30
 				}
 			}`,
-			expMin: time.Second * 10,
-			expMax: time.Second * 30,
+			expMin: func() *int64 {
+				min := int64(10)
+				return &min
+			}(),
+			expMax: func() *int64 {
+				max := int64(30)
+				return &max
+			}(),
+			expParsedMin: time.Second * 10,
+			expParsedMax: time.Second * 30,
 		},
 		{
 			note: "long polling timeout < 1",
@@ -76,25 +89,107 @@ func TestConfigValidation(t *testing.T) {
 	}
 
 	for _, test := range tests {
+		t.Run(test.note, func(t *testing.T) {
+			var config Config
 
-		var config Config
-
-		if err := json.Unmarshal([]byte(test.input), &config); err != nil {
-			t.Fatal(err)
-		}
-
-		err := config.ValidateAndInjectDefaults()
-		if err != nil && !test.wantErr {
-			t.Errorf("Unexpected error on: %v, err: %v", test.input, err)
-		}
-
-		if err == nil {
-			if time.Duration(*config.Polling.MinDelaySeconds) != test.expMin {
-				t.Errorf("For %q expected min %v but got %v", test.note, test.expMin, time.Duration(*config.Polling.MinDelaySeconds))
+			if err := json.Unmarshal([]byte(test.input), &config); err != nil {
+				t.Fatal(err)
 			}
-			if time.Duration(*config.Polling.MaxDelaySeconds) != test.expMax {
-				t.Errorf("For %q expected min %v but got %v", test.note, test.expMax, time.Duration(*config.Polling.MaxDelaySeconds))
+
+			// no matter how many calls to ValidateAndInjectDefaults, the values should stay the same
+			for range 3 {
+				err := config.ValidateAndInjectDefaults()
+				if err != nil && !test.wantErr {
+					t.Errorf("Unexpected error on: %v, err: %v", test.input, err)
+				} else if err != nil && test.wantErr {
+					return
+				}
+
+				if config.Polling.MinDelaySeconds == nil && test.expMin != nil {
+					t.Fatal("Expected min delay seconds to be set")
+				}
+				if config.Polling.MinDelaySeconds != nil && *config.Polling.MinDelaySeconds != *test.expMin {
+					t.Errorf("For %q expected min %v but got %v", test.note, test.expMin, time.Duration(*config.Polling.MinDelaySeconds))
+				}
+				if config.Polling.MaxDelaySeconds == nil && test.expMax != nil {
+					t.Fatal("Expected min delay seconds to be set")
+				}
+				if config.Polling.MaxDelaySeconds != nil && *config.Polling.MaxDelaySeconds != *test.expMax {
+					t.Errorf("For %q expected max %v but got %v", test.note, test.expMax, time.Duration(*config.Polling.MaxDelaySeconds))
+				}
+
+				if time.Duration(*config.Polling.parsedMinDelaySeconds) != test.expParsedMin {
+					t.Errorf("For %q expected min %v but got %v", test.note, test.expParsedMin, time.Duration(*config.Polling.MinDelaySeconds))
+				}
+				if time.Duration(*config.Polling.parsedMaxDelaySeconds) != test.expParsedMax {
+					t.Errorf("For %q expected max %v but got %v", test.note, test.expParsedMax, time.Duration(*config.Polling.MaxDelaySeconds))
+				}
 			}
-		}
+		})
+	}
+}
+
+func TestConfigValidationUpdate(t *testing.T) {
+	expMin := int64(10)
+	expMax := int64(30)
+	expParsedMin := time.Second * time.Duration(expMin)
+	expParsedMax := time.Second * time.Duration(expMax)
+	var config Config
+
+	if err := json.Unmarshal([]byte(fmt.Sprintf(`{
+				"polling": {
+					"min_delay_seconds": %d,
+					"max_delay_seconds": %d
+				}
+			}`, expMin, expMax)), &config); err != nil {
+		t.Fatal(err)
+	}
+
+	err := config.ValidateAndInjectDefaults()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if *config.Polling.MinDelaySeconds != expMin {
+		t.Errorf("expected min %v but got %v", expMin, time.Duration(*config.Polling.MinDelaySeconds))
+	}
+
+	if config.Polling.MaxDelaySeconds != nil && *config.Polling.MaxDelaySeconds != expMax {
+		t.Errorf("expected max %v but got %v", expMax, time.Duration(*config.Polling.MaxDelaySeconds))
+	}
+
+	if time.Duration(*config.Polling.parsedMinDelaySeconds) != expParsedMin {
+		t.Errorf("expected min %v but got %v", expParsedMin, time.Duration(*config.Polling.MinDelaySeconds))
+	}
+	if time.Duration(*config.Polling.parsedMaxDelaySeconds) != expParsedMax {
+		t.Errorf("expected max %v but got %v", expParsedMax, time.Duration(*config.Polling.MaxDelaySeconds))
+	}
+
+	expMin = int64(50)
+	expMax = int64(100)
+	expParsedMin = time.Second * time.Duration(expMin)
+	expParsedMax = time.Second * time.Duration(expMax)
+
+	config.Polling.MinDelaySeconds = &expMin
+	config.Polling.MaxDelaySeconds = &expMax
+
+	err = config.ValidateAndInjectDefaults()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if *config.Polling.MinDelaySeconds != expMin {
+		t.Errorf("expected min %v but got %v", expMin, time.Duration(*config.Polling.MinDelaySeconds))
+	}
+
+	if config.Polling.MaxDelaySeconds != nil && *config.Polling.MaxDelaySeconds != expMax {
+		t.Errorf("expected max %v but got %v", expMax, time.Duration(*config.Polling.MaxDelaySeconds))
+	}
+
+	if time.Duration(*config.Polling.parsedMinDelaySeconds) != expParsedMin {
+		t.Errorf("expected min %v but got %v", expParsedMin, time.Duration(*config.Polling.MinDelaySeconds))
+	}
+	if time.Duration(*config.Polling.parsedMaxDelaySeconds) != expParsedMax {
+		t.Errorf("expected max %v but got %v", expParsedMax, time.Duration(*config.Polling.MaxDelaySeconds))
 	}
 }
