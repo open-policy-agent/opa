f49259a26be9d1e86f7acedfc9822fc4f1de870e
Stephan Renatus <stephan.renatus@gmail.com>
2026-02-05 13:06:32 +0100

    wasm: bump LLVM 13 -> 21, adjust headers
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- wasm/src/libc++/minimal.cc
+++ wasm/src/libc++/minimal.cc
@@ -13,6 +13,10 @@ void operator delete(void *p) {
     opa_free(p);
 }
 
+void operator delete(void *p, size_t) {
+    opa_free(p);
+}
+
 void* operator new[](size_t size) {
     return opa_malloc(size);
 }
@@ -21,22 +25,24 @@ void operator delete[](void *p) {
     opa_free(p);
 }
 
+void operator delete[](void *p, size_t) {
+    opa_free(p);
+}
+
 extern "C" void __cxa_pure_virtual() {
     opa_abort("pure virtual");
 }
+
+// LLVM 21 verbose abort handler
+_LIBCPP_BEGIN_NAMESPACE_STD
+    void __libcpp_verbose_abort(const char *format, ...) noexcept {
+        opa_abort(format);
+    }
+_LIBCPP_END_NAMESPACE_STD
 
 // Instantiate the minimum set of templates part of standard libc++ ABI.
 // This is required because we do not link with the libc++.
 
 _LIBCPP_BEGIN_NAMESPACE_STD
-
-template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS __basic_string_common<true>;
-template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_string<char>;
-template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_string<wchar_t>;
-
-template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS __vector_base_common<true>;
-
-template void __sort<__less<int>&, int*>(int*, int*, __less<int>&);
-template bool __insertion_sort_incomplete<__less<int>&, int*>(int*, int*, __less<int>&);
-
+template class basic_string<char>;
 _LIBCPP_END_NAMESPACE_STD
