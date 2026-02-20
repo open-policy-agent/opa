7617f4c5590614fc3ffa6d7413edd2569017631b
Anders Eknert <anders.eknert@apple.com>
2026-02-20 15:55:06 +0100

    perf: Allocate less in array unification
    Surprisingly, this sailed up as the major source of B/op in the Regal
    linting itself benchmark. I guess we never paid attention to it in the
    past as there were many worse culprits.
    
    With help from @tsandall we tracked this down to Regal's frequent use
    of walk, and how that'd unify the path-value array. Not sure if there
    are any other noteworthy scenarios that lead on to this path, but at
    least us `walk` enthusiasts get to start the weekend in the best possible
    way.
    
    No surprises wrt the fix here. Just cram everything into a single struct that
    can be reused across requests via a sync pool. Not particularly elegant, but
    fairly limited, so I hope to get this in even if the impact should be minimal
    outside of `walk` heavy integrations.
    
    RegalLintingItself benchmark, main vs change. Ns/op and # allocs largely
    unchanged, while the difference in B/op is rather massive for a simple
    change!
    ```
    348015056 ns/op	1260364874 B/op	31868705 allocs/op
    343963417 ns/op	1122163613 B/op	31874931 allocs/op
    ```
    
    Signed-off-by: Anders Eknert <anders.eknert@apple.com>

--- v1/topdown/eval.go
+++ v1/topdown/eval.go
@@ -32,6 +32,13 @@ type queryIDFactory struct {
 	curr uint64
 }
 
+type biunifyArraysRecParams struct {
+	a, b   *ast.Array
+	b1, b2 *bindings
+	iter   unifyIterator
+	idx    int
+}
+
 // Note: The first call to Next() returns 0.
 func (f *queryIDFactory) Next() uint64 {
 	curr := f.curr
@@ -147,10 +154,11 @@ func (ep *evbp) Get() *evalBuiltin {
 }
 
 var (
-	evalPool     = util.NewSyncPool[eval]()
-	deecPool     = util.NewSyncPool[deferredEarlyExitContainer]()
-	resolverPool = util.NewSyncPool[evalResolver]()
-	evalFuncPool = &evfp{
+	evalPool      = util.NewSyncPool[eval]()
+	deecPool      = util.NewSyncPool[deferredEarlyExitContainer]()
+	resolverPool  = util.NewSyncPool[evalResolver]()
+	arraysRecPool = util.NewSyncPool[biunifyArraysRecParams]()
+	evalFuncPool  = &evfp{
 		pool: sync.Pool{
 			New: func() any {
 				return &evalFunc{}
@@ -1088,7 +1096,12 @@ func (e *eval) biunify(a, b *ast.Term, b1, b2 *bindings, iter unifyIterator) err
 			return e.biunifyValues(a, b, b1, b2, iter)
 		case *ast.Array:
 			if vA.Len() == vB.Len() {
-				return e.biunifyArraysRec(vA, vB, b1, b2, iter, 0)
+				params := arraysRecPool.Get()
+				params.a, params.b = vA, vB
+				params.b1, params.b2 = b1, b2
+				params.iter = iter
+				params.idx = 0
+				return e.biunifyArraysRec(params)
 			}
 		}
 	case ast.Object:
@@ -1104,12 +1117,15 @@ func (e *eval) biunify(a, b *ast.Term, b1, b2 *bindings, iter unifyIterator) err
 	return nil
 }
 
-func (e *eval) biunifyArraysRec(a, b *ast.Array, b1, b2 *bindings, iter unifyIterator, idx int) error {
-	if idx == a.Len() {
-		return iter()
+func (e *eval) biunifyArraysRec(params *biunifyArraysRecParams) error {
+	if params.idx == params.a.Len() {
+		err := params.iter()
+		arraysRecPool.Put(params)
+		return err
 	}
-	return e.biunify(a.Elem(idx), b.Elem(idx), b1, b2, func() error {
-		return e.biunifyArraysRec(a, b, b1, b2, iter, idx+1)
+	return e.biunify(params.a.Elem(params.idx), params.b.Elem(params.idx), params.b1, params.b2, func() error {
+		params.idx++
+		return e.biunifyArraysRec(params)
 	})
 }
 
