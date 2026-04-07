624d3378bcff0c84a5239cfd890f4cba7e79271a
Michael Munch <mm.munk@gmail.com>
2026-04-07 21:17:00 +0200

    Update internal/distributedtracing/distributedtracing_test.go
    Co-authored-by: Sebastian Spaink <sebastianspaink@gmail.com>
    Signed-off-by: Michael Munch <mm.munk@gmail.com>

--- internal/distributedtracing/distributedtracing_test.go
+++ internal/distributedtracing/distributedtracing_test.go
@@ -1,4 +1,4 @@
-// Copyright 2024 The OPA Authors.  All rights reserved.
+// Copyright 2026 The OPA Authors.  All rights reserved.
 // Use of this source code is governed by an Apache2
 // license that can be found in the LICENSE file.
 
