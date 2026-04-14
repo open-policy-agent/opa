96620b90220faec2fbcf733b6ca6a79a40046fb7
Sebastian Spaink <sebastianspaink@gmail.com>
2026-04-14 15:47:24 -0500

    stream tests
    Signed-off-by: Sebastian Spaink <sebastianspaink@gmail.com>

--- v1/tester/fixture_test.go
+++ v1/tester/fixture_test.go
@@ -1,20 +1,6 @@
 package tester
 
-const fixtureReporterVerboseBenchmark = `FAILURES
---------------------------------------------------------------------------------
-data.foo.bar.test_corge: FAIL (0s)
-
-  query:1       | Fail true = false  
-
-data.foo.bar.test_cases_fail: FAIL (0s)
-
-  query:1       | Fail true = false  
-
-  two: FAIL
-
-SUMMARY
---------------------------------------------------------------------------------
-data.foo.bar.test_baz	    1000	       123.0 ns/op
+const fixtureReporterVerboseBenchmark = `data.foo.bar.test_baz	    1000	       123.0 ns/op
 data.foo.bar.test_qux: ERROR (0s)
   some err
 data.foo.bar.test_corge: FAIL (0s)
@@ -23,39 +9,50 @@ data.foo.bar.test_cases_fail: FAIL (0s)
   two: FAIL
 data.foo.bar.test_cases_ok	    2000	        61.50 ns/op
 --------------------------------------------------------------------------------
+FAILURES
+--------------------------------------------------------------------------------
+data.foo.bar.test_corge: FAIL (0s)
+
+  query:1       | Fail true = false  
+
+data.foo.bar.test_cases_fail: FAIL (0s)
+
+  query:1       | Fail true = false  
+
+  two: FAIL
+
+--------------------------------------------------------------------------------
 PASS: 4/7
 FAIL: 2/7
 ERROR: 1/7
 `
 
-const fixtureReporterVerboseBenchmarkShowAllocations = `FAILURES
---------------------------------------------------------------------------------
-data.foo.bar.test_corge: FAIL (0s)
-
-
-SUMMARY
---------------------------------------------------------------------------------
-data.foo.bar.test_baz	    1000	       123.0 ns/op	       123.0 timer_rego_query_eval_ns/op	      91 B/op	       0 allocs/op
+const fixtureReporterVerboseBenchmarkShowAllocations = `data.foo.bar.test_baz	    1000	       123.0 ns/op	       123.0 timer_rego_query_eval_ns/op	      91 B/op	       0 allocs/op
 data.foo.bar.test_qux: ERROR (0s)
   some err
 data.foo.bar.test_corge: FAIL (0s)
+--------------------------------------------------------------------------------
+FAILURES
+--------------------------------------------------------------------------------
+data.foo.bar.test_corge: FAIL (0s)
+
+
 --------------------------------------------------------------------------------
 PASS: 1/3
 FAIL: 1/3
 ERROR: 1/3
 `
 
-const fixtureReporterVerboseBenchmarkShowAllocationsGoBenchFormat = `FAILURES
---------------------------------------------------------------------------------
-data.foo.bar.test_corge: FAIL (0s)
-
-
-SUMMARY
---------------------------------------------------------------------------------
-BenchmarkDataFooBarTestBaz	    1000	       123.0 ns/op	       123.0 timer_rego_query_eval_ns/op	      91 B/op	       0 allocs/op
+const fixtureReporterVerboseBenchmarkShowAllocationsGoBenchFormat = `BenchmarkDataFooBarTestBaz	    1000	       123.0 ns/op	       123.0 timer_rego_query_eval_ns/op	      91 B/op	       0 allocs/op
 data.foo.bar.test_qux: ERROR (0s)
   some err
 data.foo.bar.test_corge: FAIL (0s)
+--------------------------------------------------------------------------------
+FAILURES
+--------------------------------------------------------------------------------
+data.foo.bar.test_corge: FAIL (0s)
+
+
 --------------------------------------------------------------------------------
 PASS: 1/3
 FAIL: 1/3
