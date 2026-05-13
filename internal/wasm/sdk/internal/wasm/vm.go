40344ea4177d7f69782db62d202f5bf91fc1cc90
Stephan Renatus <stephan.renatus@gmail.com>
2026-05-13 10:40:34 +0200

    deps: bump wasmtime-go (v43 -> v44)
    Signed-off-by: Stephan Renatus <stephan.renatus@gmail.com>

--- internal/wasm/sdk/internal/wasm/vm.go
+++ internal/wasm/sdk/internal/wasm/vm.go
@@ -14,7 +14,7 @@ import (
 	"strings"
 	"time"
 
-	wasmtime "github.com/bytecodealliance/wasmtime-go/v43"
+	wasmtime "github.com/bytecodealliance/wasmtime-go/v44"
 
 	sdk_errors "github.com/open-policy-agent/opa/internal/wasm/sdk/opa/errors"
 	"github.com/open-policy-agent/opa/internal/wasm/util"
