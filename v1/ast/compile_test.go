6b478950d8f6621ede5751dcaf62c78d0d408478
zanarelli <zanarelli.dev@gmail.com>
2026-08-01 21:07:23 -0300

    ast: report root unsafe var for := assignments
    When an assignment is unsafe because of vars on the RHS, safetyErrorSlice now prefers those over the assignment target. Reverts the compile_test expectation from PR #8962 review.
    
    Signed-off-by: zanarelli <zanarelli.dev@gmail.com>

--- v1/ast/compile_test.go
+++ v1/ast/compile_test.go
@@ -9023,7 +9023,7 @@ func TestCompilerRewriteTemplateStringsErrors(t *testing.T) {
 				p := msg if {
 					msg := $"{x}"
 				}`,
-			exp: "var msg is unsafe",
+			exp: "var x is unsafe",
 		},
 		{
 			note: "undeclared var (wildcard)",
